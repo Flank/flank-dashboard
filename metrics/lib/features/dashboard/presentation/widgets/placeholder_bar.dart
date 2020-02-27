ff4166e65fce507a65a0d1246f9c056ed402996e
vova-beloded <41615621+vova-beloded@users.noreply.github.com>
2020-02-27 13:39:18 +0200

    Metric widgets improvements (#83)
    * Metric widgets improvements
    
    * Update light theme to match new designs (#79)
    
    * Update light theme to match new designs
    
    * Minor text improvements
    
    * More tests and minor improvements
    
    * Update light theme to match new designs (#79)
    
    * Update light theme to match new designs
    
    * Minor text improvements
    
    * Rebase commit
    
    More tests and minor improvements
    
    Update light theme to match new designs (#79)
    
    * Update light theme to match new designs
    
    * Minor text improvements
    
    * Documentation improvements
    
    * Improve sparkline graph chart axes builds calculation
    
    * Make sparkline graph axes calculation algorithm more readable

---
+++ metrics/lib/features/dashboard/presentation/widgets/placeholder_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:metrics/features/dashboard/presentation/widgets/bar_graph.dart';
+import 'package:metrics/features/dashboard/presentation/widgets/colored_bar.dart';
+
+/// Represents the placeholder bar of the [BarGraph].
+class PlaceholderBar extends StatelessWidget {
+  final double width;
+
+  /// Creates the [PlaceholderBar].
+  ///
+  /// This bar displays the missing/empty build in [BarGraph].
+  /// [width] is the width of this bar.
+  const PlaceholderBar({
+    Key key,
+    @required this.width,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      height: 6.0,
+      child: ColoredBar(
+        color: Colors.transparent,
+        width: width,
+        borderRadius: BorderRadius.circular(4.0),
+        border: Border.all(
+          color: Colors.grey,
+          width: 2.0,
+        ),
+      ),
+    );
+  }
+}
--- metrics/lib/features/dashboard/presentation/widgets/sparkline_graph.dart
+++ metrics/lib/features/dashboard/presentation/widgets/sparkline_graph.dart
@@ -11,6 +11,7 @@ import 'package:metrics/features/dashboard/presentation/widgets/expandable_text.
 /// by it's parent.
 class SparklineGraph extends StatelessWidget {
   static const _gradientColorStops = [0.0, 0.9];
+  static const _minChartAxisLength = 1;
 
   final String title;
   final String value;
@@ -151,13 +152,25 @@ class SparklineGraph extends StatelessWidget {
     );
   }
 
-  /// Creates the [DoubleSpan] for axises.
+  /// Creates the [DoubleSpan] for axes.
   ///
   /// The [DoubleSpan] represents the visible range of the axis.
   DoubleSpan _createSpan(List<num> data) {
     if (data == null || data.isEmpty) return null;
 
-    final sortedData = data.toList()..sort();
-    return DoubleSpan(sortedData.first.toDouble(), sortedData.last.toDouble());
+    double min = data.first.toDouble();
+    double max = data.first.toDouble();
+
+    for (final value in data) {
+      if (min > value) min = value.toDouble();
+      if (max < value) max = value.toDouble();
+    }
+
+    final axisLength = max - min;
+    if (axisLength < _minChartAxisLength) {
+      max += _minChartAxisLength - axisLength;
+    }
+
+    return DoubleSpan(min, max);
   }
 }
