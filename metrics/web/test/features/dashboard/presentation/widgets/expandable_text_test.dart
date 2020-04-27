77759faf0202bfb8a20be2ac830229d42be51c09
vova.beloded <vova.beloded@solid.software>
2020-04-27 12:43:39 +0300

    More tests for ExpandableText

--- metrics/web/test/features/dashboard/presentation/widgets/expandable_text_test.dart
+++ metrics/web/test/features/dashboard/presentation/widgets/expandable_text_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:metrics/features/dashboard/presentation/widgets/expandable_text.dart';
@@ -32,31 +31,75 @@ void main() {
         expect(textWidget.style, textStyle);
       },
     );
+
+    testWidgets(
+      "fits large text into a small box",
+      (tester) async {
+        const textSize = 20.0;
+        const text = 'some large text to fit the small box ...';
+
+        await tester.pumpWidget(const _ExpandableTextTestbed(
+          text: text,
+          height: textSize,
+          width: textSize,
+        ));
+
+        expect(find.text(text), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "displays the text inside of FittedBox with BoxFit.contains fit",
+      (tester) async {
+        const text = 'test text';
+        await tester.pumpWidget(const _ExpandableTextTestbed(
+          text: text,
+        ));
+
+        expect(
+          find.byWidgetPredicate(
+            (widget) =>
+                widget is FittedBox &&
+                widget.fit == BoxFit.contain &&
+                widget.child is Text,
+          ),
+          findsOneWidget,
+        );
+      },
+    );
   });
 }
 
 class _ExpandableTextTestbed extends StatelessWidget {
   final String text;
   final TextStyle style;
+  final double height;
+  final double width;
 
   const _ExpandableTextTestbed({
     Key key,
     this.text = 'text',
     this.style,
+    this.height,
+    this.width,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MetricsThemedTestbed(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ExpandableText(
-              text,
-              style: style,
+      body: SizedBox(
+        height: height,
+        width: width,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ExpandableText(
+                text,
+                style: style,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
