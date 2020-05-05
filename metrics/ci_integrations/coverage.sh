18610302e0176c9ac9609a115cd82c0da1f30cd4
dmytroSolidSoftware <dmytroSolidSoftware>
2020-05-05 14:06:31 +0300

    Remove coverage.sh.

--- metrics/ci_integrations/coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-mkdir -p build
-pub get
-pub run test test --coverage build/coverage/
-format_coverage --packages=.packages -i build/coverage/ -o build/coverage.lcov --lcov
-genhtml -o build/coverage-html build/coverage.lcov
