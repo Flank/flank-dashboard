d1f0c8c1166c2793fe04abe7ebedb9f17ed3e332
solid-maxim <maxim.ustych@solid.software>
2020-05-12 12:42:52 +0300

    firestore tests: refactoring

--- metrics/firebase/test/firestore/rules/projects-test.js
+++ metrics/firebase/test/firestore/rules/projects-test.js
@@ -1,32 +1,36 @@
 const {
   setupTestDatabaseWith,
-  getApplication,
+  getApplicationWith,
   tearDown,
 } = require("./test_utils/helpers");
 const { assertFails, assertSucceeds } = require("@firebase/testing");
-const { getProjects, getUser } = require("./test_utils/mock-data");
+const { projects, user } = require("./test_utils/test-data");
 
-describe("Database rules", async () => {
-  const app = await getApplication(getUser());
+describe("Project collection rules", async function () {
+  const app = await getApplicationWith(user);
 
   before(async () => {
-    await setupTestDatabaseWith(getProjects());
+    await setupTestDatabaseWith(projects);
   });
 
-  describe("Project rules", function () {
-    it("allows reading projects by an authenticated user", async () => {
-      await assertSucceeds(app.collection("projects").get());
-    });
+  it("allows reading projects by an authenticated user", async () => {
+    await assertSucceeds(app.collection("projects").get());
+  });
 
-    it("does not allow to read projects by an unauthenticated user", async () => {
-      const app = await getApplication(null);
+  it("allows adding projects by an authenticated user", async () => {
+    await assertSucceeds(
+      app.collection("projects").add({ name: "test_project" })
+    );
+  });
 
-      await assertFails(app.collection("projects").get());
-    });
+  it("does not allow to read projects by an unauthenticated user", async () => {
+    const app = await getApplicationWith(null);
 
-    it("does not allow to add projects without a name", async () => {
-      await assertFails(app.collection("projects").add({}));
-    });
+    await assertFails(app.collection("projects").get());
+  });
+
+  it("does not allow to add projects without a name", async () => {
+    await assertFails(app.collection("projects").add({}));
   });
 
   after(async () => {
---
+++ metrics/firebase/test/firestore/rules/test_utils/test-data.js
@@ -0,0 +1,37 @@
+const firestore = require("firebase").firestore;
+
+/** A list of test projects */
+exports.projects = {
+  "projects/1": {
+    name: "project_1",
+  },
+  "projects/2": {
+    name: "project_2",
+  },
+};
+
+/** A list of test builds */
+const builds = {
+  "build/1": {
+    projectId: "1",
+    startedAt: new firestore.Timestamp.now(),
+    duration: 234,
+    url: "url1",
+  },
+  "build/2": {
+    projectId: "2",
+    startedAt: new firestore.Timestamp.now(),
+    duration: 345,
+    url: "url2",
+  },
+};
+
+exports.builds = builds;
+
+/** A firebase user needed for tests */
+exports.user = { uid: "uid" };
+
+/** Get a test build */
+exports.getBuild = function () {
+  return Object.assign({}, builds["build/1"]);
+};
