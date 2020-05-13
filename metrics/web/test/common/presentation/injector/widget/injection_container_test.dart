23591560552c2ecfee84f9558b42e985f2b3c381
vova.beloded <vova.beloded@solid.software>
2020-05-13 15:41:36 +0300

    Add tests for injection_container.dart

---
+++ metrics/web/test/common/presentation/injector/widget/injection_container_test.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:metrics/auth/presentation/state/auth_notifier.dart';
+import 'package:metrics/common/presentation/injector/widget/injection_container.dart';
+import 'package:metrics/common/presentation/metrics_theme/state/theme_notifier.dart';
+import 'package:metrics/dashboard/presentation/state/project_metrics_notifier.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  group("InjectionContainer", () {
+    testWidgets(
+      "injects the ProjectMetricsNotifier",
+      (tester) async {
+        await tester.pumpWidget(InjectionContainerTestbed());
+
+        final context = InjectionContainerTestbed.childKey.currentContext;
+
+        expect(
+          () => Provider.of<ProjectMetricsNotifier>(context, listen: false),
+          returnsNormally,
+        );
+      },
+    );
+
+    testWidgets(
+      "injects the AuthNotifier",
+      (tester) async {
+        await tester.pumpWidget(InjectionContainerTestbed());
+
+        final context = InjectionContainerTestbed.childKey.currentContext;
+
+        expect(
+          () => Provider.of<AuthNotifier>(context, listen: false),
+          returnsNormally,
+        );
+      },
+    );
+
+    testWidgets(
+      "injects the ThemeNotifier",
+      (tester) async {
+        await tester.pumpWidget(InjectionContainerTestbed());
+
+        final context = InjectionContainerTestbed.childKey.currentContext;
+
+        expect(
+          () => Provider.of<ThemeNotifier>(context, listen: false),
+          returnsNormally,
+        );
+      },
+    );
+  });
+}
+
+/// A testbed class needed to test the [InjectionContainer] widget.
+class InjectionContainerTestbed extends StatelessWidget {
+  /// A [GlobalKey] needed to get the current context of the [InjectionContainer.child].
+  static final GlobalKey childKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return InjectionContainer(
+      child: Container(
+        key: childKey,
+      ),
+    );
+  }
+}
