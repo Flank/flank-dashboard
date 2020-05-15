356c69a76775a39c636ab016e0a322d239cc17d1
solid-maxim <maxim.ustych@solid.software>
2020-05-15 16:45:40 +0300

    fix in signInWithEmailAndPassword

--- metrics/web/lib/auth/presentation/state/auth_notifier.dart
+++ metrics/web/lib/auth/presentation/state/auth_notifier.dart
@@ -84,8 +84,8 @@ class AuthNotifier extends ChangeNotifier {
         password: PasswordValueObject(password),
       ));
     } on AuthenticationException catch (exception) {
-      notifyListeners();
       _authErrorMessage = AuthErrorMessage(exception.code);
+      notifyListeners();
     }
   }
 
