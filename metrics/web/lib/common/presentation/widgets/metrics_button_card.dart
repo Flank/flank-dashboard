42b23d96d306c711954cf7c6b61f5f410023d64b
solid-maxim <maxim.ustych@solid.software>
2020-06-05 19:09:46 +0300

    fix metrics add button

--- metrics/web/lib/common/presentation/widgets/metrics_button_card.dart
+++ metrics/web/lib/common/presentation/widgets/metrics_button_card.dart
@@ -48,26 +48,22 @@ class MetricsButtonCard extends StatelessWidget {
         elevation: elevation,
         margin: margin,
         backgroundColor: backgroundColor,
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: iconPadding,
-                  child: Icon(
-                    iconData,
-                    size: iconSize,
-                    color: iconColor,
-                  ),
-                ),
-                Padding(
-                  padding: titlePadding,
-                  child: title,
-                ),
-              ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: iconPadding,
+              child: Icon(
+                iconData,
+                size: iconSize,
+                color: iconColor,
+              ),
             ),
-          ),
+            Padding(
+              padding: titlePadding,
+              child: title,
+            ),
+          ],
         ),
       ),
     );
