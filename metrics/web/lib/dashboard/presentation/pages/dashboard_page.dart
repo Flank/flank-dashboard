6d9f710fdeeeecfcdd2c97e4e7b0e6f4c4673a4a
Illia Romanenko <illia.romanenko@gmail.com>
2020-06-19 00:36:57 +0300

    fix driver tests

--- metrics/web/lib/dashboard/presentation/pages/dashboard_page.dart
+++ metrics/web/lib/dashboard/presentation/pages/dashboard_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:metrics/common/presentation/scaffold/widget/metrics_scaffold.dart';
-import 'package:metrics/common/presentation/strings/common_strings.dart';
 import 'package:metrics/dashboard/presentation/state/project_metrics_notifier.dart';
 import 'package:metrics/dashboard/presentation/widgets/metrics_table.dart';
+import 'package:metrics/dashboard/presentation/widgets/projects_search_input.dart';
 import 'package:provider/provider.dart';
 
 /// The widget that allows to quickly get primary metrics
@@ -31,13 +31,8 @@ class _DashboardPageState extends State<DashboardPage> {
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(bottom: 24.0),
-            child: TextField(
-              onChanged: _projectMetricsNotifier.filterByProjectName,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                hintText: CommonStrings.searchForProject,
-              ),
-            ),
+            child: ProjectSearchInput(
+                onChanged: _projectMetricsNotifier.filterByProjectName),
           ),
           Expanded(
             child: MetricsTable(),
