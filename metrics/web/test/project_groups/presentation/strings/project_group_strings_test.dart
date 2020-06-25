df0a2e4f42bc03ed886b82548195ea34ab95e718
solid-maxim <maxim.ustych@solid.software>
2020-06-25 08:54:40 +0300

    minor fix

--- metrics/web/test/project_groups/presentation/strings/project_group_strings_test.dart
+++ metrics/web/test/project_groups/presentation/strings/project_group_strings_test.dart
@@ -2,13 +2,13 @@ import 'package:metrics/project_groups/presentation/strings/project_groups_strin
 import 'package:test/test.dart';
 
 void main() {
-  group('ProjectGroupsStrings', () {
+  group("ProjectGroupsStrings", () {
     const count = 2;
 
     test(
       ".getDeleteTextConfirmation() returns a message that contains the given name",
       () {
-        const name = "name";
+        const name = 'name';
 
         expect(
           ProjectGroupsStrings.getDeleteTextConfirmation(name),
