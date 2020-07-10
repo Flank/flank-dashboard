8fda6a203ed8f337c341437894464039775a053e
vova-beloded <41615621+vova-beloded@users.noreply.github.com>
2020-07-10 14:12:37 +0300

    Project groups validator name and strings tests (#316)
    * add tests for project groups name validator and strings
    
    * minor fix
    
    * fix format in project group strings test
    
    * Minor test improvements
    
    * More tests for project group name validator
    
    * Delete tests for deleted classes
    
    Co-authored-by: solid-maxim <maxim.ustych@solid.software>

---
+++ metrics/web/test/project_groups/presentation/strings/project_groups_strings_test.dart
@@ -0,0 +1,40 @@
+import 'package:metrics/project_groups/presentation/strings/project_groups_strings.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("ProjectGroupsStrings", () {
+    const count = 2;
+
+    test(
+      ".getDeleteTextConfirmation() returns a message that contains the given name",
+      () {
+        const name = 'name';
+
+        expect(
+          ProjectGroupsStrings.getDeleteTextConfirmation(name),
+          contains(name),
+        );
+      },
+    );
+
+    test(
+      ".getProjectsCount() returns a message that contains the given count",
+      () {
+        expect(
+          ProjectGroupsStrings.getProjectsCount(count),
+          contains('$count'),
+        );
+      },
+    );
+
+    test(
+      ".getSelectedCount() returns a message that contains the given count",
+      () {
+        expect(
+          ProjectGroupsStrings.getSelectedCount(count),
+          contains('$count'),
+        );
+      },
+    );
+  });
+}
