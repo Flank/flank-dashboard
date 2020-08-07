a719bac8b80c258600eb7e25c08f12e35f9526a7
vladlaba <42868882+vladlaba@users.noreply.github.com>
2020-08-07 11:54:06 +0300

    Implement the base popup widget (#409)
    * implement the base popup widget
    
    * fix formatting
    
    * widget redesign and refactoring
    
    * Update documentation and tests descriptions
    
    Co-authored-by: Yurii Uvarov <yurii.uvarov@solid.software>
    Co-authored-by: Yurii Uvarov <68225012+solid-yuriiuvarov@users.noreply.github.com>

---
+++ metrics/web/lib/base/presentation/widgets/base_popup.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+/// Signature for the function that builds an offset using the [childSize].
+typedef OffsetBuilder = Offset Function(Size childSize);
+
+/// Signature for the function that builds a trigger widget
+/// using the [openPopup] and the [closePopup] callbacks.
+typedef TriggerBuilder = Widget Function(
+  BuildContext context,
+  VoidCallback openPopup,
+  VoidCallback closePopup,
+);
+
+/// A widget that displays the trigger widget built by the [triggerBuilder]
+/// that opens the given [popup] when the trigger widget is activated.
+class BasePopup extends StatefulWidget {
+  /// A [RouteObserver] to subscribe to the route callbacks.
+  final RouteObserver routeObserver;
+
+  /// An additional constraints to apply to the [popup].
+  final BoxConstraints popupConstraints;
+
+  /// A callback that is called to build the [popup] offset from
+  /// the trigger widget.
+  final OffsetBuilder offsetBuilder;
+
+  /// A callback that is called to build the trigger widget.
+  final TriggerBuilder triggerBuilder;
+
+  /// A widget to display when the trigger widget is triggered.
+  final Widget popup;
+
+  /// Creates a new instance of the base popup.
+  ///
+  /// The [barrierDismissible] defaults to `true`.
+  /// If the [popupConstraints] is null, an empty instance of
+  /// the [BoxConstraints] is used.
+  ///
+  /// All the required parameters must not be null.
+  const BasePopup({
+    Key key,
+    BoxConstraints popupConstraints,
+    @required this.offsetBuilder,
+    @required this.triggerBuilder,
+    @required this.popup,
+    this.routeObserver,
+  })  : popupConstraints = popupConstraints ?? const BoxConstraints(),
+        assert(offsetBuilder != null),
+        assert(triggerBuilder != null),
+        assert(popup != null),
+        super(key: key);
+
+  @override
+  _BasePopupState createState() => _BasePopupState();
+}
+
+class _BasePopupState extends State<BasePopup> with RouteAware {
+  /// A [OverlayEntry] where used to display a [BasePopup.popup].
+  OverlayEntry _overlayEntry;
+
+  /// The [LayerLink] that allows a [BasePopup.popup] to follow the trigger.
+  final _layerLink = LayerLink();
+
+  @override
+  void didChangeDependencies() {
+    widget.routeObserver?.subscribe(this, ModalRoute.of(context));
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CompositedTransformTarget(
+      link: _layerLink,
+      child: widget.triggerBuilder(
+        context,
+        _openPopup,
+        _closePopup,
+      ),
+    );
+  }
+
+  /// Opens a [BasePopup.popup].
+  void _openPopup() {
+    final childBox = context.findRenderObject() as RenderBox;
+    final childSize = childBox.size;
+    final offset = widget.offsetBuilder(childSize);
+
+    final _widget = GestureDetector(
+      onTap: _closePopup,
+      child: Container(
+        color: Colors.transparent,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: offset.dx,
+              top: offset.dy,
+              child: CompositedTransformFollower(
+                link: _layerLink,
+                offset: offset,
+                child: ConstrainedBox(
+                  constraints: widget.popupConstraints,
+                  child: widget.popup,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    _overlayEntry = OverlayEntry(builder: (context) => _widget);
+    Overlay.of(context).insert(_overlayEntry);
+  }
+
+  @override
+  void didPop() {
+    _closePopup();
+    super.didPop();
+  }
+
+  @override
+  void didPushNext() {
+    _closePopup();
+    super.didPushNext();
+  }
+
+  /// Closes a [BasePopup.popup].
+  void _closePopup() {
+    if (_overlayEntry != null) {
+      _overlayEntry.remove();
+      _overlayEntry = null;
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.routeObserver?.unsubscribe(this);
+    _closePopup();
+    super.dispose();
+  }
+}
---
+++ metrics/web/test/base/presentation/widgets/base_popup_test.dart
@@ -0,0 +1,296 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:metrics/base/presentation/widgets/base_popup.dart';
+
+void main() {
+  group("BasePopup", () {
+    const testTriggerWidget = Text('trigger widget');
+    const testPopupWidget = Text('popup widget');
+    final triggerWidgetFinder = find.byWidget(testTriggerWidget);
+    final popupWidgetFinder = find.byWidget(testPopupWidget);
+
+    Widget _defaultTriggerBuilder(
+      BuildContext context,
+      VoidCallback openPopup,
+      VoidCallback closePopup,
+    ) {
+      return GestureDetector(
+        onTap: openPopup,
+        child: testTriggerWidget,
+      );
+    }
+
+    void pushDefaultRoute(GlobalKey<NavigatorState> key) {
+      key.currentState.push(MaterialPageRoute(
+        builder: (context) => const Scaffold(),
+      ));
+    }
+
+    testWidgets(
+      "throws an AssertionError if the given trigger builder is null",
+      (tester) async {
+        await tester.pumpWidget(_BasePopupTestbed(triggerBuilder: null));
+
+        expect(tester.takeException(), isAssertionError);
+      },
+    );
+
+    testWidgets(
+      "throws an AssertionError if the given popup widget is null",
+      (tester) async {
+        await tester.pumpWidget(_BasePopupTestbed(
+          popup: null,
+          triggerBuilder: _defaultTriggerBuilder,
+        ));
+
+        expect(tester.takeException(), isAssertionError);
+      },
+    );
+
+    testWidgets(
+      "throws an AssertionError if the given offset builder is null",
+      (tester) async {
+        await tester.pumpWidget(_BasePopupTestbed(
+          offsetBuilder: null,
+          triggerBuilder: _defaultTriggerBuilder,
+        ));
+
+        expect(tester.takeException(), isAssertionError);
+      },
+    );
+
+    testWidgets(
+      "displays the trigger widget built by the given trigger builder",
+      (tester) async {
+        await tester.pumpWidget(
+          _BasePopupTestbed(triggerBuilder: _defaultTriggerBuilder),
+        );
+
+        expect(triggerWidgetFinder, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "displays the given popup widget when the trigger widget is tapped",
+      (tester) async {
+        await tester.pumpWidget(_BasePopupTestbed(
+          popup: testPopupWidget,
+          triggerBuilder: _defaultTriggerBuilder,
+        ));
+
+        await tester.tap(triggerWidgetFinder);
+        await tester.pumpAndSettle();
+
+        expect(popupWidgetFinder, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "applies the offset built by the given offset builder to the popup",
+      (tester) async {
+        const offset = Offset(20.0, 20.0);
+
+        await tester.pumpWidget(_BasePopupTestbed(
+          offsetBuilder: (_) => offset,
+          triggerBuilder: _defaultTriggerBuilder,
+        ));
+
+        await tester.tap(triggerWidgetFinder);
+        await tester.pumpAndSettle();
+
+        final followerWidget = tester.widget<CompositedTransformFollower>(
+          find.byType(CompositedTransformFollower),
+        );
+
+        expect(followerWidget.offset, equals(offset));
+      },
+    );
+
+    testWidgets(
+      "applies the given popup constraints to the popup widget",
+      (WidgetTester tester) async {
+        const boxConstraints = BoxConstraints(maxWidth: 50.0, maxHeight: 50.0);
+
+        await tester.pumpWidget(
+          _BasePopupTestbed(
+            popupConstraints: boxConstraints,
+            popup: testPopupWidget,
+            triggerBuilder: _defaultTriggerBuilder,
+          ),
+        );
+
+        await tester.tap(triggerWidgetFinder);
+        await tester.pumpAndSettle();
+
+        final constrainedBox = tester.widget<ConstrainedBox>(find.ancestor(
+          of: popupWidgetFinder,
+          matching: find.byType(ConstrainedBox),
+        ));
+        final actualBoxConstraints = constrainedBox.constraints;
+
+        expect(actualBoxConstraints, equals(boxConstraints));
+      },
+    );
+
+    testWidgets(
+      "applies the default box constraints to the popup widget if the given popup constraints is null",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          _BasePopupTestbed(
+            popupConstraints: null,
+            popup: testPopupWidget,
+            triggerBuilder: _defaultTriggerBuilder,
+          ),
+        );
+
+        await tester.tap(triggerWidgetFinder);
+        await tester.pumpAndSettle();
+
+        final constrainedBox = tester.widget<ConstrainedBox>(find.ancestor(
+          of: popupWidgetFinder,
+          matching: find.byType(ConstrainedBox),
+        ));
+        final actualBoxConstraints = constrainedBox.constraints;
+
+        expect(actualBoxConstraints, isNotNull);
+      },
+    );
+
+    testWidgets(
+      "closes a popup after tap outside of the popup content",
+      (tester) async {
+        const defaultSize = 20.0;
+
+        await tester.pumpWidget(_BasePopupTestbed(
+          popupConstraints: const BoxConstraints(maxHeight: defaultSize),
+          popup: testPopupWidget,
+          triggerBuilder: _defaultTriggerBuilder,
+        ));
+
+        await tester.tap(triggerWidgetFinder);
+        await tester.pumpAndSettle();
+        await tester.tapAt(const Offset(defaultSize, defaultSize));
+        await tester.pumpAndSettle();
+
+        expect(popupWidgetFinder, findsNothing);
+      },
+    );
+
+    testWidgets(
+      "closes a popup after pushing a new route",
+      (tester) async {
+        final _key = GlobalKey<NavigatorState>();
+
+        await tester.pumpWidget(_BasePopupTestbed(
+          navigatorKey: _key,
+          popup: testPopupWidget,
+          triggerBuilder: _defaultTriggerBuilder,
+        ));
+
+        await tester.tap(triggerWidgetFinder);
+        await tester.pumpAndSettle();
+        pushDefaultRoute(_key);
+        await tester.pumpAndSettle();
+
+        expect(popupWidgetFinder, findsNothing);
+      },
+    );
+
+    testWidgets(
+      "closes a popup after popping the current route",
+      (tester) async {
+        final _key = GlobalKey<NavigatorState>();
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Scaffold(
+              body: Builder(
+                builder: (context) => GestureDetector(onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (contetx) => _BasePopupTestbed(
+                        navigatorKey: _key,
+                        popup: testPopupWidget,
+                        triggerBuilder: _defaultTriggerBuilder,
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+        );
+
+        await tester.tap(find.byType(GestureDetector));
+        await tester.pumpAndSettle();
+        await tester.tap(triggerWidgetFinder);
+        await tester.pumpAndSettle();
+        _key.currentState.pop();
+        await tester.pumpAndSettle();
+
+        expect(popupWidgetFinder, findsNothing);
+      },
+    );
+  });
+}
+
+/// A testbed class required to test the [BasePopup] widget.
+class _BasePopupTestbed extends StatelessWidget {
+  /// Builds the default offset.
+  static Offset _defaultOffsetBuilder(Size size) => Offset.zero;
+
+  /// A key to apply to the [Navigator].
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  /// A [RouteObserver] to subscribe to the route callbacks.
+  final RouteObserver routeObserver;
+
+  /// A widget to display when a trigger widget is activated.
+  final Widget popup;
+
+  /// An additional constraints to apply to the [popup].
+  final BoxConstraints popupConstraints;
+
+  /// A callback that is called to build the [popup] offset from
+  /// the trigger widget.
+  final OffsetBuilder offsetBuilder;
+
+  /// A callback that is called to build the trigger widget.
+  final TriggerBuilder triggerBuilder;
+
+  /// Creates the a new base popup testbed.
+  ///
+  /// The [popup] defaults to the [SizedBox] with the empty constructor.
+  /// The [popupConstraints] defaults to an empty [BoxConstraints] instance.
+  /// The [offsetBuilder] defaults to the [_defaultOffsetBuilder].
+  /// If the given [navigatorKey], the default [GlobalKey] instance is used.
+  /// If the given [routeObserver], the default [RouteObserver] instance is used.
+  _BasePopupTestbed({
+    Key key,
+    GlobalKey<NavigatorState> navigatorKey,
+    RouteObserver routeObserver,
+    this.popup = const SizedBox(),
+    this.popupConstraints = const BoxConstraints(),
+    this.offsetBuilder = _defaultOffsetBuilder,
+    this.triggerBuilder,
+  })  : navigatorKey = navigatorKey ?? GlobalKey<NavigatorState>(),
+        routeObserver = routeObserver ?? RouteObserver(),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      navigatorObservers: [routeObserver],
+      home: Scaffold(
+        body: BasePopup(
+          routeObserver: routeObserver,
+          triggerBuilder: triggerBuilder,
+          popupConstraints: popupConstraints,
+          offsetBuilder: offsetBuilder,
+          popup: popup,
+        ),
+      ),
+    );
+  }
+}
