f34c9c918d422a3570a000f489ca20afbc01c212
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2020-08-17 14:31:07 +0300

    Fixed appbar Metrics Logo resizing bug, aligned Metrics Logo with pixel-perfect

--- metrics/web/lib/common/presentation/app_bar/widget/metrics_app_bar.dart
+++ metrics/web/lib/common/presentation/app_bar/widget/metrics_app_bar.dart
@@ -27,9 +27,7 @@ class MetricsAppBar extends StatelessWidget {
                 onTap: () => _navigateHome(context),
                 child: Image.network(
                   'icons/logo-metrics.svg',
-                  width: 130.0,
                   height: 32.0,
-                  fit: BoxFit.contain,
                 ),
               ),
             ),
