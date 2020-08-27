1322e47550ee1679cb986a9843f0473ee8d3270b
dmytroSolidSoftware <dmytro.koniaiev@solid.software>
2020-08-27 11:48:29 +0300

    Add the error color to the light theme.

--- metrics/web/lib/common/presentation/metrics_theme/model/light_metrics_theme_data.dart
+++ metrics/web/lib/common/presentation/metrics_theme/model/light_metrics_theme_data.dart
@@ -122,7 +122,7 @@ class LightMetricsThemeData extends MetricsThemeData {
               fontWeight: FontWeight.w500,
             ),
             counterTextStyle: TextStyleConfig.captionTextStyle,
-            errorTextStyle: TextStyle(color: Colors.red),
+            errorTextStyle: TextStyle(color: ColorConfig.accentColor),
           ),
           inactiveWidgetTheme: const MetricWidgetThemeData(
             primaryColor: _inactiveColor,
