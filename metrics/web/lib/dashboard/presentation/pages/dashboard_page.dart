c95164a205c1519befea745a4869d51e67748a8d
solid-daniyilyevtyushkin <69351065+solid-daniyilyevtyushkin@users.noreply.github.com>
2020-09-03 13:19:22 +0300

    feat: Show a MetricsToast on each user action/error on the dashboard page (#495)
    * Implemented showing toast on project error in Dashboard Page, added test
    
    * Fix tests, fix documentation
    
    * add subscribeToProjectErrors, update driver version
    
    * adjust formatting for readability
    
    Co-authored-by: Illia Romanenko <illia.romanenko@gmail.com>

--- metrics/web/lib/dashboard/presentation/pages/dashboard_page.dart
+++ metrics/web/lib/dashboard/presentation/pages/dashboard_page.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:metrics/common/presentation/scaffold/widget/metrics_scaffold.dart';
+import 'package:metrics/common/presentation/toast/widgets/negative_toast.dart';
+import 'package:metrics/common/presentation/toast/widgets/toast.dart';
+import 'package:metrics/dashboard/presentation/state/project_metrics_notifier.dart';
 import 'package:metrics/dashboard/presentation/widgets/metrics_table.dart';
 import 'package:metrics/dashboard/presentation/widgets/project_groups_dropdown_menu.dart';
 import 'package:metrics/dashboard/presentation/widgets/project_metrics_search_input.dart';
+import 'package:provider/provider.dart';
 
 /// The widget that allows to quickly get primary metrics
 /// of all available projects.
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  /// A [ProjectMetricsNotifier] needed to remove added listeners
+  /// in the [dispose] method.
+  ProjectMetricsNotifier _projectMetricsNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscribeToProjectsErrors();
+  }
+
+  /// Subscribes to projects errors.
+  void _subscribeToProjectsErrors() {
+    _projectMetricsNotifier =
+        Provider.of<ProjectMetricsNotifier>(context, listen: false);
+
+    _projectMetricsNotifier.addListener(_projectsErrorListener);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MetricsScaffold(
@@ -32,5 +59,24 @@ class DashboardPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  /// Shows the [NegativeToast] with an error message
+  /// if projects error is not null.
+  void _projectsErrorListener() {
+    final errorMessage = _projectMetricsNotifier.projectsErrorMessage;
+
+    if (errorMessage != null) {
+      showToast(
+        context,
+        NegativeToast(message: errorMessage),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _projectMetricsNotifier.removeListener(_projectsErrorListener);
+    super.dispose();
   }
 }
--- metrics/web/test/dashboard/presentation/page/dashboard_page_test.dart
+++ metrics/web/test/dashboard/presentation/page/dashboard_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:metrics/auth/presentation/state/auth_notifier.dart';
 import 'package:metrics/common/presentation/app_bar/widget/metrics_app_bar.dart';
@@ -8,6 +9,7 @@ import 'package:metrics/common/presentation/metrics_theme/state/theme_notifier.d
 import 'package:metrics/common/presentation/metrics_theme/widgets/metrics_theme_builder.dart';
 import 'package:metrics/common/presentation/routes/route_generator.dart';
 import 'package:metrics/common/presentation/strings/common_strings.dart';
+import 'package:metrics/common/presentation/toast/widgets/negative_toast.dart';
 import 'package:metrics/dashboard/presentation/pages/dashboard_page.dart';
 import 'package:metrics/dashboard/presentation/state/project_metrics_notifier.dart';
 import 'package:metrics/dashboard/presentation/strings/dashboard_strings.dart';
@@ -15,13 +17,25 @@ import 'package:metrics/dashboard/presentation/widgets/build_number_scorecard.da
 import 'package:metrics/dashboard/presentation/widgets/metrics_table.dart';
 import 'package:metrics/dashboard/presentation/widgets/project_groups_dropdown_menu.dart';
 import 'package:metrics/dashboard/presentation/widgets/project_metrics_search_input.dart';
+import 'package:mockito/mockito.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 import 'package:provider/provider.dart';
 
+import '../../../test_utils/project_metrics_notifier_mock.dart';
 import '../../../test_utils/test_injection_container.dart';
 
 void main() {
   group("DashboardPage", () {
+    ProjectMetricsNotifier projectMetricsNotifier;
+
+    setUp(() {
+      projectMetricsNotifier = ProjectMetricsNotifierMock();
+    });
+
+    tearDown(() {
+      reset(projectMetricsNotifier);
+    });
+
     testWidgets(
       "contains the MetricsAppBar widget",
       (WidgetTester tester) async {
@@ -82,6 +96,33 @@ void main() {
         await tester.pumpAndSettle();
 
         expect(find.byType(MetricsDrawer), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "displays the negative toast if there is a projects error message",
+      (WidgetTester tester) async {
+        const errorMessage = "some error message";
+
+        when(projectMetricsNotifier.isMetricsLoading).thenReturn(false);
+
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(_DashboardTestbed(
+            metricsNotifier: projectMetricsNotifier,
+          ));
+        });
+
+        when(projectMetricsNotifier.projectsErrorMessage)
+            .thenReturn(errorMessage);
+
+        projectMetricsNotifier.notifyListeners();
+
+        await tester.pump();
+
+        expect(
+            find.widgetWithText(NegativeToast, errorMessage), findsOneWidget);
+
+        ToastManager().dismissAll();
       },
     );
 
