5beacf676a6df25f76a0fd112694daf376fc1ce4
dmytroSolidSoftware <dmytro.koniaiev@solid.software>
2020-09-08 14:55:34 +0300

    [WIP] Implement the metrics text style.

---
+++ metrics/web/lib/common/presentation/widgets/metrics_text_style.dart
@@ -0,0 +1,71 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+/// The metrics [TextStyle], that calculates the [TextStyle.height] using
+/// the [lineHeightInPixels].
+class MetricsTextStyle extends TextStyle {
+  /// The height of the text in pixels.
+  final double lineHeightInPixels;
+
+  /// Creates a new instance of the [MetricsTextStyle].
+  ///
+  /// If the [fontSize] and [lineHeightInPixels] are not null,
+  /// then the [TextStyle.height] equal to the [lineHeightInPixels]
+  /// divided by the [fontSize].
+  /// Otherwise the [TextStyle.height] equal to the [height].
+  ///
+  /// The [inherit] defaults to `true`.
+  const MetricsTextStyle({
+    this.lineHeightInPixels,
+    bool inherit = true,
+    Color color,
+    Color backgroundColor,
+    double fontSize,
+    FontWeight fontWeight,
+    FontStyle fontStyle,
+    double letterSpacing,
+    double wordSpacing,
+    TextBaseline textBaseline,
+    double height,
+    Locale locale,
+    Paint foreground,
+    Paint background,
+    List<Shadow> shadows,
+    List<FontFeature> fontFeatures,
+    TextDecoration decoration,
+    Color decorationColor,
+    TextDecorationStyle decorationStyle,
+    double decorationThickness,
+    String debugLabel,
+    String fontFamily,
+    List<String> fontFamilyFallback,
+    String package,
+  }) : super(
+          inherit: inherit,
+          color: color,
+          backgroundColor: backgroundColor,
+          fontSize: fontSize,
+          fontWeight: fontWeight,
+          fontStyle: fontStyle,
+          letterSpacing: letterSpacing,
+          wordSpacing: wordSpacing,
+          textBaseline: textBaseline,
+          height: lineHeightInPixels != null && fontSize != null
+              ? lineHeightInPixels / fontSize
+              : height,
+          locale: locale,
+          foreground: foreground,
+          background: background,
+          shadows: shadows,
+          fontFeatures: fontFeatures,
+          decoration: decoration,
+          decorationColor: decorationColor,
+          decorationStyle: decorationStyle,
+          decorationThickness: decorationThickness,
+          debugLabel: debugLabel,
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+          package: package,
+        );
+}
