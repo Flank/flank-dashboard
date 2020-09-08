57045bdd0e1cdde7e26ec6cd3e5c0b3138830914
Tetiana Movlian <tetianamovlian@Tetianas-MacBook-Pro.local>
2020-09-08 14:54:55 +0300

    Fix Safari issue

--- metrics/web/lib/main.dart
+++ metrics/web/lib/main.dart
@@ -10,7 +10,6 @@ import 'package:metrics/common/presentation/metrics_theme/widgets/metrics_theme_
 import 'package:metrics/common/presentation/routes/observers/toast_route_observer.dart';
 import 'package:metrics/common/presentation/routes/route_generator.dart';
 import 'package:metrics/common/presentation/strings/common_strings.dart';
-import 'package:metrics/util/favicon.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MetricsApp());
@@ -25,11 +24,8 @@ class _MetricsAppState extends State<MetricsApp> {
   /// when the page route changes.
   final _toastRouteObserver = ToastRouteObserver();
 
-  final _favicon = Favicon();
-
   @override
   Widget build(BuildContext context) {
-    _favicon.setupFavicon();
     return InjectionContainer(
       child: MetricsThemeBuilder(
         builder: (context, themeNotifier) {
