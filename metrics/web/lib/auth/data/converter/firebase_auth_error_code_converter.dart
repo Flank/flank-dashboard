85f1838740a5a92fa167d8ad94015f7494c4b426
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2020-09-10 14:49:23 +0300

    Refactor FirebaseAuthError code

---
+++ metrics/web/lib/auth/data/converter/firebase_auth_error_code_converter.dart
@@ -0,0 +1,30 @@
+import 'package:metrics/auth/data/model/firebase_auth_error_code.dart';
+import 'package:metrics/auth/domain/entities/auth_error_code.dart';
+
+/// A class that converts the [errorCode] to the corresponding [AuthErrorCode].
+class FirebaseAuthErrorCodeConverter {
+  /// Returns the corresponding [AuthErrorCode] to the given Firebase error code.
+  /// If the corresponding [AuthErrorCode] is not found,
+  /// returns [AuthErrorCode.unknown].
+  static AuthErrorCode convertErrorStringToErrorCode(String errorCode) {
+    switch (errorCode) {
+      case FirebaseAuthErrorCode.userNotFound:
+        return AuthErrorCode.userNotFound;
+
+      case FirebaseAuthErrorCode.wrongPassword:
+        return AuthErrorCode.wrongPassword;
+
+      case FirebaseAuthErrorCode.invalidEmail:
+        return AuthErrorCode.invalidEmail;
+
+      case FirebaseAuthErrorCode.tooManyRequests:
+        return AuthErrorCode.tooManyRequests;
+
+      case FirebaseAuthErrorCode.userDisabled:
+        return AuthErrorCode.userDisabled;
+
+      default:
+        return AuthErrorCode.unknown;
+    }
+  }
+}
--- metrics/web/lib/auth/data/repositories/firebase_user_repository.dart
+++ metrics/web/lib/auth/data/repositories/firebase_user_repository.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:metrics/auth/data/adapter/firebase_user_adapter.dart';
+import 'package:metrics/auth/data/converter/firebase_auth_error_code_converter.dart';
 import 'package:metrics/auth/domain/entities/auth_error_code.dart';
 import 'package:metrics/auth/domain/entities/authentication_exception.dart';
-import 'package:metrics/auth/domain/entities/firebase_auth_error_code.dart';
 import 'package:metrics/auth/domain/entities/user.dart';
 import 'package:metrics/auth/domain/repositories/user_repository.dart';
 
@@ -27,8 +27,9 @@ class FirebaseUserRepository implements UserRepository {
       );
     } catch (error) {
       final errorCode = error.code as String;
-      final firebaseAuthErrorCode = FirebaseAuthErrorCode(errorCode);
-      final authErrorCode = firebaseAuthErrorCode.toAuthErrorCode();
+      final authErrorCode =
+          FirebaseAuthErrorCodeConverter.convertErrorStringToErrorCode(
+              errorCode);
 
       throw AuthenticationException(code: authErrorCode);
     }
@@ -57,8 +58,9 @@ class FirebaseUserRepository implements UserRepository {
       }
 
       final errorCode = error.code as String;
-      final firebaseAuthErrorCode = FirebaseAuthErrorCode(errorCode);
-      final authErrorCode = firebaseAuthErrorCode.toAuthErrorCode();
+      final authErrorCode =
+          FirebaseAuthErrorCodeConverter.convertErrorStringToErrorCode(
+              errorCode);
 
       throw AuthenticationException(code: authErrorCode);
     }
