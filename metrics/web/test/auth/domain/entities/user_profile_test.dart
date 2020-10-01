d9a058b9642f2f722471e1626b918bbc4b99b16c
solid-vovabeloded <41615621+solid-vovabeloded@users.noreply.github.com>
2020-10-01 12:36:10 +0300

    feat: User profile domain layer (#636)
    * Implement user profile domain layer
    
    * Typo fixes and formatting improvements
    
    * Fix tests
    
    * Documentation improvements
    
    * Minor test improvement
    
    * Minor test improvements

---
+++ metrics/web/test/auth/domain/entities/user_profile_test.dart
@@ -0,0 +1,31 @@
+import 'package:metrics/auth/domain/entities/theme_type.dart';
+import 'package:metrics/auth/domain/entities/user_profile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("UserProfile", () {
+    const selectedTheme = ThemeType.dark;
+    const id = 'id';
+
+    test("throws an ArgumentError if the given id is null", () {
+      expect(
+        () => UserProfile(id: null, selectedTheme: selectedTheme),
+        throwsArgumentError,
+      );
+    });
+
+    test("throws an ArgumentError if the given selected theme is null", () {
+      expect(
+        () => UserProfile(id: id, selectedTheme: null),
+        throwsArgumentError,
+      );
+    });
+
+    test("successfully creates a new instance with the given parameters", () {
+      final profile = UserProfile(id: id, selectedTheme: selectedTheme);
+
+      expect(profile.id, equals(id));
+      expect(profile.selectedTheme, equals(selectedTheme));
+    });
+  });
+}
