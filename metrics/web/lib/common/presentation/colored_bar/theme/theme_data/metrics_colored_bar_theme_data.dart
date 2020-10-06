6744b1f87b116b3f470e4b571871f7fad3a2cc57
Vlad Laba <vladyslav.laba@solid.software>
2020-10-06 15:39:11 +0300

    graph indicator and colored bar changes

---
+++ metrics/web/lib/common/presentation/colored_bar/theme/theme_data/metrics_colored_bar_theme_data.dart
@@ -0,0 +1,14 @@
+import 'package:metrics/common/presentation/colored_bar/theme/attention_level/metrics_colored_bar_attention_level.dart';
+import 'package:metrics/common/presentation/metrics_theme/model/attention_level_theme_data.dart';
+
+/// A class that stores the theme data for the application colored bars.
+class MetricsColoredBarThemeData
+    extends AttentionLevelThemeData<MetricsColoredBarAttentionLevel> {
+  /// Creates a new instance of the [MetricsColoredBarThemeData].
+  ///
+  /// If the [attentionLevel] is `null`, an empty
+  /// [MetricsColoredBarAttentionLevel] instance is used.
+  const MetricsColoredBarThemeData({
+    MetricsColoredBarAttentionLevel attentionLevel,
+  }) : super(attentionLevel ?? const MetricsColoredBarAttentionLevel());
+}
---
+++ metrics/web/lib/common/presentation/graph_indicator/widgets/positive_graph_indicator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:metrics/common/presentation/graph_indicator/theme/attention_level/graph_indicator_attention_level.dart';
+import 'package:metrics/common/presentation/graph_indicator/theme/style/graph_indicator_style.dart';
+import 'package:metrics/common/presentation/graph_indicator/theme/theme_data/graph_indicator_theme_data.dart';
+import 'package:metrics/common/presentation/graph_indicator/widgets/graph_indicator.dart';
+
+/// A [GraphIndicator] widget that applies
+/// the [GraphIndicatorAttentionLevel.positive] indicator style
+/// from the [GraphIndicatorThemeData].
+class PositiveGraphIndicator extends GraphIndicator {
+  /// Creates a new instance of the [PositiveGraphIndicator].
+  const PositiveGraphIndicator({Key key}) : super(key: key);
+
+  @override
+  GraphIndicatorStyle selectStyle(GraphIndicatorAttentionLevel attentionLevel) {
+    return attentionLevel.positive;
+  }
+}
