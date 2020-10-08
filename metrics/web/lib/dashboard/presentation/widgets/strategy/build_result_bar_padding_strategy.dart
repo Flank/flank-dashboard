11fa891554d59aeb2115592f4924c90c080cb198
Dima Koniaiev <52494380+solid-dimakoniaiev@users.noreply.github.com>
2020-10-08 21:00:32 +0300

    feat: Redesign the dashboard popup. (#686)
    * Redesign the dashboard popup.
    
    * general improvements and add common tests
    
    * remove empty test case
    
    Co-authored-by: dmytroSolidSoftware <dmytroSolidSoftware>
    Co-authored-by: Vlad Laba <vladyslav.laba@solid.software>

---
+++ metrics/web/lib/dashboard/presentation/widgets/strategy/build_result_bar_padding_strategy.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:metrics/dashboard/presentation/view_models/build_result_view_model.dart';
+import 'package:metrics/dashboard/presentation/widgets/build_result_bar.dart';
+
+/// A class that represents the strategy of applying the [EdgeInsets]
+/// to the [BuildResultBar] based on the [BuildResultViewModel] position.
+class BuildResultBarPaddingStrategy {
+  /// A list of [BuildResultViewModel]s.
+  final List<BuildResultViewModel> buildResults;
+
+  /// Creates a new instance of the [BuildResultBarPaddingStrategy].
+  ///
+  /// The [buildResults] default value is an empty [List].
+  const BuildResultBarPaddingStrategy({
+    this.buildResults = const [],
+  });
+
+  /// Provides the [EdgeInsets] based on the [buildResult] position
+  /// among the [buildResults].
+  EdgeInsets getBarPadding(BuildResultViewModel buildResult) {
+    final index = buildResults.indexOf(buildResult);
+
+    if (index == buildResults.length - 1) {
+      return const EdgeInsets.only(left: 2.0);
+    }
+
+    if (index == 0) {
+      return const EdgeInsets.only(right: 2.0);
+    }
+
+    return const EdgeInsets.symmetric(horizontal: 2.0);
+  }
+}
---
+++ metrics/web/test/dashboard/presentation/widgets/strategy/build_result_bar_padding_strategy_test.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:metrics/dashboard/presentation/view_models/build_result_popup_view_model.dart';
+import 'package:metrics/dashboard/presentation/view_models/build_result_view_model.dart';
+import 'package:metrics/dashboard/presentation/widgets/strategy/build_result_bar_padding_strategy.dart';
+import 'package:metrics_core/metrics_core.dart';
+import 'package:test/test.dart';
+
+// https://github.com/software-platform/monorepo/issues/140
+// ignore_for_file: prefer_const_constructors
+
+void main() {
+  group("BuildResultBarPaddingStrategy", () {
+    final popupViewModel = BuildResultPopupViewModel(
+      duration: Duration(),
+      date: DateTime.now(),
+    );
+    final buildResults = [
+      BuildResultViewModel(
+        buildResultPopupViewModel: popupViewModel,
+        buildStatus: BuildStatus.successful,
+      ),
+      BuildResultViewModel(
+        buildResultPopupViewModel: popupViewModel,
+        buildStatus: BuildStatus.cancelled,
+      ),
+      BuildResultViewModel(
+        buildResultPopupViewModel: popupViewModel,
+        buildStatus: BuildStatus.failed,
+      ),
+    ];
+
+    final strategy = BuildResultBarPaddingStrategy(
+      buildResults: buildResults,
+    );
+
+    test(
+      ".getBarPadding() returns an edge insets only with right value if the given build result is the first in the list",
+      () {
+        const expectedInsets = EdgeInsets.only(right: 2.0);
+        final buildResult = buildResults.first;
+
+        final edgeInsets = strategy.getBarPadding(buildResult);
+
+        expect(edgeInsets, equals(expectedInsets));
+      },
+    );
+
+    test(
+      ".getBarPadding() returns an edge insets with right and left value if the given build result is not the first and last in the list",
+      () {
+        const expectedInsets = EdgeInsets.symmetric(horizontal: 2.0);
+        final buildResult = buildResults[1];
+
+        final edgeInsets = strategy.getBarPadding(buildResult);
+
+        expect(edgeInsets, equals(expectedInsets));
+      },
+    );
+
+    test(
+      ".getBarPadding() returns an edge insets only with left only if the given build result if the first in list",
+      () {
+        const expectedInsets = EdgeInsets.only(left: 2.0);
+        final buildResult = buildResults.last;
+
+        final edgeInsets = strategy.getBarPadding(buildResult);
+
+        expect(edgeInsets, equals(expectedInsets));
+      },
+    );
+
+    test(
+      ".getBarPadding() returns an edge insets only with left value if there is only one build result",
+      () {
+        const expectedInsets = EdgeInsets.only(left: 2.0);
+        final buildResult = BuildResultViewModel(
+          buildResultPopupViewModel: popupViewModel,
+          buildStatus: BuildStatus.cancelled,
+        );
+        final strategy = BuildResultBarPaddingStrategy(
+          buildResults: [buildResult],
+        );
+
+        final edgeInsets = strategy.getBarPadding(buildResult);
+
+        expect(edgeInsets, equals(expectedInsets));
+      },
+    );
+  });
+}
