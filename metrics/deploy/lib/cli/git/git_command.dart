3dd9b53a38794e8f68e92d00eb1a2cb382829df4
radekdymacz <radekdymacz@gmail.com>
2020-10-15 11:01:03 +0200

    Update metrics/deploy/lib/cli/git/git_command.dart
    Co-authored-by: solid-vovabeloded <41615621+solid-vovabeloded@users.noreply.github.com>

--- metrics/deploy/lib/cli/git/git_command.dart
+++ metrics/deploy/lib/cli/git/git_command.dart
@@ -2,7 +2,7 @@ import 'package:process_run/process_run.dart' as cmd;
 
 /// class wrapping up gcloud CLI
 class GitCommand {
-  /// Git clone command.
+  /// Clones the git repository from the given [repoURL] to the given [srcPath].
   Future<void> clone(String repoURL, String srcPath) async {
     await cmd.run('git', ['clone', repoURL, srcPath], verbose: true);
   }
