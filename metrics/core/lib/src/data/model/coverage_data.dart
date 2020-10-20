b826b0b5e681abf4a589093fc529cafcae0f6472
Tetiana Movlian <tetianamovlian@Tetianas-MacBook-Pro.local>
2020-10-20 13:45:06 +0300

    Fix CoverageData class documentation

--- metrics/core/lib/src/data/model/coverage_data.dart
+++ metrics/core/lib/src/data/model/coverage_data.dart
@@ -2,7 +2,7 @@ import 'package:meta/meta.dart';
 import 'package:metrics_core/metrics_core.dart';
 import 'package:metrics_core/src/domain/entities/coverage.dart';
 
-/// [DataModel] that represents the [Coverage] entity.
+/// A [DataModel] that represents the [Coverage] entity.
 class CoverageData extends Coverage implements DataModel {
   /// Creates a new instance of the [CoverageData] with the given [percent].
   CoverageData({@required Percent percent}) : super(percent: percent);
