5fb409fece05edd1c2a6af30d63c87508ef70ad7
Tetiana Movlian <tetianamovlian@Tetianas-MacBook-Pro.local>
2020-10-20 17:58:25 +0300

    Fix invalid file format string

--- metrics/coverage_converter/lib/common/exception/coverage_converter_error_strings.dart
+++ metrics/coverage_converter/lib/common/exception/coverage_converter_error_strings.dart
@@ -5,5 +5,5 @@ class CoverageConverterErrorStrings {
   static const String noSuchFile = 'The input file was not found';
   static const String fileIsEmpty = 'The input file is empty';
   static const String invalidFileFormat =
-      'The input file\'s format is not supported';
+      "The input file's format is not valid";
 }
