21ad262a4847e7d5d58d3f86e4fda10a5bbd42cc
Vladyslav Laba <70939496+solid-vladlaba@users.noreply.github.com>
2020-10-21 18:35:38 +0300

    fix: Change Github Actions config (#732)
    * change github configuration according to the design changes
    
    * fix typo
    
    * add missing checks to the test case

--- metrics/ci_integrations/test/source/github_actions/test_utils/test_data/github_actions_config_test_data.dart
+++ metrics/ci_integrations/test/source/github_actions/test_utils/test_data/github_actions_config_test_data.dart
@@ -3,30 +3,40 @@ import 'package:ci_integration/source/github_actions/config/model/github_actions
 /// A class containing a test data for the [GithubActionsSourceConfig].
 class GithubActionsConfigTestData {
   /// A unique identifier for the workflow to use in tests.
-  static const workflowIdentifier = 'workflow';
+  static const String workflowIdentifier = 'workflow';
 
   /// An owner of the project's repository to use in tests.
-  static const repositoryOwner = 'owner';
+  static const String repositoryOwner = 'owner';
 
   /// A name of the project's repository to use in tests.
-  static const repositoryName = 'name';
+  static const String repositoryName = 'name';
+
+  /// A name of the workflow's job to use in tests.
+  static const String jobName = 'job-name';
+
+  /// A name of an artifact with coverage data.
+  static const String coverageArtifactName = 'job_coverage.json';
 
   /// An access token to use in tests.
-  static const accessToken = 'token';
+  static const String accessToken = 'token';
 
   /// A decoded JSON object with Github Action test configurations.
   static const Map<String, dynamic> sourceConfigMap = {
-    'workflow_identifier': workflowIdentifier,
     'repository_owner': repositoryOwner,
     'repository_name': repositoryName,
+    'workflow_identifier': workflowIdentifier,
+    'job_name': jobName,
+    'coverage_artifact_name': coverageArtifactName,
     'access_token': accessToken,
   };
 
   /// A source config to use in tests.
   static final sourceConfig = GithubActionsSourceConfig(
-    workflowIdentifier: workflowIdentifier,
     repositoryOwner: repositoryOwner,
     repositoryName: repositoryName,
+    workflowIdentifier: workflowIdentifier,
+    jobName: jobName,
+    coverageArtifactName: coverageArtifactName,
     accessToken: accessToken,
   );
 }
