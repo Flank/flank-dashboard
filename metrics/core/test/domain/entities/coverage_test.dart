4274d3fdfd461b669b9c8cce2de4dd24f763006d
Tetiana Movlian <tetianamovlian@Tetianas-MacBook-Pro.local>
2020-10-21 13:19:10 +0300

    Add test to coverage class

--- metrics/core/test/domain/entities/coverage_test.dart
+++ metrics/core/test/domain/entities/coverage_test.dart
@@ -1,3 +1,4 @@
+import 'package:metrics_core/metrics_core.dart';
 import 'package:metrics_core/src/domain/entities/coverage.dart';
 import 'package:test/test.dart';
 
@@ -7,6 +8,19 @@ void main() {
       "throws an ArgumentError if the given percent is null",
       () {
         expect(() => Coverage(percent: null), throwsArgumentError);
+      },
+    );
+
+    test(
+      "two instances with the same percents are equal",
+      () {
+        const percent = 0.6;
+
+        final firstCoverage = Coverage(percent: Percent(percent));
+
+        final secondCoverage = Coverage(percent: Percent(percent));
+
+        expect(firstCoverage, equals(secondCoverage));
       },
     );
   });
