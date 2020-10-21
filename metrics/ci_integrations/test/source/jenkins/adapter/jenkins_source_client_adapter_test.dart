5f53faec5596098cba5c39bf9d5af2b5fe42c10b
Tetiana Movlian <tetianamovlian@Tetianas-MacBook-Pro.local>
2020-10-21 14:06:46 +0300

    Fix jenkins client adapter tests

--- metrics/ci_integrations/test/source/jenkins/adapter/jenkins_source_client_adapter_test.dart
+++ metrics/ci_integrations/test/source/jenkins/adapter/jenkins_source_client_adapter_test.dart
@@ -596,7 +596,7 @@ class _JenkinsClientResponse {
   /// method.
   Future<InteractionResult<Map<String, dynamic>>> artifact([_]) {
     final result = <String, dynamic>{
-      'pct': 60,
+      'pct': 0.6,
     };
     return _wrapFuture(InteractionResult.success(result: result));
   }
