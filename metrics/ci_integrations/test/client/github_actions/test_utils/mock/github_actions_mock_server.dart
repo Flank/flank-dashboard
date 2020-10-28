29b2b0ceb6b67d5f9aaef3c11663a3d4d8f5240a
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2020-10-28 13:31:37 +0200

    Implement _workflowRunJobsResponse

--- metrics/ci_integrations/test/client/github_actions/test_utils/mock/github_actions_mock_server.dart
+++ metrics/ci_integrations/test/client/github_actions/test_utils/mock/github_actions_mock_server.dart
@@ -4,17 +4,26 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:api_mock_server/api_mock_server.dart';
-import 'package:ci_integration/client/github_actions/mappers/run_status_mapper.dart';
-import 'package:ci_integration/client/github_actions/models/run_conclusion.dart';
-import 'package:ci_integration/client/github_actions/models/run_status.dart';
+import 'package:ci_integration/client/github_actions/mappers/github_action_conclusion_mapper.dart';
+import 'package:ci_integration/client/github_actions/mappers/github_action_status_mapper.dart';
+import 'package:ci_integration/client/github_actions/models/github_action_conclusion.dart';
+import 'package:ci_integration/client/github_actions/models/github_action_status.dart';
 import 'package:ci_integration/client/github_actions/models/workflow_run.dart';
 import 'package:ci_integration/client/github_actions/models/workflow_run_artifact.dart';
-import 'package:ci_integration/client/github_actions/models/workflow_run_duration.dart';
+import 'package:ci_integration/client/github_actions/models/workflow_run_job.dart';
 
 /// A mock server for the Github Actions API.
 class GithubActionsMockServer extends ApiMockServer {
   /// A path to emulate a download url.
   static const String _downloadPath = '/download';
+
+  /// A [GithubActionStatusMapper] of this mock server;
+  static const GithubActionStatusMapper statusMapper =
+      GithubActionStatusMapper();
+
+  /// A [GithubActionConclusionMapper] of this mock server;
+  static const GithubActionConclusionMapper conclusionMapper =
+      GithubActionConclusionMapper();
 
   /// A random value.
   final random = Random();
@@ -35,6 +44,18 @@ class GithubActionsMockServer extends ApiMockServer {
         ),
         RequestHandler.get(
           pathMatcher: ExactPathMatcher(
+            '/repos/owner/name/actions/runs/1/jobs',
+          ),
+          dispatcher: _workflowRunJobsResponse,
+        ),
+        RequestHandler.get(
+          pathMatcher: ExactPathMatcher(
+            '/repos/owner/name/actions/runs/test/jobs',
+          ),
+          dispatcher: _notFoundResponse,
+        ),
+        RequestHandler.get(
+          pathMatcher: ExactPathMatcher(
             '/repos/owner/name/actions/runs/1/artifacts',
           ),
           dispatcher: _workflowRunArtifactsResponse,
@@ -42,18 +63,6 @@ class GithubActionsMockServer extends ApiMockServer {
         RequestHandler.get(
           pathMatcher: ExactPathMatcher(
             '/repos/owner/name/actions/runs/test/artifacts',
-          ),
-          dispatcher: _notFoundResponse,
-        ),
-        RequestHandler.get(
-          pathMatcher: ExactPathMatcher(
-            '/repos/owner/name/actions/runs/1/timing',
-          ),
-          dispatcher: _workflowUsageResponse,
-        ),
-        RequestHandler.get(
-          pathMatcher: ExactPathMatcher(
-            '/repos/owner/name/actions/runs/test/timing',
           ),
           dispatcher: _notFoundResponse,
         ),
@@ -89,6 +98,9 @@ class GithubActionsMockServer extends ApiMockServer {
     );
 
     final hasMorePages = pageNumber < lastPageNumber;
+    if (hasMorePages) {
+      _setNextPageUrlHeader(request, pageNumber);
+    }
 
     workflowRuns = _paginate(
       workflowRuns,
@@ -101,7 +113,36 @@ class GithubActionsMockServer extends ApiMockServer {
       'workflow_runs': workflowRuns.map((run) => run.toJson()).toList(),
     };
 
-    _setNextPageUrlHeader(request, hasMorePages, pageNumber);
+    await _writeResponse(request, _response);
+  }
+
+  /// Responses with a list of all workflow run jobs for a specific workflow run.
+  Future<void> _workflowRunJobsResponse(HttpRequest request) async {
+    final status = _extractRunStatus(request);
+    final runsPerPage = _extractPerPage(request);
+    final pageNumber = _extractPage(request);
+
+    List<WorkflowRunJob> workflowRunJobs = _generateWorkflowRunJobs(status);
+
+    final lastPageNumber = _getLastPageNumber(
+      workflowRunJobs.length,
+      runsPerPage,
+    );
+
+    final hasMorePages = pageNumber < lastPageNumber;
+    if (hasMorePages) {
+      _setNextPageUrlHeader(request, pageNumber);
+    }
+
+    workflowRunJobs = _paginate(
+      workflowRunJobs,
+      runsPerPage,
+      pageNumber,
+    );
+    final _response = {
+      'total_count': workflowRunJobs.length,
+      'jobs': workflowRunJobs.map((run) => run.toJson()).toList(),
+    };
 
     await _writeResponse(request, _response);
   }
@@ -124,24 +165,16 @@ class GithubActionsMockServer extends ApiMockServer {
     );
 
     final hasMorePages = pageNumber < lastPageNumber;
+    if (hasMorePages) {
+      _setNextPageUrlHeader(request, pageNumber);
+    }
 
     final _response = {
       'total_count': artifacts.length,
       'artifacts': artifacts.map((artifact) => artifact.toJson()).toList(),
     };
 
-    _setNextPageUrlHeader(request, hasMorePages, pageNumber);
-
     await _writeResponse(request, _response);
-  }
-
-  /// Responses with the total run time for a specific workflow run.
-  Future<void> _workflowUsageResponse(HttpRequest request) async {
-    const workflowRunDuration = WorkflowRunDuration(
-      duration: Duration(milliseconds: 500000),
-    );
-
-    await _writeResponse(request, workflowRunDuration.toJson());
   }
 
   /// Redirects to the URL to download an archive for a repository.
@@ -183,20 +216,16 @@ class GithubActionsMockServer extends ApiMockServer {
     return items;
   }
 
-  /// Generates a list of [WorkflowRun], filtered by the given [RunStatus].
-  List<WorkflowRun> _generateWorkflowRuns(RunStatus status) {
-    const statuses = RunStatus.values;
-    const conclusions = RunConclusion.values;
-
+  /// Generates a list of [WorkflowRun], filtered by the given [status].
+  List<WorkflowRun> _generateWorkflowRuns(GithubActionStatus status) {
     final runs = List.generate(100, (index) {
-      final id = index + 1;
+      final runNumber = index + 1;
 
       return WorkflowRun(
-        id: id,
-        number: random.nextInt(100),
+        id: runNumber,
+        number: runNumber,
         url: 'url',
-        status: status ?? statuses[random.nextInt(statuses.length)],
-        conclusion: conclusions[random.nextInt(conclusions.length)],
+        status: status ?? _generateRandomStatus(),
         createdAt: DateTime.now().toUtc(),
       );
     });
@@ -204,30 +233,59 @@ class GithubActionsMockServer extends ApiMockServer {
     return runs;
   }
 
+  /// Generates a list of [WorkflowRunJob]s, filtered by the given [status].
+  List<WorkflowRunJob> _generateWorkflowRunJobs(GithubActionStatus status) {
+    final jobs = List.generate(100, (index) {
+      final id = index + 1;
+
+      return WorkflowRunJob(
+        id: id,
+        runId: 1,
+        name: 'name',
+        url: 'url',
+        status: status ?? _generateRandomStatus(),
+        conclusion: _generateRandomConclusion(),
+        startedAt: DateTime(2019),
+        completedAt: DateTime(2020),
+      );
+    });
+
+    return jobs;
+  }
+
+  /// Generates a random [GithubActionStatus].
+  GithubActionStatus _generateRandomStatus() {
+    const statuses = GithubActionStatus.values;
+    return statuses[random.nextInt(statuses.length)];
+  }
+
+  /// Generates a random [GithubActionStatus].
+  GithubActionConclusion _generateRandomConclusion() {
+    const conclusions = GithubActionConclusion.values;
+    return conclusions[random.nextInt(conclusions.length)];
+  }
+
   /// Generates a list of [WorkflowRunArtifact].
   List<WorkflowRunArtifact> _generateArtifacts() {
-    final artifacts = List.generate(
-      100,
-      (index) {
-        final id = index + 1;
+    final artifacts = List.generate(100, (index) {
+      final id = index + 1;
 
-        return WorkflowRunArtifact(
-          id: id,
-          name: 'coverage$id.json',
-          downloadUrl: 'https://api.github.com$_downloadPath',
-        );
-      },
-    );
+      return WorkflowRunArtifact(
+        id: id,
+        name: 'coverage$id.json',
+        downloadUrl: 'https://api.github.com$_downloadPath',
+      );
+    });
 
     return artifacts;
   }
 
   /// Provides the [RunStatus], based on the `status` query parameter
   /// in the given [request].
-  RunStatus _extractRunStatus(HttpRequest request) {
+  GithubActionStatus _extractRunStatus(HttpRequest request) {
     final status = request.uri.queryParameters['status'];
 
-    return const RunStatusMapper().map(status);
+    return statusMapper.map(status);
   }
 
   /// Returns the `int` representation of the `per_page` query parameter
@@ -263,17 +321,14 @@ class GithubActionsMockServer extends ApiMockServer {
     return max((total / perPage).ceil(), 1);
   }
 
-  /// Sets next page url header if the given [hasMorePages] parameter is `true`.
-  void _setNextPageUrlHeader(
-      HttpRequest request, bool hasMorePages, int pageNumber) {
-    if (hasMorePages) {
-      final requestUrl = request.requestedUri.toString();
-      final indexOfPageParam = requestUrl.indexOf("&page=");
-      final nextPageUrl = requestUrl.replaceRange(
-          indexOfPageParam, requestUrl.length, "&page=${pageNumber + 1}");
+  /// Sets next page url header.
+  void _setNextPageUrlHeader(HttpRequest request, int pageNumber) {
+    final requestUrl = request.requestedUri.toString();
+    final indexOfPageParam = requestUrl.indexOf("&page=");
+    final nextPageUrl = requestUrl.replaceRange(
+        indexOfPageParam, requestUrl.length, "&page=${pageNumber + 1}");
 
-      request.response.headers.set('link', '<$nextPageUrl> rel="next"');
-    }
+    request.response.headers.set('link', '<$nextPageUrl> rel="next"');
   }
 
   /// Writes the given [response].
