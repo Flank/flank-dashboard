f015c68342214f625ed24cb4642144ebc79717cf
Maksym Ustych <54895368+solid-maxim@users.noreply.github.com>
2020-11-04 15:32:20 +0200

    feat: Buildkite YAML config and config parser (#771)
    * Add Buildkite YAML config and tests
    
    * Fix descriptions and tests
    
    * Add coverage artifact name parameter
    
    * Fix descriptions
    
    * Delete coverage artifact name property
    
    * Delete artifact name from config template
    
    * Fix documentation
    
    Co-authored-by: solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>

---
+++ metrics/ci_integrations/lib/source/buildkite/config/model/buildkite_source_config.dart
@@ -0,0 +1,65 @@
+import 'package:ci_integration/integration/interface/source/config/model/source_config.dart';
+import 'package:ci_integration/util/validator/string_validator.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+/// A class that represents a [SourceConfig] for the Buildkite integration.
+class BuildkiteSourceConfig extends Equatable implements SourceConfig {
+  /// A Buildkite access token.
+  final String accessToken;
+
+  /// A unique slug (identifier) of the pipeline on Buildkite the integration
+  /// should work with.
+  final String pipelineSlug;
+
+  /// A unique slug (identifier) of the organization on Buildkite in the scope
+  /// of which the integration should work and perform requests.
+  final String organizationSlug;
+
+  @override
+  String get sourceProjectId => pipelineSlug;
+
+  @override
+  List<Object> get props => [
+        accessToken,
+        pipelineSlug,
+        organizationSlug,
+      ];
+
+  /// Creates a new instance of the [BuildkiteSourceConfig].
+  ///
+  /// Throws an [ArgumentError] if any of the required parameters
+  /// is `null` or empty.
+  BuildkiteSourceConfig({
+    @required this.accessToken,
+    @required this.pipelineSlug,
+    @required this.organizationSlug,
+  }) {
+    StringValidator.checkNotNullOrEmpty(
+      accessToken,
+      name: 'accessToken',
+    );
+    StringValidator.checkNotNullOrEmpty(
+      pipelineSlug,
+      name: 'pipelineSlug',
+    );
+    StringValidator.checkNotNullOrEmpty(
+      organizationSlug,
+      name: 'organizationSlug',
+    );
+  }
+
+  /// Creates a new instance of the [BuildkiteSourceConfig] from the
+  /// decoded JSON object.
+  ///
+  /// Returns `null` if the given [json] is `null`.
+  factory BuildkiteSourceConfig.fromJson(Map<String, dynamic> json) {
+    if (json == null) return null;
+
+    return BuildkiteSourceConfig(
+      accessToken: json['access_token'] as String,
+      pipelineSlug: json['pipeline_slug'] as String,
+      organizationSlug: json['organization_slug'] as String,
+    );
+  }
+}
---
+++ metrics/ci_integrations/test/source/buildkite/config/parser/buildkite_source_config_parser_test.dart
@@ -0,0 +1,76 @@
+import 'package:ci_integration/source/buildkite/config/parser/buildkite_source_config_parser.dart';
+import 'package:test/test.dart';
+
+import '../../test_utils/test_data/buildkite_config_test_data.dart';
+
+// ignore_for_file: prefer_const_constructors
+
+void main() {
+  group("BuildkiteSourceConfigParser", () {
+    const configMap = {
+      'buildkite': BuildkiteConfigTestData.sourceConfigMap,
+    };
+
+    final buildkiteSourceConfigParser = BuildkiteSourceConfigParser();
+
+    test(
+      ".canParse() returns false if the given map is null",
+      () {
+        final canParse = buildkiteSourceConfigParser.canParse(null);
+
+        expect(canParse, isFalse);
+      },
+    );
+
+    test(
+      ".canParse() returns false if the given map does not contain a Buildkite key",
+      () {
+        final map = {'test': {}};
+
+        final canParse = buildkiteSourceConfigParser.canParse(map);
+
+        expect(canParse, isFalse);
+      },
+    );
+
+    test(
+      ".canParse() returns true if the parser can parse the given map",
+      () {
+        final canParse = buildkiteSourceConfigParser.canParse(configMap);
+
+        expect(canParse, isTrue);
+      },
+    );
+
+    test(
+      ".parse() returns null if the given map is null",
+      () {
+        final result = buildkiteSourceConfigParser.parse(null);
+
+        expect(result, isNull);
+      },
+    );
+
+    test(
+      ".parse() returns null if the given map does not contain a Buildkite key",
+      () {
+        final map = {'test': {}};
+
+        final result = buildkiteSourceConfigParser.parse(map);
+
+        expect(result, isNull);
+      },
+    );
+
+    test(
+      ".parse() parses the given map into the Buildkite source config",
+      () {
+        final expected = BuildkiteConfigTestData.sourceConfig;
+
+        final result = buildkiteSourceConfigParser.parse(configMap);
+
+        expect(result, equals(expected));
+      },
+    );
+  });
+}
