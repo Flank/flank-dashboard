666114d1734c2eca5a4f4138c1b916d11405d855
Maksym Ustych <54895368+solid-maxim@users.noreply.github.com>
2020-11-09 19:14:42 +0200

    test: Buildkite mock server (#774)
    * Add Buildkite mock server
    
    * Add mock server utils and fix buildkite and github  mock servers
    
    * Fix descriptions, add extract uri parameter methods from the mock server utils
    
    * Remove the setNextPageUrlHeader method from the mock server utils
    
    * Add trailing coma, reuse notFoundResponse
    
    * reuse downloadResponse
    
    * Fix dock, remove _notFoundResponse in Jenkins Mock Server
    
    * Reuse writeResponse in Jenkins Mock Server
    
    * Refactor responses in Jenkins Mock Server
    
    Co-authored-by: solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>

---
+++ metrics/ci_integrations/test/client/test_utils/mock_server_utils.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+/// A class that holds methods for processing requests in mock servers.
+class MockServerUtils {
+  /// Writes the given [response] to the given [request].
+  static Future<void> writeResponse(
+    HttpRequest request,
+    dynamic response,
+  ) async {
+    request.response.write(jsonEncode(response));
+
+    await request.response.flush();
+    await request.response.close();
+  }
+
+  /// Chunks the given [items], limiting to the [limit],
+  /// starting from the [pageIndex].
+  static List<T> paginate<T>(List<T> items, [int limit, int pageIndex]) {
+    if (limit != null && pageIndex != null) {
+      final from = (pageIndex - 1) * limit;
+
+      return items.skip(from).take(limit).toList();
+    } else if (limit != null) {
+      return items.take(limit).toList();
+    }
+
+    return items;
+  }
+
+  /// Adds a [HttpStatus.notFound] status code to the [HttpRequest.response]
+  /// and closes it.
+  static Future<void> notFoundResponse(HttpRequest request) async {
+    request.response.statusCode = HttpStatus.notFound;
+
+    await request.response.close();
+  }
+
+  /// Returns a [Uint8List] to emulate download.
+  static Future<void> downloadResponse(HttpRequest request) async {
+    await MockServerUtils.writeResponse(request, Uint8List.fromList([]));
+  }
+}
