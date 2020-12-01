e72fed641e4d6de147c83d9827716f0f7c216c01
vova.beloded <vova.beloded@solid.software>
2020-12-01 19:07:29 +0200

    Fix test description

--- metrics/ci_integrations/test/cli/error/sync_error_test.dart
+++ metrics/ci_integrations/test/cli/error/sync_error_test.dart
@@ -11,7 +11,7 @@ void main() {
       expect(syncError.message, equals(message));
     });
 
-    test(".toString returns an error message", () {
+    test(".toString() returns an error message", () {
       final syncError = SyncError(message: message);
 
       expect(syncError.toString(), equals(message));
