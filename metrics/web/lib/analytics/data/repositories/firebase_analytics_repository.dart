a528e1e761ec1bd12f1e153057e8d5cdfc99f754
solid-tetianamovlian <70527894+solid-tetianamovlian@users.noreply.github.com>
2020-12-03 13:43:42 +0200

    feat: Implement Firebase Analytics Data layer (#857)
    * Implement Analytics Data layer
    
    * Redesign repository. Implement ResetUserUseCase.
    
    * Review suggestions
    
    * Improve documentation.
    
    Co-authored-by: solid-dimakoniaiev <dmytro.koniaiev@solid.software>

---
+++ metrics/web/lib/analytics/data/repositories/firebase_analytics_repository.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:metrics/analytics/domain/repositories/analytics_repository.dart';
+
+/// A class that provides methods for interaction with the [FirebaseAnalytics].
+class FirebaseAnalyticsRepository implements AnalyticsRepository {
+  /// An instance of the [FirebaseAnalytics] that provides an ability
+  /// to interact with the remote analytics service.
+  final FirebaseAnalytics _analytics = FirebaseAnalytics();
+
+  @override
+  Future<void> logLogin() async {
+    return _analytics.logLogin();
+  }
+
+  @override
+  Future<void> logPageView(String pageName) {
+    return _analytics.logEvent(name: pageName);
+  }
+
+  @override
+  Future<void> setUserId(String id) {
+    return _analytics.setUserId(id);
+  }
+}
