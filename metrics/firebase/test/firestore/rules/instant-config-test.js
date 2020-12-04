71ae6f637ee75cabee170ad3faadd65f9007d0c9
solid-maxim <maxim.ustych@solid.software>
2020-12-04 17:11:33 +0200

    add more tests for the instant config security rules

--- metrics/firebase/test/firestore/rules/instant-config-test.js
+++ metrics/firebase/test/firestore/rules/instant-config-test.js
@@ -8,36 +8,123 @@ const {
 
 const {
   instantConfig,
-  getAllowedEmailUser,
   passwordSignInProviderId,
+  googleSignInProviderId,
+  getAllowedEmailUser,
+  getDeniedEmailUser,
 } = require("./test_utils/test-data");
 
 describe("", async () => {
+  const unauthenticatedApp = await getApplicationWith(null);
   const collection = "instant_config";
   const config = { config: {} };
 
   const users = [
     {
-      describe: "Authenticated user",
-      app: await getApplicationWith(
-        getAllowedEmailUser(passwordSignInProviderId, true)
+      'describe': 'Authenticated with a password and allowed email domain user with a verified email',
+      'app': await getApplicationWith(
+          getAllowedEmailUser(passwordSignInProviderId, true)
       ),
-      can: {
-        create: false,
-        read: true,
-        update: false,
-        delete: false,
-      },
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
     },
     {
-      describe: "Unauthenticated user",
-      app: await getApplicationWith(null),
-      can: {
-        create: false,
-        read: true,
-        update: false,
-        delete: false,
-      },
+      'describe': 'Authenticated with a password and not allowed email domain user with a verified email',
+      'app': await getApplicationWith(
+        getDeniedEmailUser(passwordSignInProviderId, true)
+      ),
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
+    },
+    {
+      'describe': 'Authenticated with a password and allowed email domain user with not verified email',
+      'app': await getApplicationWith(
+        getAllowedEmailUser(passwordSignInProviderId, false)
+      ),
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
+    },
+    {
+      'describe': 'Authenticated with a password and not allowed email domain user with not verified email',
+      'app': await getApplicationWith(
+        getDeniedEmailUser(passwordSignInProviderId, false)
+      ),
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
+    },
+    {
+      'describe': 'Authenticated with google and allowed email domain user with a verified email',
+      'app': await getApplicationWith(
+        getAllowedEmailUser(googleSignInProviderId, true)
+      ),
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
+    },
+    {
+      'describe': 'Authenticated with google and not allowed email domain user with a verified email',
+      'app': await getApplicationWith(
+        getDeniedEmailUser(googleSignInProviderId, true)
+      ),
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
+    },
+    {
+      'describe': 'Authenticated with google and allowed email domain user with not verified email',
+      'app': await getApplicationWith(
+        getAllowedEmailUser(googleSignInProviderId, false)
+      ),
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
+    },
+    {
+      'describe': 'Authenticated with google and not allowed email domain user with not verified email',
+      'app': await getApplicationWith(
+        getDeniedEmailUser(googleSignInProviderId, false)
+      ),
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
+    },
+    {
+      'describe': 'Unauthenticated user',
+      'app': unauthenticatedApp,
+      'can': {
+        'create': false,
+        'read': true,
+        'update': false,
+        'delete': false,
+      }
     },
   ];
 
