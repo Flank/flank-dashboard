e8cc8a2f528fb6c098b9ff8a36e7dc4f3b72d44c
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2020-12-09 16:15:15 +0200

    Extract Login and Password form to a separate widget, extract and update tests

---
+++ metrics/web/test/auth/presentation/widgets/login_form_test.dart
@@ -0,0 +1,321 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:metrics/auth/presentation/state/auth_notifier.dart';
+import 'package:metrics/auth/presentation/strings/auth_strings.dart';
+import 'package:metrics/auth/presentation/widgets/login_form.dart';
+import 'package:metrics/base/presentation/widgets/tappable_area.dart';
+import 'package:metrics/common/presentation/button/widgets/metrics_positive_button.dart';
+import 'package:metrics/common/presentation/metrics_theme/model/login_theme_data.dart';
+import 'package:metrics/common/presentation/metrics_theme/model/metrics_theme_data.dart';
+import 'package:metrics/common/presentation/widgets/metrics_text_form_field.dart';
+import 'package:mockito/mockito.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+
+import '../../../test_utils/auth_notifier_mock.dart';
+import '../../../test_utils/metrics_themed_testbed.dart';
+import '../../../test_utils/test_injection_container.dart';
+
+void main() {
+  const passwordVisibilityIconColor = Colors.red;
+  const metricsThemeData = MetricsThemeData(
+    loginTheme: LoginThemeData(
+      passwordVisibilityIconColor: passwordVisibilityIconColor,
+    ),
+  );
+
+  final emailInputFinder =
+      find.widgetWithText(MetricsTextFormField, AuthStrings.email);
+  final passwordInputFinder =
+      find.widgetWithText(MetricsTextFormField, AuthStrings.password);
+  final submitButtonFinder =
+      find.widgetWithText(MetricsPositiveButton, AuthStrings.signIn);
+
+  const testEmail = 'test@email.com';
+  const testPassword = 'testPassword';
+  const errorMessage = 'Error Message';
+
+  AuthNotifier authNotifier;
+
+  Widget _getPasswordFieldSuffixIcon(WidgetTester tester) {
+    final passwordField = tester.widget<MetricsTextFormField>(
+      passwordInputFinder,
+    );
+
+    return passwordField.suffixIcon;
+  }
+
+  setUp(() {
+    authNotifier = AuthNotifierMock();
+    when(authNotifier.isLoading).thenReturn(false);
+  });
+
+  group("LoginForm", () {
+    testWidgets(
+      "email input shows an error message if a value is empty on submit",
+      (WidgetTester tester) async {
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(
+            _LoginFormTestbed(authNotifier: authNotifier),
+          );
+        });
+
+        await tester.tap(submitButtonFinder);
+        await tester.pumpAndSettle();
+
+        expect(
+          find.text(AuthStrings.emailRequiredErrorMessage),
+          findsOneWidget,
+        );
+      },
+    );
+
+    testWidgets(
+      "email input shows an error message if a value is not a valid email on submit",
+      (WidgetTester tester) async {
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(
+            _LoginFormTestbed(authNotifier: authNotifier),
+          );
+        });
+
+        await tester.enterText(emailInputFinder, 'notAnEmail');
+
+        await tester.tap(submitButtonFinder);
+        await tester.pump();
+
+        expect(find.text(AuthStrings.invalidEmailErrorMessage), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "password input shows an error message if a value is empty on submit",
+      (WidgetTester tester) async {
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(
+            _LoginFormTestbed(authNotifier: authNotifier),
+          );
+        });
+
+        await tester.tap(submitButtonFinder);
+        await tester.pump();
+
+        expect(
+          find.text(AuthStrings.passwordRequiredErrorMessage),
+          findsOneWidget,
+        );
+      },
+    );
+
+    testWidgets(
+      "uses the AuthNotifier to sign in a user with login and password",
+      (WidgetTester tester) async {
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(
+            _LoginFormTestbed(authNotifier: authNotifier),
+          );
+        });
+
+        await tester.enterText(emailInputFinder, testEmail);
+        await tester.enterText(passwordInputFinder, testPassword);
+        await tester.tap(submitButtonFinder);
+
+        verify(authNotifier.signInWithEmailAndPassword(testEmail, testPassword))
+            .called(equals(1));
+      },
+    );
+
+    testWidgets(
+      "shows a progress indicator if the sign in process is in progress",
+      (WidgetTester tester) async {
+        when(authNotifier.authErrorMessage).thenReturn(null);
+        when(authNotifier.isLoading).thenReturn(true);
+
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(_LoginFormTestbed(
+            authNotifier: authNotifier,
+          ));
+        });
+
+        final finder = find.byType(LinearProgressIndicator);
+
+        expect(finder, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "disables the submit button if the sign in process is in progress",
+      (WidgetTester tester) async {
+        when(authNotifier.authErrorMessage).thenReturn(null);
+        when(authNotifier.isLoading).thenReturn(true);
+
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(_LoginFormTestbed(
+            authNotifier: authNotifier,
+          ));
+        });
+
+        final button = tester.widget<RaisedButton>(
+          find.descendant(
+            of: submitButtonFinder,
+            matching: find.byType(RaisedButton),
+          ),
+        );
+
+        expect(button.enabled, isFalse);
+      },
+    );
+
+    testWidgets(
+      "applies the enabled eye icon to the password input field if the password is obscured",
+      (WidgetTester tester) async {
+        await mockNetworkImagesFor(
+          () => tester.pumpWidget(
+            const _LoginFormTestbed(),
+          ),
+        );
+
+        final suffixIcon = _getPasswordFieldSuffixIcon(tester);
+        final imageWidget = tester.widget<Image>(find.descendant(
+          of: find.byWidget(suffixIcon),
+          matching: find.byType(Image),
+        ));
+        final networkImage = imageWidget.image as NetworkImage;
+
+        expect(networkImage.url, 'icons/eye_on.svg');
+      },
+    );
+
+    testWidgets(
+      "applies the disabled eye icon to the password input field if the password is not obscured",
+      (WidgetTester tester) async {
+        await mockNetworkImagesFor(
+          () => tester.pumpWidget(
+            const _LoginFormTestbed(),
+          ),
+        );
+
+        final suffixIcon = _getPasswordFieldSuffixIcon(tester);
+
+        await tester.tap(find.byWidget(suffixIcon));
+        await tester.pump();
+
+        final suffixIconAfterTap = _getPasswordFieldSuffixIcon(tester);
+        final imageWidget = tester.widget<Image>(find.descendant(
+          of: find.byWidget(suffixIconAfterTap),
+          matching: find.byType(Image),
+        ));
+        final networkImage = imageWidget.image as NetworkImage;
+
+        expect(networkImage.url, 'icons/eye_off.svg');
+      },
+    );
+
+    testWidgets(
+      "applies the color from the metrics theme to the password visibility icon",
+      (WidgetTester tester) async {
+        await mockNetworkImagesFor(
+          () => tester.pumpWidget(
+            const _LoginFormTestbed(
+              metricsThemeData: metricsThemeData,
+            ),
+          ),
+        );
+
+        final suffixIcon = _getPasswordFieldSuffixIcon(tester);
+        final image = tester.widget<Image>(find.descendant(
+          of: find.byWidget(suffixIcon),
+          matching: find.byType(Image),
+        ));
+
+        expect(image.color, equals(passwordVisibilityIconColor));
+      },
+    );
+
+    testWidgets(
+      "applies a tappable area to the suffix icon of the password field",
+      (WidgetTester tester) async {
+        await mockNetworkImagesFor(
+          () => tester.pumpWidget(
+            const _LoginFormTestbed(),
+          ),
+        );
+
+        final suffixIcon = _getPasswordFieldSuffixIcon(tester);
+        final imageWidget = tester.widget<Image>(find.descendant(
+          of: find.byWidget(suffixIcon),
+          matching: find.byType(Image),
+        ));
+        final tappableAreaFinder = find.ancestor(
+          of: find.byWidget(imageWidget),
+          matching: find.byType(TappableArea),
+        );
+
+        expect(tappableAreaFinder, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "applies the email error message to the email field",
+      (tester) async {
+        when(authNotifier.emailErrorMessage).thenReturn(errorMessage);
+
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(
+            _LoginFormTestbed(authNotifier: authNotifier),
+          );
+        });
+
+        final emailField = tester.widget<MetricsTextFormField>(
+          emailInputFinder,
+        );
+
+        expect(emailField.errorText, equals(errorMessage));
+      },
+    );
+
+    testWidgets(
+      "applies the password error message to the password field",
+      (tester) async {
+        when(authNotifier.passwordErrorMessage).thenReturn(errorMessage);
+
+        await mockNetworkImagesFor(() {
+          return tester.pumpWidget(
+            _LoginFormTestbed(authNotifier: authNotifier),
+          );
+        });
+
+        final passwordField = tester.widget<MetricsTextFormField>(
+          passwordInputFinder,
+        );
+
+        expect(passwordField.errorText, equals(errorMessage));
+      },
+    );
+  });
+}
+
+/// A testbed widget used to test the [LoginForm] widget.
+class _LoginFormTestbed extends StatelessWidget {
+  /// An [AuthNotifier] to use in tests.
+  final AuthNotifier authNotifier;
+
+  /// A [MetricsThemeData] to use in tests.
+  final MetricsThemeData metricsThemeData;
+
+  /// Creates the [_LoginFormTestbed] with the given [authNotifier].
+  const _LoginFormTestbed({
+    this.metricsThemeData = const MetricsThemeData(),
+    this.authNotifier,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TestInjectionContainer(
+      authNotifier: authNotifier,
+      child: MetricsThemedTestbed(
+        metricsThemeData: metricsThemeData,
+        body: const LoginForm(),
+      ),
+    );
+  }
+}
