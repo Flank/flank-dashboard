b8f028d439b92dd099974e042abb4ea7918ad26e
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2020-12-09 09:56:00 +0200

    Improve import

--- metrics/web/test/test_utils/matcher_util.dart
+++ metrics/web/test/test_utils/matcher_util.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:metrics/auth/domain/entities/authentication_exception.dart';
 import 'package:test/test.dart';
 
