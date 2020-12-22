adf9d5f001efcb1c5f1e9568b03e5e18fd96c6c0
solid-maxim <maxim.ustych@solid.software>
2020-12-22 14:47:17 +0200

    Add more integration tests for the project groups page

--- metrics/web/test_driver/app_test.dart
+++ metrics/web/test_driver/app_test.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:metrics/auth/presentation/strings/auth_strings.dart';
 import 'package:metrics/auth/presentation/widgets/auth_form.dart';
+import 'package:metrics/base/presentation/widgets/icon_label_button.dart';
+import 'package:metrics/common/presentation/button/widgets/metrics_negative_button.dart';
 import 'package:metrics/common/presentation/button/widgets/metrics_positive_button.dart';
 import 'package:metrics/common/presentation/strings/common_strings.dart';
+import 'package:metrics/common/presentation/widgets/metrics_checkbox.dart';
 import 'package:metrics/common/presentation/widgets/metrics_text_form_field.dart';
 import 'package:metrics/dashboard/presentation/pages/dashboard_page.dart';
 import 'package:metrics/dashboard/presentation/strings/dashboard_strings.dart';
@@ -16,7 +21,10 @@ import 'package:metrics/dashboard/presentation/widgets/project_metrics_tile.dart
 import 'package:metrics/dashboard/presentation/widgets/projects_search_input.dart';
 import 'package:metrics/dashboard/presentation/widgets/stability_circle_percentage.dart';
 import 'package:metrics/main.dart';
+import 'package:metrics/project_groups/domain/value_objects/project_group_name.dart';
+import 'package:metrics/project_groups/presentation/strings/project_groups_strings.dart';
 import 'package:metrics/project_groups/presentation/widgets/add_project_group_card.dart';
+import 'package:metrics/project_groups/presentation/widgets/project_group_card.dart';
 
 import 'arguments/model/user_credentials.dart';
 
@@ -147,6 +155,9 @@ void main() {
   });
 
   group("ProjectGroup page", () {
+    final projectGroupName = 'x' * ProjectGroupName.charactersLimit;
+    final updatedProjectGroupName = projectGroupName.replaceFirst('x', 'y');
+
     testWidgets(
       "shows add project group card button",
       (WidgetTester tester) async {
@@ -155,6 +166,137 @@ void main() {
         await _openProjectGroupPage(tester);
 
         expect(find.byType(AddProjectGroupCard), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "creates a project group",
+      (WidgetTester tester) async {
+        await _pumpApp(tester);
+        await _openProjectGroupPage(tester);
+
+        await tester.tap(find.byType(AddProjectGroupCard));
+        await tester.pumpAndSettle();
+
+        final projectGroupNameInputFinder = find.widgetWithText(
+          MetricsTextFormField,
+          ProjectGroupsStrings.nameYourGroup,
+        );
+        await tester.enterText(projectGroupNameInputFinder, projectGroupName);
+
+        await tester.tap(find.byType(MetricsCheckbox).first);
+        await tester.pumpAndSettle();
+
+        final createButtonFinder = find.widgetWithText(
+          MetricsPositiveButton,
+          ProjectGroupsStrings.createGroup,
+        );
+        await tester.tap(createButtonFinder);
+        await tester.pumpAndSettle();
+
+        final projectGroupCardFinder = find.widgetWithText(
+          ProjectGroupCard,
+          projectGroupName,
+        );
+
+        expect(projectGroupCardFinder, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "updates a project group",
+      (WidgetTester tester) async {
+        await _pumpApp(tester);
+        await _openProjectGroupPage(tester);
+
+        final projectGroupCardFinder = find.widgetWithText(
+          ProjectGroupCard,
+          projectGroupName,
+        );
+
+        await tester.ensureVisible(projectGroupCardFinder);
+
+        final mouseRegion = tester.firstWidget<MouseRegion>(
+          find.descendant(
+            of: projectGroupCardFinder,
+            matching: find.byType(MouseRegion),
+          ),
+        );
+        const pointerEvent = PointerEnterEvent();
+        mouseRegion.onEnter(pointerEvent);
+        await tester.pumpAndSettle();
+
+        final editButtonFinder = find.descendant(
+          of: projectGroupCardFinder,
+          matching: find.widgetWithText(IconLabelButton, CommonStrings.edit),
+        );
+        await tester.tap(editButtonFinder);
+        await tester.pumpAndSettle(const Duration(seconds: 2));
+
+        final projectGroupNameInputFinder = find.widgetWithText(
+          MetricsTextFormField,
+          projectGroupName,
+        );
+        await tester.tap(projectGroupNameInputFinder);
+        await tester.enterText(
+          projectGroupNameInputFinder,
+          updatedProjectGroupName,
+        );
+        await tester.pumpAndSettle(const Duration(seconds: 2));
+
+        final saveButtonFinder = find.widgetWithText(
+          MetricsPositiveButton,
+          ProjectGroupsStrings.saveChanges,
+        );
+        await tester.tap(saveButtonFinder);
+        await tester.pumpAndSettle(const Duration(seconds: 2));
+
+        final updatedProjectGroupCardFinder = find.widgetWithText(
+          ProjectGroupCard,
+          updatedProjectGroupName,
+        );
+
+        expect(updatedProjectGroupCardFinder, findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      "deletes a project group",
+      (WidgetTester tester) async {
+        await _pumpApp(tester);
+        await _openProjectGroupPage(tester);
+
+        final projectGroupCardFinder = find.widgetWithText(
+          ProjectGroupCard,
+          updatedProjectGroupName,
+        );
+
+        await tester.ensureVisible(projectGroupCardFinder);
+
+        final mouseRegion = tester.firstWidget<MouseRegion>(
+          find.descendant(
+            of: projectGroupCardFinder,
+            matching: find.byType(MouseRegion),
+          ),
+        );
+        const pointerEvent = PointerEnterEvent();
+        mouseRegion.onEnter(pointerEvent);
+        await tester.pumpAndSettle();
+
+        final deleteButtonFinder = find.descendant(
+          of: projectGroupCardFinder,
+          matching: find.widgetWithText(IconLabelButton, CommonStrings.delete),
+        );
+        await tester.tap(deleteButtonFinder);
+        await tester.pumpAndSettle();
+
+        await tester.tap(find.byType(MetricsNegativeButton));
+        await tester.pumpAndSettle(const Duration(seconds: 2));
+
+        expect(
+          find.widgetWithText(ProjectGroupCard, projectGroupName),
+          findsNothing,
+        );
       },
     );
   });
