2ddfb23e1e3c3759503800a6f9487bc7b54071e4
Daniyil Yevtyushkin <69351065+solid-daniyilyevtyushkin@users.noreply.github.com>
2021-01-05 18:16:40 +0200

    feat: Pass user agent using EventProcessor (#956)
    * Move files, implement SentryEventProcessors
    
    * Fix typo

---
+++ metrics/web/lib/metrics_logger/sentry/event_processors/user_agent_event_processor.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:metrics/metrics_logger/sentry/event_processors/sentry_event_processor.dart';
+import 'package:sentry/sentry.dart';
+
+/// A [SentryEventProcessor] that adds the user agent header
+/// to the [SentryEvent.request].
+class UserAgentEventProcessor implements SentryEventProcessor {
+  /// A a characteristic [String] that contains information about the
+  /// browser and operating system.
+  final String userAgent;
+
+  /// Creates a new instance of the [UserAgentEventProcessor] with the given
+  /// [userAgent].
+  const UserAgentEventProcessor(this.userAgent);
+
+  @override
+  SentryEvent call(SentryEvent event, {dynamic hint}) {
+    if (event == null) return event;
+
+    final request = event.request ?? Request();
+    final headers = request.headers ?? {};
+
+    final newHeaders = Map<String, String>.from(headers);
+    newHeaders[HttpHeaders.userAgentHeader] = userAgent;
+
+    final newRequest = request.copyWith(headers: newHeaders);
+
+    return event.copyWith(request: newRequest);
+  }
+}
