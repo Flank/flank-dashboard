7b0de13e048eb17bf5999497dfe5829a889fe3a4
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-01-14 15:34:38 +0200

    Update ProcessWrapper documentation

--- metrics/web/test_driver/cli/common/process/process_wrapper.dart
+++ metrics/web/test_driver/cli/common/process/process_wrapper.dart
@@ -9,20 +9,59 @@ import 'package:meta/meta.dart';
 /// We need to wrap the [Process] because it contains external static methods to start/run
 /// the processes, that are not inheriting in Dart.
 abstract class ProcessWrapper implements Process {
+  /// A [Process] of this wrapper.
   final Process _process;
+
+  /// A [StreamController] of the process errors.
   final StreamController<List<int>> _stderrController = StreamController();
+
+  /// A [StreamController] of the process output.
   final StreamController<List<int>> _stdoutController = StreamController();
-  final StreamController<int> _exitCodeController = StreamController();
 
+  /// A [StreamController] of the process exit code.
+  final StreamController<int> _exitCodeController =
+      StreamController.broadcast();
+
+  /// A [Stream] of the process errors.
   Stream<List<int>> _stderrBroadcast;
-  Stream<List<int>> _stdoutBroadcast;
-  Stream<int> _exitCodeBroadcast;
 
-  StreamSubscription _stdoutSubscription;
+  /// A [Stream] of the process output.
+  Stream<List<int>> _stdoutBroadcast;
+
+  /// A [StreamSubscription] of the process errors.
   StreamSubscription _stderrSubscription;
+
+  /// A [StreamSubscription] of the process output.
+  StreamSubscription _stdoutSubscription;
+
+  /// A [StreamSubscription] of the process exit code.
   StreamSubscription _exitCodeSubscription;
 
-  /// Creates the [ProcessWrapper] that delegates to the [_process].
+  @override
+  Future<int> get exitCode => _process.exitCode;
+
+  @override
+  int get pid => _process.pid;
+
+  @override
+  IOSink get stdin => _process.stdin;
+
+  @override
+  Stream<List<int>> get stdout => _stdoutController.stream;
+
+  @override
+  Stream<List<int>> get stderr => _stderrController.stream;
+
+  /// Provides the broadcast [Stream] of the process errors.
+  Stream<List<int>> get stderrBroadcast => _stderrBroadcast;
+
+  ///  Provides the broadcast [Stream] of the process output.
+  Stream<List<int>> get stdoutBroadcast => _stdoutBroadcast;
+
+  /// Provides the broadcast [Stream] of the process exit code.
+  Stream<int> get exitCodeBroadcast => _exitCodeController.stream;
+
+  /// Creates an instance of the [ProcessWrapper] with the given [Process].
   ProcessWrapper(this._process) {
     _stdoutSubscription = _process.stdout.listen(_stdoutController.add);
     _stdoutBroadcast = _stdoutController.stream.asBroadcastStream();
@@ -31,38 +70,14 @@ abstract class ProcessWrapper implements Process {
     _stderrBroadcast = _stderrController.stream.asBroadcastStream();
 
     _exitCodeSubscription = exitCode.asStream().listen(_exitCodeController.add);
-    _exitCodeBroadcast = _exitCodeController.stream.asBroadcastStream();
   }
-
-  @override
-  Future<int> get exitCode => _process.exitCode;
 
   @override
   bool kill([ProcessSignal signal = ProcessSignal.sigterm]) {
     return _process.kill(signal);
   }
 
-  @override
-  int get pid => _process.pid;
-
-  @override
-  Stream<List<int>> get stderr => _stderrController.stream;
-
-  /// The broadcast stream of the process errors.
-  Stream<List<int>> get stderrBroadcast => _stderrBroadcast;
-
-  Stream<int> get exitCodeBroadcast => _exitCodeBroadcast;
-
-  @override
-  IOSink get stdin => _process.stdin;
-
-  @override
-  Stream<List<int>> get stdout => _stdoutController.stream;
-
-  /// The broadcast stream of the process output.
-  Stream<List<int>> get stdoutBroadcast => _stdoutBroadcast;
-
-  /// Cancels creates subscriptions and closes the [StreamController]s.
+  /// Cancels [StreamSubscription]s and closes the [StreamController]s.
   @mustCallSuper
   void dispose() {
     _stdoutSubscription?.cancel();
