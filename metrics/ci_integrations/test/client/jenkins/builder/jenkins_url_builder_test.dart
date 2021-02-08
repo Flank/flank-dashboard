fa9ac85b146a32daacfc69c6fe251c3c284961fe
Illia Romanenko <illia.romanenko@gmail.com>
2021-02-08 19:41:46 +0200

    add license banner

--- metrics/ci_integrations/test/client/jenkins/builder/jenkins_url_builder_test.dart
+++ metrics/ci_integrations/test/client/jenkins/builder/jenkins_url_builder_test.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:ci_integration/client/jenkins/builder/jenkins_url_builder.dart';
 import 'package:ci_integration/client/jenkins/constants/jenkins_constants.dart';
 import 'package:ci_integration/client/jenkins/constants/tree_query.dart';
