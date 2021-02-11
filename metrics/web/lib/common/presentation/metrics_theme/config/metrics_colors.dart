8f5861decf0334a97f605d7466d8911de105fdbd
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-02-11 18:04:13 +0200

    Add MetricsColors class

---
+++ metrics/web/lib/common/presentation/metrics_theme/config/metrics_colors.dart
@@ -0,0 +1,44 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+// ignore_for_file: public_member_api_docs
+
+/// A class that holds main colors used in the application.
+class MetricsColors {
+  static const Color black = Color(0xFF000000);
+
+  static const Color gray100 = Color(0xFFF5F8FA);
+  static const Color gray200 = Color(0xFFDCDCE3);
+  static const Color gray300 = Color(0xFF878799);
+  static const Color gray400 = Color(0xFF4F4F56);
+  static const Color gray500 = Color(0xFF3B3B41);
+  static const Color gray600 = Color(0xFF303033);
+  static const Color gray700 = Color(0xFF252528);
+  static const Color gray800 = Color(0xFF1B1B1D);
+  static const Color gray900 = Color(0xFF0D0D0D);
+
+  static const Color green100 = Color(0xFFE6F9F3);
+  static const Color green200 = Color(0xFFB6E3D5);
+  static const Color green500 = Color(0xFF20CE9A);
+  static const Color green600 = Color(0xFF1EB284);
+  static const Color green800 = Color(0xFF07372F);
+  static const Color green900 = Color(0xFF182B27);
+
+  static const Color orange100 = Color(0xFFFFF5F3);
+  static const Color orange200 = Color(0xFFF9BCAE);
+  static const Color orange500 = Color(0xFFF45531);
+  static const Color orange600 = Color(0xFFE2431F);
+  static const Color orange800 = Color(0xFF5A281F);
+  static const Color orange900 = Color(0xFF2D1F1F);
+
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color yellow100 = Color(0xFFFAF6E6);
+  static const Color yellow200 = Color(0xFFFCEEB0);
+  static const Color yellow500 = Color(0xFFFED100);
+  static const Color yellow600 = Color(0xFFCCA800);
+  static const Color yellow800 = Color(0xFF54480C);
+  static const Color yellow900 = Color(0xFF292618);
+}
