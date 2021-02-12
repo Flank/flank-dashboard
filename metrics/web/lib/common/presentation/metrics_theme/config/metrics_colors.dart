6a83799488ecb01a285b511693fd58673f83e413
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-02-12 14:53:48 +0200

    Migrate MetricsColors to use MetricsColor class

--- metrics/web/lib/common/presentation/metrics_theme/config/metrics_colors.dart
+++ metrics/web/lib/common/presentation/metrics_theme/config/metrics_colors.dart
@@ -2,6 +2,7 @@
 // that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:metrics/common/presentation/metrics_theme/model/metrics_color/metrics_color.dart';
 
 // ignore_for_file: public_member_api_docs
 
@@ -9,36 +10,58 @@ import 'package:flutter/material.dart';
 class MetricsColors {
   static const Color black = Color(0xFF000000);
 
-  static const Color gray100 = Color(0xFFF5F8FA);
-  static const Color gray200 = Color(0xFFDCDCE3);
-  static const Color gray300 = Color(0xFF878799);
-  static const Color gray400 = Color(0xFF4F4F56);
-  static const Color gray500 = Color(0xFF3B3B41);
-  static const Color gray600 = Color(0xFF303033);
-  static const Color gray700 = Color(0xFF252528);
-  static const Color gray800 = Color(0xFF1B1B1D);
-  static const Color gray900 = Color(0xFF0D0D0D);
+  static const MetricsColor gray = MetricsColor(
+    0xFF3B3B41,
+    <int, Color>{
+      100: Color(0xFFF5F8FA),
+      200: Color(0xFFDCDCE3),
+      300: Color(0xFF878799),
+      400: Color(0xFF4F4F56),
+      500: Color(0xFF3B3B41),
+      600: Color(0xFF303033),
+      700: Color(0xFF252528),
+      800: Color(0xFF1B1B1D),
+      900: Color(0xFF0D0D0D),
+    },
+  );
 
-  static const Color green100 = Color(0xFFE6F9F3);
-  static const Color green200 = Color(0xFFB6E3D5);
-  static const Color green500 = Color(0xFF20CE9A);
-  static const Color green600 = Color(0xFF1EB284);
-  static const Color green800 = Color(0xFF07372F);
-  static const Color green900 = Color(0xFF182B27);
+  static const MetricsColor green = MetricsColor(
+    0xFF20CE9A,
+    <int, Color>{
+      100: Color(0xFFE6F9F3),
+      200: Color(0xFFB6E3D5),
+      500: Color(0xFF20CE9A),
+      600: Color(0xFF1EB284),
+      800: Color(0xFF07372F),
+      900: Color(0xFF182B27),
+    },
+  );
 
-  static const Color orange100 = Color(0xFFFFF5F3);
-  static const Color orange200 = Color(0xFFF9BCAE);
-  static const Color orange500 = Color(0xFFF45531);
-  static const Color orange600 = Color(0xFFE2431F);
-  static const Color orange800 = Color(0xFF5A281F);
-  static const Color orange900 = Color(0xFF2D1F1F);
+  static const MetricsColor orange = MetricsColor(
+    0xFFF45531,
+    <int, Color>{
+      100: Color(0xFFFFF5F3),
+      200: Color(0xFFF9BCAE),
+      500: Color(0xFFF45531),
+      600: Color(0xFFE2431F),
+      800: Color(0xFF5A281F),
+      900: Color(0xFF2D1F1F),
+    },
+  );
+
+  static const Color shadowColor = Color.fromRGBO(0, 0, 0, 0.5);
 
   static const Color white = Color(0xFFFFFFFF);
 
-  static const Color yellow100 = Color(0xFFFAF6E6);
-  static const Color yellow200 = Color(0xFFFCEEB0);
-  static const Color yellow500 = Color(0xFFFED100);
-  static const Color yellow600 = Color(0xFFCCA800);
-  static const Color yellow800 = Color(0xFF54480C);
-  static const Color yellow900 = Color(0xFF292618);
+  static const MetricsColor yellow = MetricsColor(
+    0xFFFED100,
+    <int, Color>{
+      100: Color(0xFFFAF6E6),
+      200: Color(0xFFFCEEB0),
+      500: Color(0xFFFED100),
+      600: Color(0xFFCCA800),
+      800: Color(0xFF54480C),
+      900: Color(0xFF292618),
+    },
+  );
 }
