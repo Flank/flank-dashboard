79b05e5bc59c5c607c38b64cac8ac629e7024a77
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-02-17 16:09:34 +0200

    Fix license headers

--- metrics/ci_integrations/lib/source/buildkite/config/validator/buildkite_source_validator.dart
+++ metrics/ci_integrations/lib/source/buildkite/config/validator/buildkite_source_validator.dart
@@ -1,18 +1,3 @@
-/*
- * Use of this source code is governed by the Apache License, Version 2.0
- * that can be found in the LICENSE file.
- */
-
-/*
- * Use of this source code is governed by the Apache License, Version 2.0
- * that can be found in the LICENSE file.
- */
-
-/*
- * Use of this source code is governed by the Apache License, Version 2.0
- * that can be found in the LICENSE file.
- */
-
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
