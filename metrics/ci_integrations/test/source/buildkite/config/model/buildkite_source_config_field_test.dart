982d0e1cf91ad3be6be6376f6e00083213986380
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-02-17 16:06:36 +0200

    Add BuildkiteSourceConfigField

---
+++ metrics/ci_integrations/test/source/buildkite/config/model/buildkite_source_config_field_test.dart
@@ -0,0 +1,25 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:ci_integration/source/buildkite/config/model/buildkite_source_config_field.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("BuildkiteSourceConfigField", () {
+    test(
+      ".values contains all buildkite source config fields",
+      () {
+        final expectedConfigFields = [
+          BuildkiteSourceConfigField.accessToken,
+          BuildkiteSourceConfigField.organizationSlug,
+          BuildkiteSourceConfigField.pipelineSlug,
+        ];
+
+        expect(
+          BuildkiteSourceConfigField.values,
+          containsAll(expectedConfigFields),
+        );
+      },
+    );
+  });
+}
