0bdecbc48b5fa61432a0c9b7e529a647a08417a3
solid-maxim <maxim.ustych@solid.software>
2021-02-22 14:34:47 +0200

    Add more tests for Github Actions models

---
+++ metrics/ci_integrations/test/client/github_actions/models/github_user_test.dart
@@ -0,0 +1,77 @@
+import 'package:ci_integration/client/github_actions/models/github_user.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("GithubUser", () {
+    const id = 1;
+    const login = 'login';
+
+    const githubUserJson = {
+      'id': id,
+      'login': login,
+    };
+
+    const githubUser = GithubUser(id: id, login: login);
+
+    test(
+      "creates an instance with the given parameters",
+      () {
+        const githubUser = GithubUser(id: id, login: login);
+
+        expect(githubUser.id, equals(id));
+        expect(githubUser.login, equals(login));
+      },
+    );
+
+    test(
+      ".fromJson() returns null if the given json is null",
+      () {
+        final githubUser = GithubUser.fromJson(null);
+
+        expect(githubUser, isNull);
+      },
+    );
+
+    test(
+      ".listFromJson() returns null if the given list is null",
+      () {
+        final list = GithubUser.listFromJson(null);
+
+        expect(list, isNull);
+      },
+    );
+
+    test(
+      ".listFromJson() returns an empty list if the given one is empty",
+      () {
+        final list = GithubUser.listFromJson([]);
+
+        expect(list, isEmpty);
+      },
+    );
+
+    test(
+      ".listFromJson() creates a list of Github users from the given list of JSON encodable objects",
+      () {
+        const anotherId = 2;
+        const anotherJson = {'id': anotherId};
+        const anotherGithubUser = GithubUser(id: anotherId);
+        const jsonList = [githubUserJson, anotherJson];
+        const expectedList = [githubUser, anotherGithubUser];
+
+        final pipelineList = GithubUser.listFromJson(jsonList);
+
+        expect(pipelineList, equals(expectedList));
+      },
+    );
+
+    test(
+      ".toJson() converts an instance to the json encodable map",
+      () {
+        final json = githubUser.toJson();
+
+        expect(json, equals(githubUserJson));
+      },
+    );
+  });
+}
---
+++ metrics/ci_integrations/test/client/github_actions/models/workflow_test.dart
@@ -0,0 +1,93 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:ci_integration/client/github_actions/models/workflow.dart';
+import 'package:test/test.dart';
+
+// ignore_for_file: avoid_redundant_argument_values
+
+void main() {
+  group("Workflow", () {
+    const id = 1;
+    const name = 'name';
+    const path = 'path';
+
+    const workflowJson = {
+      'id': id,
+      'name': name,
+      'path': path,
+    };
+
+    const expectedWorkflow = Workflow(id: id, name: name, path: path);
+
+    test(
+      "creates an instance with the given values",
+      () {
+        const workflow = Workflow(id: id, name: name, path: path);
+
+        expect(workflow.id, equals(id));
+        expect(workflow.name, equals(name));
+        expect(workflow.path, equals(path));
+      },
+    );
+
+    test(
+      ".fromJson() returns null if the given json is null",
+      () {
+        final workflow = Workflow.fromJson(null);
+
+        expect(workflow, isNull);
+      },
+    );
+
+    test(
+      ".fromJson() creates an instance from the given json",
+      () {
+        final workflow = Workflow.fromJson(workflowJson);
+
+        expect(workflow, equals(expectedWorkflow));
+      },
+    );
+
+    test(
+      ".listFromJson() returns null if the given list is null",
+      () {
+        final workflows = Workflow.listFromJson(null);
+
+        expect(workflows, isNull);
+      },
+    );
+
+    test(
+      ".listFromJson() returns an empty list if the given one is empty",
+      () {
+        final workflows = Workflow.listFromJson([]);
+
+        expect(workflows, isEmpty);
+      },
+    );
+
+    test(
+      ".listFromJson() creates a list of workflows from the given list of JSON encodable objects",
+      () {
+        final anotherJson = {'id': 2, 'name': 'name2', 'path': 'path2'};
+        final anotherWorkflow = Workflow.fromJson(anotherJson);
+        final jsonList = [workflowJson, anotherJson];
+        final expectedList = [expectedWorkflow, anotherWorkflow];
+
+        final workflows = Workflow.listFromJson(jsonList);
+
+        expect(workflows, equals(expectedList));
+      },
+    );
+
+    test(
+      ".toJson() converts an instance to the json encodable map",
+      () {
+        final json = expectedWorkflow.toJson();
+
+        expect(json, equals(workflowJson));
+      },
+    );
+  });
+}
