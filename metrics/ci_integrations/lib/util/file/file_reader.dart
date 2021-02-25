9d4c26ab96f12ada87104b153d8e9312b16c7961
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-02-25 11:35:46 +0200

    Implement FileHelper and FileReader

---
+++ metrics/ci_integrations/lib/util/file/file_reader.dart
@@ -0,0 +1,33 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:ci_integration/util/file/file_helper.dart';
+
+/// A class that provides methods for reading [File]s.
+class FileReader {
+  /// A [FileHelper] of this reader uses to work with the file system.
+  final FileHelper fileHelper;
+
+  /// Creates a new instance of the [FileReader] with the given [fileHelper].
+  ///
+  /// If the given [fileHelper] is `null`, an instance of the [FileHelper]
+  /// is used.
+  const FileReader(
+    FileHelper fileHelper,
+  ) : fileHelper = fileHelper ?? const FileHelper();
+
+  /// Returns the contents of the [File] located by the given [path].
+  ///
+  /// Throws a [StateError] if the [File] with the given [path] does not exist.
+  String read(String path) {
+    final file = fileHelper.getFile(path);
+
+    if (!file.existsSync()) {
+      throw StateError('The file with the given path is not found.');
+    }
+
+    return file.readAsStringSync();
+  }
+}
---
+++ metrics/ci_integrations/test/util/file/file_reader_test.dart
@@ -0,0 +1,81 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:ci_integration/util/file/file_helper.dart';
+import 'package:ci_integration/util/file/file_reader.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import '../../cli/test_util/mock/file_mock.dart';
+
+void main() {
+  group("FileReader", () {
+    const path = 'path';
+    const content = 'content';
+
+    final fileHelper = _FileHelperMock();
+    final file = FileMock();
+
+    final reader = FileReader(fileHelper);
+
+    tearDown(() {
+      reset(fileHelper);
+      reset(file);
+    });
+
+    test(
+      "creates an instance with the given file helper",
+      () {
+        final reader = FileReader(fileHelper);
+
+        expect(reader.fileHelper, equals(fileHelper));
+      },
+    );
+
+    test(
+      "creates an instance with the default file helper, if the given file helper is null",
+      () {
+        const reader = FileReader(null);
+
+        expect(reader.fileHelper, isNotNull);
+      },
+    );
+
+    test(
+      ".read() gets the file with the given path using the given file helper",
+      () {
+        when(fileHelper.getFile(path)).thenReturn(file);
+        when(file.existsSync()).thenReturn(true);
+
+        reader.read(path);
+
+        verify(fileHelper.getFile(path)).called(1);
+      },
+    );
+
+    test(
+      ".read() throws a StateError if the file with the given path does not exist",
+      () {
+        when(fileHelper.getFile(path)).thenReturn(file);
+        when(file.existsSync()).thenReturn(false);
+
+        expect(() => reader.read(path), throwsStateError);
+      },
+    );
+
+    test(
+      ".read() returns the contents of the file",
+      () {
+        when(fileHelper.getFile(path)).thenReturn(file);
+        when(file.existsSync()).thenReturn(true);
+        when(file.readAsStringSync()).thenReturn(content);
+
+        final fileContent = reader.read(path);
+
+        expect(fileContent, equals(content));
+      },
+    );
+  });
+}
+
+class _FileHelperMock extends Mock implements FileHelper {}
