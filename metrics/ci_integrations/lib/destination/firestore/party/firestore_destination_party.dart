be7fa7fcbd4a5162907425ed593042ca79c2804f
Daniyil Yevtyushkin <69351065+solid-daniyilyevtyushkin@users.noreply.github.com>
2021-02-25 18:50:23 +0200

    feat: Inject ConfigValidatorFactory to IntegrationParty interface (#1125)
    * Create config validation stubs, include config validator factories in integration parties
    
    * Add missing tests
    
    * Update stub package structure
    
    * Update source parties
    
    * Update destination parties
    
    * Minor improvements. Add tests for stubs
    
    * Add license
    
    * Minor improvements
    
    Co-authored-by: Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>

--- metrics/ci_integrations/lib/destination/firestore/party/firestore_destination_party.dart
+++ metrics/ci_integrations/lib/destination/firestore/party/firestore_destination_party.dart
@@ -1,11 +1,13 @@
-// Use of this source code is governed by the Apache License, Version 2.0 
+// Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
 import 'package:ci_integration/destination/firestore/adapter/firestore_destination_client_adapter.dart';
 import 'package:ci_integration/destination/firestore/client_factory/firestore_destination_client_factory.dart';
 import 'package:ci_integration/destination/firestore/config/model/firestore_destination_config.dart';
 import 'package:ci_integration/destination/firestore/config/parser/firestore_destination_config_parser.dart';
+import 'package:ci_integration/integration/interface/base/config/validator_factory/config_validator_factory.dart';
 import 'package:ci_integration/integration/interface/destination/party/destination_party.dart';
+import 'package:ci_integration/integration/stub/base/config/validator_factory/config_validator_factory_stub.dart';
 
 /// An integration party for the Firestore destination integration.
 class FirestoreDestinationParty
@@ -19,4 +21,8 @@ class FirestoreDestinationParty
   @override
   final FirestoreDestinationConfigParser configParser =
       const FirestoreDestinationConfigParser();
+
+  @override
+  final ConfigValidatorFactory<FirestoreDestinationConfig>
+      configValidatorFactory = const ConfigValidatorFactoryStub();
 }
