b410f3077ee1eb9535d3e29f0cb6fa74b5763528
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-02-25 17:00:00 +0200

    Add license

--- metrics/ci_integrations/test/test_utils/config_stub.dart
+++ metrics/ci_integrations/test/test_utils/config_stub.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:ci_integration/integration/interface/base/config/model/config.dart';
 
 /// A stub implementation of the [Config] class to use in tests.
