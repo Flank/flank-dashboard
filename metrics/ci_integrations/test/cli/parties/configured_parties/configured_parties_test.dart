f2181aec3c1c7c3a1839b58b76d11e016fe4e217
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-03-01 19:16:49 +0200

    Add ConfiguredParties

---
+++ metrics/ci_integrations/test/cli/parties/configured_parties/configured_parties_test.dart
@@ -0,0 +1,66 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:ci_integration/cli/parties/configured_parties/configured_destination_party.dart';
+import 'package:ci_integration/cli/parties/configured_parties/configured_parties.dart';
+import 'package:ci_integration/cli/parties/configured_parties/configured_source_party.dart';
+import 'package:test/test.dart';
+
+import '../../test_util/destination_config_stub.dart';
+import '../../test_util/destination_party_stub.dart';
+import '../../test_util/source_config_stub.dart';
+import '../../test_util/source_party_stub.dart';
+
+void main() {
+  group("ConfiguredParties", () {
+    final sourceConfig = SourceConfigStub();
+    final sourceParty = SourcePartyStub();
+    final configuredSourceParty = ConfiguredSourceParty(
+      sourceConfig,
+      sourceParty,
+    );
+    final destinationConfig = DestinationConfigStub();
+    final destinationParty = DestinationPartyStub();
+    final configuredDestinationParty = ConfiguredDestinationParty(
+      destinationConfig,
+      destinationParty,
+    );
+
+    test(
+      "throws an ArgumentError if the given configured source party is null",
+      () {
+        expect(
+          () => ConfiguredParties(null, configuredDestinationParty),
+          throwsArgumentError,
+        );
+      },
+    );
+
+    test(
+      "throws an ArgumentError if the given configured destination party is null",
+      () {
+        expect(
+          () => ConfiguredParties(configuredSourceParty, null),
+          throwsArgumentError,
+        );
+      },
+    );
+
+    test(
+      "creates an instance with the given parameters",
+      () {
+        final configuredParties = ConfiguredParties(
+          configuredSourceParty,
+          configuredDestinationParty,
+        );
+
+        final resultSourceParty = configuredParties.configuredSourceParty;
+        final resultDestinationParty =
+            configuredParties.configuredDestinationParty;
+
+        expect(resultSourceParty, equals(configuredSourceParty));
+        expect(resultDestinationParty, equals(configuredDestinationParty));
+      },
+    );
+  });
+}
