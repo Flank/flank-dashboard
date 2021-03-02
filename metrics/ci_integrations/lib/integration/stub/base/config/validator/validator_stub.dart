e86f45bdf2a6288c00d12021e277c91c0ffa65d7
solid-tetianamovlian <tetiana.movlian@solid.software>
2021-03-02 15:26:49 +0200

    Minor fix

--- metrics/ci_integrations/lib/integration/stub/base/config/validator/validator_stub.dart
+++ metrics/ci_integrations/lib/integration/stub/base/config/validator/validator_stub.dart
@@ -4,9 +4,9 @@
 import 'package:ci_integration/integration/interface/base/config/model/config.dart';
 import 'package:ci_integration/integration/interface/base/config/validation_delegate/validation_delegate.dart';
 import 'package:ci_integration/integration/interface/base/config/validator/config_validator.dart';
-import 'package:ci_integration/integration/stub/base/config/validation_delegate/validation_delegate_stub.dart';
 import 'package:ci_integration/integration/validation/model/validation_result.dart';
 import 'package:ci_integration/integration/validation/model/validation_result_builder.dart';
+import 'package:ci_integration/integration/stub/base/config/validation_delegate/validation_delegate_stub.dart';
 
 /// A stub implementation of the [ConfigValidator].
 class ValidatorStub<T extends Config> implements ConfigValidator<T> {
