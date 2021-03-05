f39953ba02677386911f21fd652b129439815048
solid-yevheniiashchypeleva <74961502+solid-yevheniiashchypeleva@users.noreply.github.com>
2021-03-05 12:59:40 +0200

    feat: Implement ValidationResultPrinter (#1154)
    * Implement ValidationResultPrinter
    
    * Improve formatting, fix mock in io_logger_writer_test.dart
    
    * Minor improvements
    
    * Improve tests
    
    * Fix test
    
    Co-authored-by: solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>

---
+++ metrics/ci_integrations/lib/integration/validation/printer/validation_result_printer.dart
@@ -0,0 +1,100 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:ci_integration/integration/interface/base/config/model/config_field.dart';
+import 'package:ci_integration/integration/validation/model/field_validation_conclusion.dart';
+import 'package:ci_integration/integration/validation/model/field_validation_result.dart';
+import 'package:ci_integration/integration/validation/model/mappers/field_validation_conclusion_mapper.dart';
+import 'package:ci_integration/integration/validation/model/validation_result.dart';
+
+/// A class that is responsible for printing the [ValidationResult].
+class ValidationResultPrinter {
+  /// An instance of the [IOSink] this printer uses to print the result.
+  final IOSink ioSink;
+
+  /// Create a new instance of the [ValidationResultPrinter] with the given
+  /// [ioSink].
+  ///
+  /// If the [ioSink] is `null`, an instance of the standard output stream is
+  /// used.
+  ValidationResultPrinter({
+    IOSink ioSink,
+  }) : ioSink = ioSink ?? stdout;
+
+  /// Prints the given [result] to the [ioSink].
+  void print(ValidationResult result) {
+    final validationResults = result.results;
+
+    final validationResultEntries = validationResults.entries;
+
+    for (final entry in validationResultEntries) {
+      final field = entry.key;
+      final result = entry.value;
+
+      final message = _buildFieldValidationOutput(
+        field,
+        result,
+      );
+
+      ioSink.writeln(message);
+    }
+  }
+
+  /// Builds the output that provides the information
+  /// about the [fieldValidationResult] for the given [field].
+  String _buildFieldValidationOutput(
+    ConfigField field,
+    FieldValidationResult fieldValidationResult,
+  ) {
+    final conclusionMarker = _getConclusionMarker(
+      fieldValidationResult.conclusion,
+    );
+    final fieldName = field.value;
+    final result = _getFieldValidationResultMessage(fieldValidationResult);
+
+    return '[$conclusionMarker] $fieldName: $result';
+  }
+
+  /// Converts the given [validationResult] to its [String] representation.
+  ///
+  /// Converts the [validationResult.conclusion] to the [String] representation
+  /// using the [FieldValidationConclusionMapper].
+  ///
+  /// Specifies the [validationResult.additionalContext] if it is not `null`.
+  String _getFieldValidationResultMessage(
+    FieldValidationResult validationResult,
+  ) {
+    const conclusionMapper = FieldValidationConclusionMapper();
+
+    final conclusionValue = conclusionMapper.unmap(validationResult.conclusion);
+
+    final resultMessage = '$conclusionValue.';
+
+    final additionalContext = validationResult.additionalContext;
+    if (additionalContext != null) {
+      return '$resultMessage Additional context: $additionalContext';
+    }
+
+    return resultMessage;
+  }
+
+  /// Returns a marker that illustrates the given [conclusion].
+  ///
+  /// Returns `+` if the given [conclusion] is [FieldValidationConclusion.valid].
+  /// Returns `-` if the given [conclusion] is [FieldValidationConclusion.invalid].
+  /// Returns `?` if the given [conclusion] is [FieldValidationConclusion.unknown].
+  /// Otherwise, returns `null`.
+  String _getConclusionMarker(FieldValidationConclusion conclusion) {
+    switch (conclusion) {
+      case FieldValidationConclusion.valid:
+        return '+';
+      case FieldValidationConclusion.invalid:
+        return '-';
+      case FieldValidationConclusion.unknown:
+        return '?';
+    }
+    return null;
+  }
+}
---
+++ metrics/ci_integrations/test/integration/validation/printer/validation_result_printer_test.dart
@@ -0,0 +1,300 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:ci_integration/integration/interface/base/config/model/config_field.dart';
+import 'package:ci_integration/integration/validation/model/field_validation_conclusion.dart';
+import 'package:ci_integration/integration/validation/model/field_validation_result.dart';
+import 'package:ci_integration/integration/validation/model/mappers/field_validation_conclusion_mapper.dart';
+import 'package:ci_integration/integration/validation/model/validation_result.dart';
+import 'package:ci_integration/integration/validation/printer/validation_result_printer.dart';
+import 'package:ci_integration/source/buildkite/config/model/buildkite_source_config_field.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import '../../../test_utils/mock/io_sink_mock.dart';
+
+// ignore_for_file: avoid_redundant_argument_values
+
+void main() {
+  group("ValidationResultPrinter", () {
+    final ioSink = IOSinkMock();
+
+    const additionalContext = 'context';
+
+    final firstField = BuildkiteSourceConfigField.accessToken;
+    final secondField = BuildkiteSourceConfigField.organizationSlug;
+    final thirdField = BuildkiteSourceConfigField.pipelineSlug;
+
+    const successResult = FieldValidationResult.success(additionalContext);
+    const failureResult = FieldValidationResult.failure();
+    const unknownResult = FieldValidationResult.unknown();
+
+    final resultPrinter = ValidationResultPrinter(
+      ioSink: ioSink,
+    );
+
+    Matcher startsWithConclusionMarker(FieldValidationConclusion conclusion) {
+      String conclusionMarker;
+      switch (conclusion) {
+        case FieldValidationConclusion.valid:
+          conclusionMarker = '+';
+          break;
+        case FieldValidationConclusion.invalid:
+          conclusionMarker = '-';
+          break;
+        case FieldValidationConclusion.unknown:
+          conclusionMarker = '?';
+          break;
+      }
+
+      return startsWith('[$conclusionMarker]');
+    }
+
+    Matcher containsFieldName(ConfigField field) {
+      return contains(field.value);
+    }
+
+    Matcher containsFieldValidationConclusion(
+      FieldValidationConclusion conclusion,
+    ) {
+      const conclusionMapper = FieldValidationConclusionMapper();
+
+      final expectedConclusion = conclusionMapper.unmap(conclusion);
+
+      return contains(expectedConclusion);
+    }
+
+    Matcher endsWithAdditionalContext(String additionalContext) {
+      return endsWith('Additional context: $additionalContext');
+    }
+
+    Matcher validationResultMessageMatcher(
+      ConfigField field,
+      FieldValidationResult result,
+    ) {
+      final conclusion = result.conclusion;
+      final additionalContext = result.additionalContext;
+
+      final additionalContextMatcher = additionalContext != null
+          ? endsWithAdditionalContext(additionalContext)
+          : isNot(endsWithAdditionalContext(additionalContext));
+
+      return allOf(
+        startsWithConclusionMarker(conclusion),
+        containsFieldName(field),
+        containsFieldValidationConclusion(conclusion),
+        additionalContextMatcher,
+      );
+    }
+
+    tearDown(() {
+      reset(ioSink);
+    });
+
+    test(
+      "creates an instance with the default stdout iosink, if the given one is null",
+      () {
+        final printer = ValidationResultPrinter(ioSink: null);
+
+        expect(printer.ioSink, equals(stdout));
+      },
+    );
+
+    test(
+      "creates an instance with the given iosink",
+      () {
+        final printer = ValidationResultPrinter(ioSink: ioSink);
+
+        expect(printer.ioSink, equals(ioSink));
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink with the '+' conclusion marker for the successful field validation result",
+      () {
+        final validationResult = ValidationResult({
+          firstField: successResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        verify(ioSink.writeln(argThat(
+          startsWithConclusionMarker(successResult.conclusion),
+        ))).called(1);
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink with the '-' conclusion marker for the failure field validation result",
+      () {
+        final validationResult = ValidationResult({
+          firstField: failureResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        verify(ioSink.writeln(argThat(
+          startsWithConclusionMarker(failureResult.conclusion),
+        ))).called(1);
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink with the '?' conclusion marker for the unknown field validation result",
+      () {
+        final validationResult = ValidationResult({
+          firstField: unknownResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        verify(ioSink.writeln(argThat(
+          startsWithConclusionMarker(unknownResult.conclusion),
+        ))).called(1);
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink containing the validated field name",
+      () {
+        final validationResult = ValidationResult({
+          firstField: successResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        verify(
+          ioSink.writeln(argThat(containsFieldName(firstField))),
+        ).called(1);
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink with the string representation of the successful field validation conclusion",
+      () {
+        final validationResult = ValidationResult({
+          firstField: successResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        verify(ioSink.writeln(argThat(
+          containsFieldValidationConclusion(successResult.conclusion),
+        ))).called(1);
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink with the string representation of the failure field validation conclusion",
+      () {
+        final validationResult = ValidationResult({
+          firstField: failureResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        verify(ioSink.writeln(argThat(
+          containsFieldValidationConclusion(failureResult.conclusion),
+        ))).called(1);
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink with the string representation of the unknown field validation conclusion",
+      () {
+        final validationResult = ValidationResult({
+          firstField: unknownResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        verify(ioSink.writeln(argThat(
+          containsFieldValidationConclusion(unknownResult.conclusion),
+        ))).called(1);
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink with the additional context if it is not null",
+      () {
+        final validationResult = ValidationResult({
+          firstField: successResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        verify(ioSink.writeln(argThat(
+          endsWithAdditionalContext(successResult.additionalContext),
+        ))).called(1);
+      },
+    );
+
+    test(
+      ".print() prints the validation result message to the iosink without the additional context if it is null",
+      () {
+        final validationResult = ValidationResult({
+          firstField: successResult,
+        });
+
+        resultPrinter.print(validationResult);
+
+        final containsAdditionalContext = contains('Additional Context');
+
+        verify(
+          ioSink.writeln(argThat(isNot(containsAdditionalContext))),
+        ).called(1);
+      },
+    );
+
+    test(
+      ".print() prints validation result messages for all fields",
+      () {
+        final results = {
+          firstField: successResult,
+          secondField: failureResult,
+          thirdField: unknownResult,
+        };
+        final validationResult = ValidationResult(results);
+
+        resultPrinter.print(validationResult);
+
+        results.forEach((field, result) {
+          verify(
+            ioSink.writeln(argThat(
+              validationResultMessageMatcher(field, result),
+            )),
+          ).called(1);
+        });
+      },
+    );
+
+    test(
+      ".print() prints the validation result in the given order",
+      () {
+        final results = {
+          firstField: successResult,
+          secondField: failureResult,
+          thirdField: unknownResult,
+        };
+        final validationResult = ValidationResult(results);
+
+        resultPrinter.print(validationResult);
+
+        final expectedMessages = results.entries.map((entry) {
+          final field = entry.key;
+          final result = entry.value;
+
+          return argThat(
+            validationResultMessageMatcher(field, result),
+          );
+        });
+
+        verifyInOrder(
+          expectedMessages.map(ioSink.writeln).toList(),
+        );
+      },
+    );
+  });
+}
