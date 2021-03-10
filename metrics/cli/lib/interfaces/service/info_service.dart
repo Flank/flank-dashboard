020e4de222917c54a6ac50ff9127ccad121b059f
solid-dimakoniaiev <dmytro.koniaiev@solid.software>
2021-03-10 19:34:13 +0200

    Fix suggestions

--- metrics/cli/lib/interfaces/service/info_service.dart
+++ metrics/cli/lib/interfaces/service/info_service.dart
@@ -1,8 +1,9 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-/// A base class for services that provides common methods for them.
+/// A base class for info services that provides common methods
+/// for getting information about service.
 abstract class InfoService {
-  /// Shows the version information of the tool used by the service.
+  /// Shows the version information of this service.
   Future<void> version();
 }
