50f507e5c847dad427a55df51bfa096f17bd80f6
Yurii Uvarov <yurii.uvarov@solid.software>
2021-03-10 15:18:54 +0200

    Add license

--- metrics/cli/lib/interfaces/cli/cli.dart
+++ metrics/cli/lib/interfaces/cli/cli.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 /// A base class for CLIs that provides common methods for them.
 abstract class Cli {
   /// Shows the version information of this CLI.
--- metrics/cli/lib/interfaces/service/info_service.dart
+++ metrics/cli/lib/interfaces/service/info_service.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 /// A base class for services that provides common methods for them.
 abstract class InfoService {
   /// Shows the version information of the tool used by the service.
