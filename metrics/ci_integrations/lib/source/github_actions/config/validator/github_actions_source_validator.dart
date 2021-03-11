c6c8c4eee644a359473fe76b11dadfba971f8134
solid-tetianamovlian <tetiana.movlian@solid.software>
2021-03-11 09:50:15 +0200

    Add license

--- metrics/ci_integrations/lib/source/github_actions/config/validator/github_actions_source_validator.dart
+++ metrics/ci_integrations/lib/source/github_actions/config/validator/github_actions_source_validator.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:ci_integration/integration/interface/base/config/validator/config_validator.dart';
 import 'package:ci_integration/integration/validation/model/field_validation_result.dart';
 import 'package:ci_integration/integration/validation/model/validation_result.dart';
