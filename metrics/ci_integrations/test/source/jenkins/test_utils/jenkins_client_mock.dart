1fbd076842f02c49f7cd5dfa6a5e21f789b34057
Daniyil Yevtyushkin <69351065+solid-daniyilyevtyushkin@users.noreply.github.com>
2021-03-11 15:57:26 +0200

    feat: Implement Jenkins Source Validation Delegate (#1168)
    * Implement Jenkins Source Validation Delegate
    
    * Implement Jenkins Source Validation Delegate tests
    
    * Minor code improvement
    
    * Minor code and test improvements
    
    * Improve tests
    
    * Fix tests
    
    * Minor test description improvements
    
    * Minor test description improvement
    
    Co-authored-by: Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
    Co-authored-by: vova.beloded <vova.beloded@solid.software>

---
+++ metrics/ci_integrations/test/source/jenkins/test_utils/jenkins_client_mock.dart
@@ -0,0 +1,7 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:ci_integration/client/jenkins/jenkins_client.dart';
+import 'package:mockito/mockito.dart';
+
+class JenkinsClientMock extends Mock implements JenkinsClient {}
