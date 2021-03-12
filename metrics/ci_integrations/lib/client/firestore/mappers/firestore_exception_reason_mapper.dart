daa6e4fcf8d8e6876d7956a641acf898fdbee48f
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-03-12 15:45:15 +0200

    Add Firestore exception reason. Remove not needed files

---
+++ metrics/ci_integrations/lib/client/firestore/mappers/firestore_exception_reason_mapper.dart
@@ -0,0 +1,66 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:ci_integration/client/firestore/model/firestore_exception_reason.dart';
+import 'package:ci_integration/integration/interface/base/client/mapper/mapper.dart';
+
+/// A class that provides methods for mapping [FirestoreExceptionReason]s.
+class FirestoreExceptionReasonMapper
+    implements Mapper<String, FirestoreExceptionReason> {
+  /// An exception reason indicating that the provided Firebase project ID is not
+  /// valid or does not exist.
+  static const String consumerInvalid = 'CONSUMER_INVALID';
+
+  /// An exception reason indicating that the provided Firebase with the given
+  /// project ID is not found.
+  static const String notFound = 'NOT_FOUND';
+
+  /// An exception reason indicating that the provided Firebase project is
+  /// invalid.
+  static const String projectDeleted = 'PROJECT_DELETED';
+
+  /// An exception reason indicating that the provided Firebase project is
+  /// deleted.
+  static const String projectInvalid = 'PROJECT_INVALID';
+
+  /// Creates a new intance of the [FirestoreExceptionReasonMapper].
+  const FirestoreExceptionReasonMapper();
+
+  @override
+  FirestoreExceptionReason map(String value) {
+    switch (value) {
+      case consumerInvalid:
+        return FirestoreExceptionReason.consumerInvalid;
+
+      case notFound:
+        return FirestoreExceptionReason.notFound;
+
+      case projectDeleted:
+        return FirestoreExceptionReason.projectDeleted; 
+
+      case projectInvalid:
+        return FirestoreExceptionReason.projectInvalid; 
+      
+      default:
+        return null;
+    }
+  }
+
+  @override
+  String unmap(FirestoreExceptionReason value) {
+    switch (value) {
+      case FirestoreExceptionReason.consumerInvalid:
+        return consumerInvalid;
+
+      case FirestoreExceptionReason.notFound:
+        return notFound;
+      
+      case FirestoreExceptionReason.projectDeleted:
+        return projectDeleted;
+
+      case FirestoreExceptionReason.projectInvalid:
+        return projectInvalid;
+    }
+    return null;
+  }
+}
--- metrics/ci_integrations/lib/client/firestore/model/firestore_exception_reason.dart
+++ metrics/ci_integrations/lib/client/firestore/model/firestore_exception_reason.dart
@@ -1,8 +1,8 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-/// Represents a Firebase gRPC error reason.
-enum FirebaseGrpcErrorReason {
+/// Represents a Firestore exception reason.
+enum FirestoreExceptionReason {
   /// Indicates whether the provided Firebase project ID is not valid
   /// or does not exist.
   consumerInvalid,
@@ -10,4 +10,12 @@ enum FirebaseGrpcErrorReason {
   /// Indicates whether the provided Firebase with the given project ID
   /// is not found.
   notFound,
+
+  /// Indicates whether the provided Firebase with the given project ID
+  /// is deleted.
+  projectDeleted,
+
+  /// Indicates whether the given Firebase project ID does not represent
+  /// a valid one.
+  projectInvalid,
 }
