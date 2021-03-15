3e44f6f63b9bf19f366ee660fd8b13f3d9453029
solid-tetianamovlian <tetiana.movlian@solid.software>
2021-03-15 18:14:02 +0200

    Minor documentation improvements

--- metrics/ci_integrations/test/test_utils/matchers.dart
+++ metrics/ci_integrations/test/test_utils/matchers.dart
@@ -4,6 +4,9 @@
 import 'package:ci_integration/cli/error/sync_error.dart';
 import 'package:test/test.dart';
 
+/// A matcher that can be used to detect that test case throws
+/// an [SyncError].
 final Matcher throwsSyncError = throwsA(isA<SyncError>());
 
+/// A matcher that can be used to verify that the value is equal to `1`.
 final Matcher once = equals(1);
