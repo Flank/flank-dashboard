e16a176039230ae9ba946b3ac7c67ceb9a41443c
Dima Koniaiev <52494380+solid-dimakoniaiev@users.noreply.github.com>
2021-03-17 15:54:01 +0200

    feat: Implement FlutterCLI (#1184)
    * Implement FlutterCLI. Add documentation.

---
+++ metrics/cli/lib/flutter/cli/flutter_cli.dart
@@ -0,0 +1,25 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:cli/interfaces/cli/cli.dart';
+
+/// A class that represents the Flutter [Cli].
+class FlutterCli extends Cli {
+  @override
+  final String executable = 'flutter';
+
+  /// Builds a Flutter web application in the given [workingDirectory].
+  Future<void> buildWeb(String workingDirectory) async {
+    await run(['build', 'web'], workingDirectory: workingDirectory);
+  }
+
+  /// Enables web support for the Flutter.
+  Future<void> enableWeb() async {
+    await run(['config', '--enable-web']);
+  }
+
+  @override
+  Future<void> version() async {
+    await run(['--version']);
+  }
+}
--- metrics/cli/lib/interfaces/cli/cli.dart
+++ metrics/cli/lib/interfaces/cli/cli.dart
@@ -1,8 +1,44 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
+import 'dart:async';
+import 'dart:io';
+
+import 'package:process_run/process_run.dart' as cmd;
+import 'package:process_run/shell_run.dart';
+
 /// A base class for CLIs that provides common methods for them.
 abstract class Cli {
+  /// A name of the executable for this CLI.
+  String get executable;
+
   /// Shows the version information of this CLI.
   Future<void> version();
+
+  /// Starts a process running [executable] in the [workingDirectory]
+  /// with the specified [arguments].
+  ///
+  /// The [arguments] is a specified [List] of arguments passed to
+  /// the [executable]. If the [arguments] is `null`, the empty [List] is used.
+  /// The [attachOutput] specifies whether print the [executable]'s output
+  /// or not. The [attachOutput] default value is `true`.
+  /// Use the [workingDirectory] to set the directory from where the [executable]
+  /// will run. If the [workingDirectory] is `null`, the [executable] will run
+  /// from the current terminal's working directory.
+  /// Provide the [stdin] to enable the interaction with the [executable].
+  /// If the [stdin] is `null`, the [sharedStdIn] is used.
+  Future<ProcessResult> run(
+    List<String> arguments, {
+    bool attachOutput = true,
+    String workingDirectory,
+    Stream<List<int>> stdin,
+  }) async {
+    return cmd.run(
+      executable,
+      arguments ?? [],
+      verbose: attachOutput,
+      workingDirectory: workingDirectory,
+      stdin: stdin ?? sharedStdIn,
+    );
+  }
 }
