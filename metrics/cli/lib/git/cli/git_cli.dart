4c12cc925d420035bc30cf55052b8cfeb84c05e1
solid-dimakoniaiev <dmytro.koniaiev@solid.software>
2021-03-17 16:34:13 +0200

    Implement GitCli

---
+++ metrics/cli/lib/git/cli/git_cli.dart
@@ -0,0 +1,18 @@
+import 'package:cli/interfaces/cli/cli.dart';
+
+/// A class that represents the Git [Cli].
+class GitCli extends Cli {
+  @override
+  final String executable = 'git';
+
+  /// Clones a Git repository from the given [repoUrl] into
+  /// a newly created [directory].
+  Future<void> clone(String repoUrl, String directory) {
+    return run(['clone', repoUrl, directory]);
+  }
+
+  @override
+  Future<void> version() {
+    return run(['--version']);
+  }
+}
