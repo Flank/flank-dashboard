626c25d59b40d4f4114310f65451a734f4354c72
solid-dimakoniaiev <dmytro.koniaiev@solid.software>
2021-03-17 17:00:43 +0200

    Add license.

--- metrics/cli/lib/git/cli/git_cli.dart
+++ metrics/cli/lib/git/cli/git_cli.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:cli/interfaces/cli/cli.dart';
 
 /// A class that represents the Git [Cli].
