c5f60ae9dc9373883a777f5617c5467ba78bc6fa
Yurii Uvarov <68225012+solid-yuriiuvarov@users.noreply.github.com>
2021-03-19 12:16:16 +0200

    feat: Implement FirebaseCli (#1205)
    * Implement FirebaseCli

---
+++ metrics/cli/lib/firebase/cli/firebase_cli.dart
@@ -0,0 +1,83 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:cli/interfaces/cli/cli.dart';
+
+/// A class that represents the Firebase [Cli].
+class FirebaseCli extends Cli {
+  @override
+  final String executable = 'firebase';
+
+  /// Logins into the Firebase CLI.
+  Future<void> login() {
+    return run(['login', '--interactive']);
+  }
+
+  /// Adds the Firebase capabilities to the project with the given [projectId].
+  Future<void> addFirebase(String projectId) {
+    return run(['projects:addfirebase', projectId]);
+  }
+
+  /// Creates a Firebase web app with the given [appName]
+  /// within the project with the given [projectId].
+  Future<void> createWebApp(String projectId, String appName) {
+    return run(['apps:create', '--project', projectId, "WEB", appName]);
+  }
+
+  /// Sets the project with the [projectId] identifier as the default one
+  /// for the Firebase project in the [workingDirectory].
+  Future<void> setFirebaseProject(String projectId, String workingDirectory) {
+    return run(['use', projectId], workingDirectory: workingDirectory);
+  }
+
+  /// Clears the firebase [target] in the given [workingDirectory].
+  Future<void> clearTarget(String target, String workingDirectory) {
+    return run(
+      ['target:clear', 'hosting', target],
+      workingDirectory: workingDirectory,
+    );
+  }
+
+  /// Associates the firebase [target] with the given [hostingName]
+  /// in the given [workingDirectory].
+  Future<void> applyTarget(
+    String hostingName,
+    String target,
+    String workingDirectory,
+  ) {
+    return run(
+      ['target:apply', 'hosting', target, hostingName],
+      workingDirectory: workingDirectory,
+    );
+  }
+
+  /// Deploys a project's [target] from the given [workingDirectory]
+  /// to the Firebase hosting.
+  Future<void> deployHosting(String target, String workingDirectory) {
+    return run(
+      ['deploy', '--only', 'hosting:$target'],
+      workingDirectory: workingDirectory,
+    );
+  }
+
+  /// Deploys Firestore rules from the given [workingDirectory] to the Firebase.
+  Future<void> deployRules(String target, String workingDirectory) {
+    return run(
+      ['deploy', '--only', 'firestore:rules'],
+      workingDirectory: workingDirectory,
+    );
+  }
+
+  /// Deploys functions from the given [workingDirectory] to the Firebase.
+  Future<void> deployFunctions(String workingDirectory) {
+    return run(
+      ['deploy', '--only', 'functions'],
+      workingDirectory: workingDirectory,
+    );
+  }
+
+  @override
+  Future<void> version() {
+    return run(['--version']);
+  }
+}
