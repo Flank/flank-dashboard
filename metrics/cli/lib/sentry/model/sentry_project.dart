d86ec5cb26a683cf143b60552b74d4e51c13bfdb
Yurii Uvarov <yurii.uvarov@solid.software>
2021-03-24 17:21:32 +0200

    Add license information

--- metrics/cli/lib/sentry/model/sentry_project.dart
+++ metrics/cli/lib/sentry/model/sentry_project.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 /// A class that represents slugs for the Sentry project.
 class SentryProject {
   /// The identifier of the Sentry organization.
