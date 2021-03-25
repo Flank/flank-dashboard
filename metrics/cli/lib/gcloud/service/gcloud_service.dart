fc683daad35307cacd311736594dfd0ef2f9a77d
Yurii Uvarov <68225012+solid-yuriiuvarov@users.noreply.github.com>
2021-03-25 15:42:23 +0200

    feat: Implement GCloudService (#1221)
    * Implement GCloudService

---
+++ metrics/cli/lib/gcloud/service/gcloud_service.dart
@@ -0,0 +1,14 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:cli/interfaces/service/info_service.dart';
+
+/// An abstract class for GCloud service that provides methods
+/// for working with GCloud.
+abstract class GCloudService extends InfoService {
+  /// Logins into the GCloud.
+  Future<void> login();
+
+  /// Creates a new GCloud project.
+  Future<String> createProject();
+}
