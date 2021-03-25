86e782a7c3a2ece68a3f439375be4f19032a4b34
Yurii Uvarov <68225012+solid-yuriiuvarov@users.noreply.github.com>
2021-03-25 16:07:18 +0200

    feat: Implement SentryCLI (#1226)
    * Implement SentryCli

---
+++ metrics/cli/lib/sentry/cli/sentry_cli.dart
@@ -0,0 +1,77 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:cli/interfaces/cli/cli.dart';
+import 'package:cli/sentry/model/sentry_project.dart';
+
+/// A class that represents the Sentry [Cli].
+class SentryCli extends Cli {
+  @override
+  final String executable = 'sentry-cli';
+
+  /// Logs in to the Sentry CLI.
+  Future<void> login() {
+    return run(['login']);
+  }
+
+  /// Creates a Sentry release with the given [releaseName]
+  /// within the given [project].
+  Future<void> createRelease(String releaseName, SentryProject project) {
+    return run([
+      'releases',
+      '--org=${project.organizationSlug}',
+      '--project=${project.projectSlug}',
+      'new',
+      releaseName,
+    ]);
+  }
+
+  /// Uploads source maps of the files with the given [extensions] located
+  /// in the [sourcePath] to the release with the given [releaseName] within
+  /// the given [project].
+  ///
+  /// If the [extensions] are not specified, source maps of all files
+  /// are uploaded.
+  Future<void> uploadSourceMaps(
+    String sourcePath,
+    List<String> extensions,
+    SentryProject project,
+    String releaseName,
+  ) {
+    final parameters = [
+      'releases',
+      '--org=${project.organizationSlug}',
+      '--project=${project.projectSlug}',
+      'files',
+      releaseName,
+      'upload-sourcemaps',
+      sourcePath,
+      '--rewrite',
+    ];
+
+    if (extensions != null) {
+      for (final extension in extensions) {
+        parameters.add('--ext=$extension');
+      }
+    }
+
+    return run(parameters);
+  }
+
+  /// Finalizes the release with the given [releaseName]
+  /// within the given [project].
+  Future<void> finalizeRelease(String releaseName, SentryProject project) {
+    return run([
+      'releases',
+      '--org=${project.organizationSlug}',
+      '--project=${project.projectSlug}',
+      'finalize',
+      releaseName,
+    ]);
+  }
+
+  @override
+  Future<void> version() {
+    return run(['--version']);
+  }
+}
