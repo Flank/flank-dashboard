1cb208a213486802b8b9019f9999108e760f91fd
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-03-29 15:01:37 +0300

    Minor fix

--- metrics/web/lib/dashboard/presentation/widgets/strategy/build_result_duration_strategy.dart
+++ metrics/web/lib/dashboard/presentation/widgets/strategy/build_result_duration_strategy.dart
@@ -1,9 +1,10 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
+import 'package:clock/clock.dart';
 import 'package:metrics/dashboard/presentation/view_models/build_result_view_model.dart';
 import 'package:metrics/dashboard/presentation/view_models/finished_build_result_view_model.dart';
-import 'package:clock/clock.dart';
+import 'package:metrics/dashboard/presentation/view_models/in_progress_build_result_view_model.dart';
 
 /// A class that represents a strategy for defining the build's duration
 /// depending on the given [BuildResultViewModel].
@@ -12,19 +13,19 @@ class BuildResultDurationStrategy {
   const BuildResultDurationStrategy();
 
   /// Returns the [Duration] for the given [buildResultViewModel].
-  /// 
+  ///
   /// Returns the [FinishedBuildResultViewModel.duration] if the given
   /// [buildResultViewModel] is a [FinishedBuildResultViewModel].
   ///
   /// Otherwise, returns the difference between the current timestamp and the
-  /// [buildResultViewModel.date].
+  /// [InProgressBuildResultViewModel.date].
   Duration getDuration(BuildResultViewModel buildResultViewModel) {
     if (buildResultViewModel is FinishedBuildResultViewModel) {
       return buildResultViewModel.duration;
     }
 
     final currentDateTime = clock.now();
-    
+
     return currentDateTime.difference(buildResultViewModel.date);
   }
 }
