44853527336a85080435ad2ed447838f8f45db8e
solid-yevheniiashchypeleva <74961502+solid-yevheniiashchypeleva@users.noreply.github.com>
2021-03-29 15:33:41 +0300

    feat: Implement BuildResultDurationStrategy (#1244)
    * [WIP] Add BuildResultDurationStrategy
    
    * Add tests
    
    * Improve tests
    
    * Minor improvements
    
    * Minor fix
    
    * Minor fix
    
    * Fix tests description
    
    Co-authored-by: Daniyil Yevtyushkin <69351065+solid-daniyilyevtyushkin@users.noreply.github.com>

---
+++ metrics/web/lib/dashboard/presentation/widgets/strategy/build_result_duration_strategy.dart
@@ -0,0 +1,30 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:clock/clock.dart';
+import 'package:metrics/dashboard/presentation/view_models/build_result_view_model.dart';
+import 'package:metrics/dashboard/presentation/view_models/finished_build_result_view_model.dart';
+
+/// A class that represents a strategy for defining the build's duration
+/// depending on the given [BuildResultViewModel].
+class BuildResultDurationStrategy {
+  /// Creates a new instance of the [BuildResultDurationStrategy].
+  const BuildResultDurationStrategy();
+
+  /// Returns the [Duration] for the given [buildResultViewModel].
+  ///
+  /// Returns the [FinishedBuildResultViewModel.duration] if the given
+  /// [buildResultViewModel] is a [FinishedBuildResultViewModel].
+  ///
+  /// Otherwise, returns the difference between the current timestamp and the
+  /// [BuildResultViewModel.date].
+  Duration getDuration(BuildResultViewModel buildResultViewModel) {
+    if (buildResultViewModel is FinishedBuildResultViewModel) {
+      return buildResultViewModel.duration;
+    }
+
+    final currentDateTime = clock.now();
+
+    return currentDateTime.difference(buildResultViewModel.date);
+  }
+}
---
+++ metrics/web/test/dashboard/presentation/widgets/strategy/build_result_duration_strategy_test.dart
@@ -0,0 +1,59 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:clock/clock.dart';
+import 'package:metrics/dashboard/presentation/view_models/build_result_popup_view_model.dart';
+import 'package:metrics/dashboard/presentation/view_models/finished_build_result_view_model.dart';
+import 'package:metrics/dashboard/presentation/view_models/in_progress_build_result_view_model.dart';
+import 'package:metrics/dashboard/presentation/widgets/strategy/build_result_duration_strategy.dart';
+import 'package:metrics_core/metrics_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("BuildResultDurationStrategy", () {
+    const buildStatus = BuildStatus.unknown;
+    const duration = Duration.zero;
+    const strategy = BuildResultDurationStrategy();
+
+    final date = DateTime(2021);
+    final buildResultPopupViewModel = BuildResultPopupViewModel(
+      duration: duration,
+      date: date,
+    );
+    final finishedBuildViewModel = FinishedBuildResultViewModel(
+      duration: duration,
+      date: date,
+      buildResultPopupViewModel: buildResultPopupViewModel,
+      buildStatus: buildStatus,
+    );
+    final inProgressBuildResultViewModel = InProgressBuildResultViewModel(
+      date: date,
+      buildResultPopupViewModel: buildResultPopupViewModel,
+    );
+
+    test(
+      ".getDuration() returns the duration of the given build result view model if it is finished",
+      () {
+        final result = strategy.getDuration(finishedBuildViewModel);
+
+        expect(result, equals(duration));
+      },
+    );
+
+    test(
+      ".getDuration() returns the difference between the current time and the build date if the given build result view model is in progress",
+      () {
+        final currentDateTime = DateTime(2022);
+        final expectedResult = currentDateTime.difference(
+          inProgressBuildResultViewModel.date,
+        );
+
+        withClock(Clock.fixed(currentDateTime), () {
+          final result = strategy.getDuration(inProgressBuildResultViewModel);
+
+          expect(result, equals(expectedResult));
+        });
+      },
+    );
+  });
+}
