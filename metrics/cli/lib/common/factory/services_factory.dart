f8ae5eee1e28c72547d326c3717dfcfd5aa6e543
Yurii Uvarov <68225012+solid-yuriiuvarov@users.noreply.github.com>
2021-03-29 14:13:44 +0300

    feat: Implement ServicesFactory (#1242)
    * Implement ServicesFactory

---
+++ metrics/cli/lib/common/factory/services_factory.dart
@@ -0,0 +1,29 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+import 'package:cli/common/model/services.dart';
+import 'package:cli/flutter/adapter/flutter_cli_service_adapter.dart';
+import 'package:cli/flutter/cli/flutter_cli.dart';
+import 'package:cli/gcloud/adapter/gcloud_cli_service_adapter.dart';
+import 'package:cli/gcloud/cli/gcloud_cli.dart';
+import 'package:cli/prompt/prompter.dart';
+import 'package:cli/prompt/writer/io_prompt_writer.dart';
+
+/// A class providing method for creating [Services] instance.
+class ServicesFactory {
+  /// Creates a new instance of the [Services].
+  Services create() {
+    final promptWriter = IOPromptWriter();
+    final prompter = Prompter(promptWriter);
+
+    final flutterCli = FlutterCli();
+    final gcloudCli = GCloudCli();
+
+    final flutterService = FlutterCliServiceAdapter(flutterCli);
+    final gcloudService = GCloudCliServiceAdapter(gcloudCli, prompter);
+
+    return Services(
+      flutterService: flutterService,
+      gcloudService: gcloudService,
+    );
+  }
+}
