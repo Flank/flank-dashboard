59dbf3efbbf183b4e4cbba41194462294930ff11
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-03-29 13:27:56 +0300

    Improve tests

--- metrics/web/test/dashboard/presentation/widgets/strategy/build_result_duration_strategy_test.dart
+++ metrics/web/test/dashboard/presentation/widgets/strategy/build_result_duration_strategy_test.dart
@@ -1,13 +1,13 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
+import 'package:clock/clock.dart';
 import 'package:metrics/dashboard/presentation/view_models/build_result_popup_view_model.dart';
 import 'package:metrics/dashboard/presentation/view_models/finished_build_result_view_model.dart';
 import 'package:metrics/dashboard/presentation/view_models/in_progress_build_result_view_model.dart';
 import 'package:metrics/dashboard/presentation/widgets/strategy/build_result_duration_strategy.dart';
 import 'package:metrics_core/metrics_core.dart';
 import 'package:test/test.dart';
-import 'package:clock/clock.dart';
 
 void main() {
   group("BuildResultDurationStrategy", () {
@@ -41,15 +41,14 @@ void main() {
     );
 
     test(
-      ".getDuration() returns the duration of the given build result view model if it is in progress",
+      ".getDuration() returns the difference between the current time and the build date if given build result view model is in progress",
       () {
         final currentTime = DateTime(2022);
+        final expectedResult = currentTime.difference(
+          inProgressBuildResultViewModel.date,
+        );
 
         withClock(Clock.fixed(currentTime), () {
-          final expectedResult = currentTime.difference(
-            inProgressBuildResultViewModel.date,
-          );
-
           final result = strategy.getDuration(inProgressBuildResultViewModel);
 
           expect(result, equals(expectedResult));
