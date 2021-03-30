f88c5df9c8512e9572bb7e1229255f1bf5eb7b0f
Yurii Uvarov <yurii.uvarov@solid.software>
2021-03-30 14:57:57 +0300

    Add license information

--- metrics/cli/lib/deploy/constants/deploy_constants.dart
+++ metrics/cli/lib/deploy/constants/deploy_constants.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 /// Holds the strings for the Deployer.
 class DeployConstants {
   /// A URL to the Metrics repository.
--- metrics/cli/lib/deploy/factory/deployer_factory.dart
+++ metrics/cli/lib/deploy/factory/deployer_factory.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:cli/cli/firebase/firebase_command.dart';
 import 'package:cli/cli/git/git_command.dart';
 import 'package:cli/common/factory/services_factory.dart';
