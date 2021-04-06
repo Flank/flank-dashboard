1776474382de68d63cec1faf9bb0005a4145fbe2
Yurii Uvarov <68225012+solid-yuriiuvarov@users.noreply.github.com>
2021-04-06 15:10:08 +0300

    feat: Add CLI commands error handling (#1256)
    * Add CLI commands error handling

---
+++ metrics/cli/lib/common/strings/common_strings.dart
@@ -0,0 +1,10 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+/// A class that holds the common strings.
+class CommonStrings {
+  static String executionWentWrong(String executable) =>
+      'Something went wrong while executing the $executable command.';
+}
