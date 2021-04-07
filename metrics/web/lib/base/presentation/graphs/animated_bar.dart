2caf0d24cce89e97dcfdb827cdd22ca8cf9347b8
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-04-07 10:40:26 +0300

    Add tests for AnimatedBar, minor refactoring

--- metrics/web/lib/base/presentation/graphs/animated_bar.dart
+++ metrics/web/lib/base/presentation/graphs/animated_bar.dart
@@ -1,48 +1,42 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
+import 'package:flutter/widgets.dart';
 import 'package:metrics/base/presentation/graphs/bar_graph.dart';
 import 'package:metrics/base/presentation/widgets/rive_animation.dart';
-import 'package:flutter/widgets.dart';
 import 'package:rive/rive.dart';
 
 /// A widget that displays a rectangle animated bar of the [BarGraph].
 class AnimatedBar extends StatelessWidget {
-  /// A width of this bar.
-  final double width;
+  /// A path to the [RiveAnimation] asset this bar displays.
+  final String riveAsset;
 
   /// A height of this bar.
   final double height;
 
-  /// A [String] representing a path to the [RiveAnimation] asset this bar
-  /// displays.
-  final String riveAsset;
-
-  /// A [BoxFit] that determines how to inscribe this animation into the space
-  /// allocated during layout.
-  final BoxFit fit;
+  /// A width of this bar.
+  final double width;
 
   /// A [RiveAnimationController] to control this bar's animation.
   final RiveAnimationController controller;
 
-  /// A [String] representing a name of the [Artboard] to load for this bar's
-  /// animation.
+  /// A a name of the [Artboard] to load for this bar's animation.
   final String artboardName;
 
   /// Creates a new instance of the [AnimatedBar] with the given parameters.
   ///
-  /// The [width] defaults to the `0.0`.
-  ///
-  /// The [width] must not be `null` or negative.
+  /// The [riveAsset] must not be `null`.
+  /// The [height], [width] must not be `null` or negative.
   const AnimatedBar({
     Key key,
-    this.fit,
-    this.height,
-    this.riveAsset,
+    @required this.riveAsset,
+    @required this.height,
+    @required this.width,
     this.controller,
     this.artboardName,
-    this.width = 0.0,
-  })  : assert(width != null && width >= 0.0),
+  })  : assert(riveAsset != null),
+        assert(height != null && height >= 0.0),
+        assert(width != null && width >= 0.0),
         super(key: key);
 
   @override
@@ -52,9 +46,9 @@ class AnimatedBar extends StatelessWidget {
       width: width,
       child: RiveAnimation(
         riveAsset,
-        fit: fit,
         controller: controller,
         artboardName: artboardName,
+        fit: BoxFit.fitWidth,
       ),
     );
   }
--- metrics/web/test/base/presentation/graphs/animated_bar_test.dart
+++ metrics/web/test/base/presentation/graphs/animated_bar_test.dart
@@ -2,18 +2,55 @@
 // that can be found in the LICENSE file.
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:metrics/base/presentation/graphs/animated_bar.dart';
+import 'package:metrics/base/presentation/widgets/rive_animation.dart';
 import 'package:rive/rive.dart';
 
-import '../../../test_utils/metrics_themed_testbed.dart';
+import '../../../test_utils/test_animation_container.dart';
+
+// ignore_for_file: avoid_redundant_argument_values
 
 void main() {
   group("AnimatedBar", () {
+    const asset = 'asset';
+    final riveAnimationFinder = find.byType(RiveAnimation);
+
+    testWidgets(
+      "throws an AssertionError if the given height is null",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          const _AnimatedBarTestbed(
+            height: null,
+          ),
+        );
+
+        expect(tester.takeException(), isAssertionError);
+      },
+    );
+
+    testWidgets(
+      "throws an AssertionError if the given height is negative",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          const _AnimatedBarTestbed(
+            height: -1.0,
+          ),
+        );
+
+        expect(tester.takeException(), isAssertionError);
+      },
+    );
+
     testWidgets(
       "throws an AssertionError if the given width is null",
       (WidgetTester tester) async {
-        await tester.pumpWidget(const _AnimatedBarTestbed(width: null));
+        await tester.pumpWidget(
+          const _AnimatedBarTestbed(
+            width: null,
+          ),
+        );
 
         expect(tester.takeException(), isAssertionError);
       },
@@ -22,9 +59,75 @@ void main() {
     testWidgets(
       "throws an AssertionError if the given width is negative",
       (WidgetTester tester) async {
-        await tester.pumpWidget(const _AnimatedBarTestbed(width: -1.0));
+        await tester.pumpWidget(
+          const _AnimatedBarTestbed(
+            width: -1.0,
+          ),
+        );
 
         expect(tester.takeException(), isAssertionError);
+      },
+    );
+
+    testWidgets(
+      "displays the rive animation with the given asset",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          const _AnimatedBarTestbed(
+            riveAsset: asset,
+          ),
+        );
+
+        final rive = tester.widget<RiveAnimation>(riveAnimationFinder);
+
+        expect(rive.assetName, equals(asset));
+      },
+    );
+
+    testWidgets(
+      "displays the rive animation with the given controller",
+      (WidgetTester tester) async {
+        final controller = SimpleAnimation('1');
+
+        await tester.pumpWidget(
+          _AnimatedBarTestbed(
+            controller: controller,
+          ),
+        );
+
+        final rive = tester.widget<RiveAnimation>(riveAnimationFinder);
+
+        expect(rive.controller, equals(controller));
+      },
+    );
+
+    testWidgets(
+      "displays the rive animation with the given artboard name",
+      (WidgetTester tester) async {
+        const artboardName = 'name';
+
+        await tester.pumpWidget(
+          const _AnimatedBarTestbed(
+            artboardName: artboardName,
+          ),
+        );
+
+        final rive = tester.widget<RiveAnimation>(riveAnimationFinder);
+
+        expect(rive.artboardName, equals(artboardName));
+      },
+    );
+
+    testWidgets(
+      "applies the BoxFit.fitWidth to the rive animation",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          const _AnimatedBarTestbed(),
+        );
+
+        final rive = tester.widget<RiveAnimation>(riveAnimationFinder);
+
+        expect(rive.fit, equals(BoxFit.fitWidth));
       },
     );
   });
@@ -41,9 +144,6 @@ class _AnimatedBarTestbed extends StatelessWidget {
   /// A rive animation asset to use in tests.
   final String riveAsset;
 
-  /// A [BoxFit] that determines how to inscribe this animation in tests.
-  final BoxFit fit;
-
   /// A [RiveAnimationController] to use in tests.
   final RiveAnimationController controller;
 
@@ -52,24 +152,26 @@ class _AnimatedBarTestbed extends StatelessWidget {
 
   const _AnimatedBarTestbed({
     Key key,
-    this.height,
-    this.riveAsset,
-    this.fit,
     this.controller,
     this.artboardName,
+    this.riveAsset = 'some asset',
     this.width = 1.0,
+    this.height = 1.0,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MetricsThemedTestbed(
-      body: AnimatedBar(
-        fit: fit,
-        height: height,
-        width: width,
-        riveAsset: riveAsset,
-        controller: controller,
-        artboardName: artboardName,
+    return MaterialApp(
+      home: Scaffold(
+        body: TestAssetContainer(
+          child: AnimatedBar(
+            height: height,
+            width: width,
+            riveAsset: riveAsset,
+            controller: controller,
+            artboardName: artboardName,
+          ),
+        ),
       ),
     );
   }
