f3411895a38c0c19aedf6ab64e14ddcf5b8e7753
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-04-14 13:59:00 +0300

    [WIP]: add BuildsSyncStageFake for testing

--- metrics/ci_integrations/test/integration/ci/sync_stage/builds/builds_sync_stage_test.dart
+++ metrics/ci_integrations/test/integration/ci/sync_stage/builds/builds_sync_stage_test.dart
@@ -5,6 +5,27 @@ import 'package:test/test.dart';
 
 void main() {
   group("BuildsSyncStage", () {
-    test("", () {},);
+    test(
+      "",
+      () {},
+    );
   });
+}
+
+/// A fake class needed to test the [BuildsSyncStage] non-abstract methods.
+class _BuildsSyncStageFake extends BuildsSyncStage {
+  @override
+  final SourceClient sourceClient;
+
+  @override
+  final DestinationClient destinationClient;
+
+  /// Creates a new instance of this fake with the given [sourceClient]
+  /// and [destinationClient].
+  _BuildsSyncStageFake(this.sourceClient, this.destinationClient);
+
+  @override
+  FutureOr<InteractionResult> call(SyncConfig syncConfig) {
+    return Future.value();
+  }
 }
