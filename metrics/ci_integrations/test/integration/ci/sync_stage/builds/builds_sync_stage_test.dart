d6140ab9409ecf8a5cb1968029c1aca615cc571d
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-04-14 13:59:58 +0300

    Fix imports

--- metrics/ci_integrations/test/integration/ci/sync_stage/builds/builds_sync_stage_test.dart
+++ metrics/ci_integrations/test/integration/ci/sync_stage/builds/builds_sync_stage_test.dart
@@ -1,6 +1,13 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
+import 'dart:async';
+
+import 'package:ci_integration/integration/ci/config/model/sync_config.dart';
+import 'package:ci_integration/integration/ci/sync_stage/builds/builds_sync_stage.dart';
+import 'package:ci_integration/integration/interface/destination/client/destination_client.dart';
+import 'package:ci_integration/integration/interface/source/client/source_client.dart';
+import 'package:ci_integration/util/model/interaction_result.dart';
 import 'package:test/test.dart';
 
 void main() {
