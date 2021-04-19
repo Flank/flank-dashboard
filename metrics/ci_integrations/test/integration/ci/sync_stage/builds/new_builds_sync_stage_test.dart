77ad622100de4371090b87097c1d7dcee2fdd758
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-04-19 14:25:34 +0300

    Add ignore

--- metrics/ci_integrations/test/integration/ci/sync_stage/builds/new_builds_sync_stage_test.dart
+++ metrics/ci_integrations/test/integration/ci/sync_stage/builds/new_builds_sync_stage_test.dart
@@ -12,6 +12,8 @@ import 'package:test/test.dart';
 import '../../../../cli/test_util/mock/integration_client_mock.dart';
 import '../../../../test_utils/matchers.dart';
 
+// ignore_for_file: avoid_redundant_argument_values
+
 void main() {
   group("NewBuildsSyncStage", () {
     const sourceProjectId = 'sourceId';
