1ca209e05b038f5f946dc6a6973d5ad99bc4fcc8
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-04-21 14:33:46 +0300

    Fix rebase changes

--- metrics/ci_integrations/lib/integration/ci/ci_integration.dart
+++ metrics/ci_integrations/lib/integration/ci/ci_integration.dart
@@ -1,24 +1,19 @@
-// Use of this source code is governed by the Apache License, Version 2.0 
+// Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
 import 'package:ci_integration/cli/logger/mixin/logger_mixin.dart';
 import 'package:ci_integration/integration/ci/config/model/sync_config.dart';
-import 'package:ci_integration/integration/interface/destination/client/destination_client.dart';
-import 'package:ci_integration/integration/interface/source/client/source_client.dart';
+import 'package:ci_integration/integration/ci/sync_stage/sync_stage.dart';
 import 'package:ci_integration/util/model/interaction_result.dart';
 import 'package:meta/meta.dart';
 
 /// A class providing a synchronization algorithm for a project's builds
 /// performed on a CI tool and stored in a builds storage.
 class CiIntegration with LoggerMixin {
-  /// Used to interact with a source API.
-  final SourceClient sourceClient;
+  /// A [List] of [SyncStage]s this CI integration performs.
+  final List<SyncStage> stages;
 
-  /// Used to interact with a builds storage.
-  final DestinationClient destinationClient;
-
-  /// Creates a [CiIntegration] instance with the given [sourceClient]
-  /// and [destinationClient].
+  /// Creates a [CiIntegration] instance with the given [stages].
   ///
   /// Throws an [ArgumentError] if the given [stages] list is `null`.
   CiIntegration({
@@ -27,17 +22,13 @@ class CiIntegration with LoggerMixin {
     ArgumentError.checkNotNull(stages, 'stages');
   }
 
-  /// Synchronizes builds for a project specified in the given [config].
+  /// Synchronizes builds for a project specified in the given [syncConfig].
   ///
-  /// Throws an [ArgumentError] if the given [config] is `null`.
+  /// Throws an [ArgumentError] if the given [syncConfig] is `null`.
   Future<InteractionResult> sync(
-    SyncConfig config,
+    SyncConfig syncConfig,
   ) async {
-    ArgumentError.checkNotNull(config);
-
-    try {
-      final sourceProjectId = config.sourceProjectId;
-      final destinationProjectId = config.destinationProjectId;
+    ArgumentError.checkNotNull(syncConfig);
 
     for (final stage in stages) {
       final stageResult = await stage(syncConfig);
@@ -47,21 +38,7 @@ class CiIntegration with LoggerMixin {
           message: _getStageErrorMessage(stage, stageResult),
         );
       }
-
-      if (newBuilds == null || newBuilds.isEmpty) {
-        return const InteractionResult.success(
-          message: 'The project data is up-to-date!',
-        );
-      }
-
-      if (config.coverage) {
-        newBuilds = await _fetchCoverage(newBuilds);
-      }
-
-      await destinationClient.addBuilds(
-        destinationProjectId,
-        newBuilds,
-      );
+    }
 
     return const InteractionResult.success(
       message: 'The data has been synced successfully!',
