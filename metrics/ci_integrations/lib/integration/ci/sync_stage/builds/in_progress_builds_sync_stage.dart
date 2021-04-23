3d14fefb583c7b1b7d60a6a41d0970308fa66df5
Daniyil Yevtyushkin <69351065+solid-daniyilyevtyushkin@users.noreply.github.com>
2021-04-23 14:19:50 +0300

    feat: Implement `InProgressBuildsSyncStage` (#1316)
    * Changes by yevheniiashchypeleva
    
    * Refactor addCoverageData method
    
    * Add tests for .addCoverageData() method
    
    * Add tests for NewBuildsSyncStage
    
    * Minor fixes
    
    * Minor fixes
    
    * Update CI Integration class
    
    * Fix rebase changes
    
    * Add in progress builds sync stage
    
    * Update documentation, extract methods
    
    * [WIP] InProgressBuildsSyncStage tests
    
    * [WIP] add more tests for in-progress stage
    
    * Implement tests for the InProgressBuildsSyncStage
    
    * Implement tests for the InProgressBuildsSyncStage
    
    * Fix rebase changes
    
    * Delete rebased file
    
    * Minor improvements
    
    * Improve documentation
    
    * Resolve suggestions for the InProgressBuildsSyncStage
    
    * Resolve suggestions for the InProgressBuildsSyncStage tests
    
    * Minor fix
    
    * Minor fix in tests
    
    * Minor improvements
    
    * Minor fix
    
    * Improve doc
    
    Co-authored-by: Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>

--- metrics/ci_integrations/lib/integration/ci/sync_stage/builds/in_progress_builds_sync_stage.dart
+++ metrics/ci_integrations/lib/integration/ci/sync_stage/builds/in_progress_builds_sync_stage.dart
@@ -3,15 +3,27 @@
 
 import 'dart:async';
 
+import 'package:ci_integration/cli/logger/mixin/logger_mixin.dart';
 import 'package:ci_integration/integration/ci/config/model/sync_config.dart';
 import 'package:ci_integration/integration/ci/sync_stage/builds/builds_sync_stage.dart';
 import 'package:ci_integration/integration/ci/sync_stage/sync_stage.dart';
 import 'package:ci_integration/integration/interface/destination/client/destination_client.dart';
 import 'package:ci_integration/integration/interface/source/client/source_client.dart';
 import 'package:ci_integration/util/model/interaction_result.dart';
+import 'package:metrics_core/metrics_core.dart';
 
-/// A class that represents a [SyncStage] for in-progress builds.
-class InProgressBuildsSyncStage extends BuildsSyncStage {
+/// A class that represents a [SyncStage] for re-syncing builds with
+/// the [BuildStatus.inProgress].
+///
+/// Fetches the [BuildStatus.inProgress] builds using the
+/// [DestinationClient] and resynchronizes them using the [SourceClient].
+/// Then updates the corresponding builds using the [DestinationClient].
+///
+/// If the build loaded from the [SourceClient] is still
+/// [BuildStatus.inProgress] or updating a build fails, and the difference
+/// between the [DateTime.now] and [BuildData.startedAt] exceeds the in-progress
+/// timeout, sets the [BuildStatus.unknown] to the corresponding build.
+class InProgressBuildsSyncStage extends BuildsSyncStage with LoggerMixin {
   @override
   final DestinationClient destinationClient;
 
@@ -29,9 +41,118 @@ class InProgressBuildsSyncStage extends BuildsSyncStage {
   }
 
   @override
-  FutureOr<InteractionResult> call(SyncConfig syncConfig) {
+  Future<InteractionResult> call(SyncConfig syncConfig) async {
+    logger.info('Running InProgressBuildsSyncStage...');
+
     ArgumentError.checkNotNull(syncConfig, 'syncConfig');
 
-    return const InteractionResult.success();
+    final destinationProjectId = syncConfig.destinationProjectId;
+
+    try {
+      final inProgressBuilds = await destinationClient.fetchBuildsWithStatus(
+        destinationProjectId,
+        BuildStatus.inProgress,
+      );
+
+      if (inProgressBuilds.isEmpty) {
+        return const InteractionResult.success(
+          message: 'There are no in-progress builds to re-sync.',
+        );
+      }
+
+      List<BuildData> buildUpdates = await _resyncBuilds(
+        syncConfig,
+        inProgressBuilds,
+      );
+
+      if (syncConfig.coverage) {
+        buildUpdates = await addCoverageData(buildUpdates);
+      }
+
+      await destinationClient.updateBuilds(destinationProjectId, buildUpdates);
+
+      return const InteractionResult.success();
+    } catch (error) {
+      return InteractionResult.error(
+        message: '$error',
+      );
+    }
+  }
+
+  /// Re-synchronizes each [BuildData] of the given [inProgressBuilds] list
+  /// and returns a list of updated [BuildData].
+  Future<List<BuildData>> _resyncBuilds(
+    SyncConfig syncConfig,
+    List<BuildData> inProgressBuilds,
+  ) async {
+    final buildUpdates = <BuildData>[];
+
+    for (final build in inProgressBuilds) {
+      final refreshedBuild = await _resyncOneBuild(syncConfig, build);
+
+      if (refreshedBuild != null) buildUpdates.add(refreshedBuild);
+    }
+
+    return buildUpdates;
+  }
+
+  /// Re-synchronizes the given [build]. Returns `null` if the given [build]
+  /// shouldn't be updated.
+  ///
+  /// Fetches the corresponding build from the source using the [_fetchBuild]
+  /// method. If fetching is failed or the corresponding build is
+  /// [BuildStatus.inProgress], tries to timeout this build using the
+  /// [_timeoutIfNecessary] method. Otherwise, returns the re-synced build.
+  Future<BuildData> _resyncOneBuild(
+    SyncConfig syncConfig,
+    BuildData build,
+  ) async {
+    final sourceBuild = await _fetchBuild(
+      syncConfig.sourceProjectId,
+      build.buildNumber,
+    );
+
+    if (sourceBuild == null ||
+        sourceBuild.buildStatus == BuildStatus.inProgress) {
+      return _timeoutIfNecessary(build, syncConfig.inProgressTimeout);
+    }
+
+    return sourceBuild.copyWith(id: build.id, projectId: build.projectId);
+  }
+
+  /// Fetches a build with the given [buildNumber] of a project identified
+  /// by the given [sourceProjectId].
+  ///
+  /// Returns `null` if fetching a build fails.
+  Future<BuildData> _fetchBuild(String sourceProjectId, int buildNumber) {
+    return sourceClient
+        .fetchOneBuild(sourceProjectId, buildNumber)
+        .catchError((_) => null);
+  }
+
+  /// Times out the given [build] if this build satisfies the
+  /// [_shouldTimeoutBuild] method. Otherwise, returns `null`.
+  BuildData _timeoutIfNecessary(BuildData build, Duration timeout) {
+    if (_shouldTimeoutBuild(build.startedAt, timeout)) {
+      return build.copyWith(
+        duration: timeout,
+        buildStatus: BuildStatus.unknown,
+      );
+    }
+
+    return null;
+  }
+
+  /// Determines whether a build with the given [startedAt] date exceeds
+  /// the given [timeout].
+  ///
+  /// Returns `true` if the difference between the [DateTime.now] and
+  /// the [startedAt] is greater than or equal to the given [timeout].
+  /// Otherwise, returns `false`.
+  bool _shouldTimeoutBuild(DateTime startedAt, Duration timeout) {
+    final currentDate = DateTime.now();
+    final buildDuration = currentDate.difference(startedAt);
+
+    return buildDuration >= timeout;
   }
 }
