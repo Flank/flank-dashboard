6a9cddd8d18fc4ea02d10d513294ff4400f7c293
Yurii Uvarov <yurii.uvarov@solid.software>
2021-04-27 14:55:39 +0300

    Improve documentation

--- metrics/cli/lib/deploy/constants/deploy_constants.dart
+++ metrics/cli/lib/deploy/constants/deploy_constants.dart
@@ -13,17 +13,17 @@ class DeployConstants {
   static String tempDir([String suffix]) =>
       suffix == null ? 'tempDir' : 'tempDir_$suffix';
 
-  /// A path to the Web project sources.
+  /// A path to the Web project sources based on the given [tempDir].
   static String webPath(String tempDir) => '$tempDir/metrics/web';
 
-  /// A path to the Firebase sources.
+  /// A path to the Firebase sources based on the given [tempDir].
   static String firebasePath(String tempDir) => '$tempDir/metrics/firebase';
 
-  /// A path to the Firebase functions sources.
+  /// A path to the Firebase functions sources based on the given [tempDir].
   static String firebaseFunctionsPath(String tempDir) =>
       '${firebasePath(tempDir)}/functions';
 
-  /// A path to the Metrics configuration file.
+  /// A path to the Metrics configuration file based on the given [tempDir].
   static String metricsConfigPath(String tempDir) =>
       '${webPath(tempDir)}/build/web/metrics_config.js';
 }
