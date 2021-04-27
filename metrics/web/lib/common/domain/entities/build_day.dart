ffdaae66fbbc1a1b431f93a8eb4669ad4bc595d4
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-04-27 12:00:10 +0300

    Add tests for the BuildDay entity

--- metrics/web/lib/common/domain/entities/build_day.dart
+++ metrics/web/lib/common/domain/entities/build_day.dart
@@ -1,24 +1,29 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:metrics_core/metrics_core.dart';
 
 /// A class that represents a build day.
-@immutable
-class BuildDay {
+class BuildDay extends Equatable {
   /// An identifier of the project this build day belongs to.
   final String projectId;
 
-  /// A number of successful builds performed during this build day.
+  /// A number of [BuildStatus.successful] builds performed during this build
+  /// day.
   final int successful;
 
   /// A number of failed builds performed during this build day.
   final int failed;
 
-  /// A number of unknown status builds performed during this build day.
+  /// A number of [BuildStatus.unknown] builds performed during this build day.
   final int unknown;
 
   /// A number of in-progress builds ran during this build day.
   final int inProgress;
 
-  /// A total amount of time taken for builds during this build day.
+  /// A total [Duration] taken for builds during this build day.
   final Duration totalDuration;
 
   /// A [DateTime] that represents the date of this build day.
@@ -27,12 +32,12 @@ class BuildDay {
   /// Creates a new instance of the [BuildDay].
   BuildDay({
     @required this.projectId,
-    this.successful,
-    this.failed,
-    this.unknown,
-    this.inProgress,
-    this.totalDuration,
-    this.day,
+    @required this.successful,
+    @required this.failed,
+    @required this.unknown,
+    @required this.inProgress,
+    @required this.totalDuration,
+    @required this.day,
   }) {
     ArgumentError.checkNotNull(projectId, 'projectId');
     ArgumentError.checkNotNull(successful, 'successful');
