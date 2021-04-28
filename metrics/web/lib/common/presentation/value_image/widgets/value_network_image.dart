e81d0158dc8889fe7d31870e8596650eaf35b77e
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-04-28 15:57:36 +0300

    Fix ValueNetworkImage with tests

--- metrics/web/lib/common/presentation/value_image/widgets/value_network_image.dart
+++ metrics/web/lib/common/presentation/value_image/widgets/value_network_image.dart
@@ -1,9 +1,9 @@
-// Use of this source code is governed by the Apache License, Version 2.0 
+// Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
 import 'package:metrics/base/presentation/widgets/svg_image.dart';
-import 'package:metrics/common/presentation/value_image/strategy/value_based_image_asset_strategy.dart';
+import 'package:metrics/common/presentation/asset/strategy/value_based_asset_strategy.dart';
 
 /// A widget that displays an image from the [strategy] depending on
 /// the given [value].
@@ -17,8 +17,8 @@ class ValueNetworkImage<T> extends StatelessWidget {
   /// A value the [strategy] uses to select an image asset to display.
   final T value;
 
-  /// An image asset strategy to apply to this widget.
-  final ValueBasedImageAssetStrategy<T> strategy;
+  /// An asset strategy to apply to this widget.
+  final ValueBasedAssetStrategy<T> strategy;
 
   /// Creates a new instance of the [ValueNetworkImage].
   const ValueNetworkImage({
@@ -32,7 +32,7 @@ class ValueNetworkImage<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final iconImage = strategy.getImageAsset(value);
+    final iconImage = strategy.getAsset(value);
 
     return SvgImage(
       iconImage,
