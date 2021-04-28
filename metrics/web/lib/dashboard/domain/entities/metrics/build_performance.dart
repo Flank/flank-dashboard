00715dd79f237b059139c06ac598aede1f019eb2
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-04-28 10:40:56 +0300

    Resolve the suggestions for the BuildPerformance entity

--- metrics/web/lib/dashboard/domain/entities/metrics/build_performance.dart
+++ metrics/web/lib/dashboard/domain/entities/metrics/build_performance.dart
@@ -4,19 +4,19 @@
 import 'package:equatable/equatable.dart';
 import 'package:metrics/dashboard/domain/entities/collections/date_time_set_entry.dart';
 
-/// A class that represents the performance of the build day.
+/// A class that represents the performance of builds by day.
 class BuildPerformance with EquatableMixin implements DateTimeSetEntry {
   @override
   final DateTime date;
 
-  /// A [Duration] of the build.
+  /// A [Duration] of builds by day.
   final Duration duration;
 
   @override
   List<Object> get props => [date, duration];
 
-  /// Creates a new instance of the [BuildPerformance] with the given [date] and
-  /// [duration].
+  /// Creates a new instance of the [BuildPerformance] with the given
+  /// parameters.
   const BuildPerformance({
     this.date,
     this.duration,
