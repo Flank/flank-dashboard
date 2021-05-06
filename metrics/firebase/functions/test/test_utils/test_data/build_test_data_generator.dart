fbdf6c18ef4f1a4f97101b147362326dd8b06efd
solid-tetianamovlian <tetiana.movlian@solid.software>
2021-05-06 11:14:43 +0300

    Add license

--- metrics/firebase/functions/test/test_utils/test_data/build_test_data_generator.dart
+++ metrics/firebase/functions/test/test_utils/test_data/build_test_data_generator.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:firebase_functions_interop/firebase_functions_interop.dart';
 import 'package:metrics_core/metrics_core.dart';
 
