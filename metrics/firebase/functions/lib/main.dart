47e11e5c8efab78e698ae93d69dff83f9c80e23b
Yurii Uvarov <yurii.uvarov@solid.software>
2021-05-12 19:51:40 +0300

    Test #1

--- metrics/firebase/functions/lib/main.dart
+++ metrics/firebase/functions/lib/main.dart
@@ -1,6 +1,6 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
-
+//test
 import 'package:clock/clock.dart';
 import 'package:firebase_functions_interop/firebase_functions_interop.dart';
 import 'package:metrics_core/metrics_core.dart';
