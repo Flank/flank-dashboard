6dedced562689b43e44f569b7db30ce941b8f856
solid-tetianamovlian <tetiana.movlian@solid.software>
2021-05-18 18:14:45 +0300

    Add license

--- metrics/firebase/functions/lib/models/build_data_model.dart
+++ metrics/firebase/functions/lib/models/build_data_model.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:metrics_core/metrics_core.dart';
 
 /// A [DataModel] that represents the [Build] entity.
