45ab2d4874ef2b24b13765518fcdfe875ea6d4b7
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-06-10 16:55:24 +0300

    Update MetricsRouteInformationParser

--- metrics/web/lib/common/presentation/navigation/metrics_route_information_parser.dart
+++ metrics/web/lib/common/presentation/navigation/metrics_route_information_parser.dart
@@ -1,10 +1,11 @@
-// Use of this source code is governed by the Apache License, Version 2.0 
+// Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:metrics/common/presentation/navigation/route_configuration/route_configuration.dart';
 import 'package:metrics/common/presentation/navigation/route_configuration/route_configuration_factory.dart';
+import 'package:metrics/common/presentation/navigation/route_configuration/route_configuration_location_converter.dart';
 
 /// A [RouteInformationParser] that parses the [RouteInformation]
 /// into the [RouteConfiguration] and vice versa.
@@ -13,11 +14,19 @@ class MetricsRouteInformationParser
   /// A factory needed to create a [RouteConfiguration] depending on the [Uri].
   final RouteConfigurationFactory _routeConfigurationFactory;
 
+  /// A converter needed to create a location [String] from the
+  /// [RouteConfiguration].
+  final RouteConfigurationLocationConverter _locationConverter;
+
   /// Creates a new instance of the [MetricsRouteInformationParser].
   ///
-  /// The route configuration factory must not be `null`.
-  MetricsRouteInformationParser(this._routeConfigurationFactory)
-      : assert(_routeConfigurationFactory != null);
+  /// Throws an [AssertionError] if the given [RouteConfigurationFactory] or
+  /// [RouteConfigurationLocationConverter] is `null`.
+  MetricsRouteInformationParser(
+    this._routeConfigurationFactory,
+    this._locationConverter,
+  )   : assert(_routeConfigurationFactory != null),
+        assert(_locationConverter != null);
 
   @override
   Future<RouteConfiguration> parseRouteInformation(
@@ -36,6 +45,8 @@ class MetricsRouteInformationParser
   RouteInformation restoreRouteInformation(RouteConfiguration configuration) {
     if (configuration == null) return null;
 
-    return RouteInformation(location: configuration.path);
+    final location = _locationConverter.convert(configuration);
+
+    return RouteInformation(location: location);
   }
 }
