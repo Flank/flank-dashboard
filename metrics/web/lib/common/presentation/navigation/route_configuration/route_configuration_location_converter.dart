6ca5250370430f98407afafcd0de758d2a56cbec
solid-daniyilyevtyushkin <daniyil.yevtyushkin@solid.software>
2021-06-10 14:27:00 +0300

    Add const constructor to the RouteConfigurationLocationConverter

--- metrics/web/lib/common/presentation/navigation/route_configuration/route_configuration_location_converter.dart
+++ metrics/web/lib/common/presentation/navigation/route_configuration/route_configuration_location_converter.dart
@@ -3,6 +3,9 @@ import 'package:metrics/common/presentation/navigation/route_configuration/route
 /// A class that provides a method for converting the given [RouteConfiguration]
 /// to a location [String].
 class RouteConfigurationLocationConverter {
+  /// Creates a new instance of the [RouteConfigurationLocationConverter].
+  const RouteConfigurationLocationConverter();
+
   /// Converts the given [routeConfiguration] to a location [String].
   ///
   /// Returns `null` if the given [routeConfiguration] is `null`.
