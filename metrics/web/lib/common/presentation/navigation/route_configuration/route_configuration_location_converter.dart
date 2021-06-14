3b6cf1dc4ec111952d49891273856ac6ba227b93
Daniyil Yevtyushkin <69351065+solid-daniyilyevtyushkin@users.noreply.github.com>
2021-06-14 18:05:56 +0300

    feat: Update the `RouteConfiguration` class to include the `parameters` field (#1451)
    * Add `parameters` field to `RouteConfiguration`
    
    * Update `RouteConfiguration` tests
    
    * Implement RouteConfigurationLocationConverter
    
    * Add const constructor to the RouteConfigurationLocationConverter
    
    * Fix calling isEmpty on null in RouteConfigurationLocationConverter
    
    * Add tests for RouteConfigurationLocationConverter
    
    * Update MetricsRouteInformationParser
    
    * Add tests for MetricsRouteInformationParser
    
    * Add const constructor for RouteConfigurationFactory
    
    * Update MetricsRouteInformationParser injection
    
    * Add missing licenses
    
    * Add default values for the MetricsRouteInformationParser(). Minor improvements
    
    * Fix flutter analyze
    
    * Improve doc
    
    Co-authored-by: Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
    Co-authored-by: Yevheniia Shchypeleva <74961502+solid-yevheniiashchypeleva@users.noreply.github.com>

---
+++ metrics/web/lib/common/presentation/navigation/route_configuration/route_configuration_location_converter.dart
@@ -0,0 +1,28 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:metrics/common/presentation/navigation/route_configuration/route_configuration.dart';
+
+/// A class that provides a method for converting the [RouteConfiguration]
+/// to a location [String].
+class RouteConfigurationLocationConverter {
+  /// Creates a new instance of the [RouteConfigurationLocationConverter].
+  const RouteConfigurationLocationConverter();
+
+  /// Converts the given [routeConfiguration] to a location [String].
+  ///
+  /// Returns `null` if the given [routeConfiguration] is `null`.
+  String convert(RouteConfiguration routeConfiguration) {
+    if (routeConfiguration == null) return null;
+
+    final path = routeConfiguration.path;
+    final parameters = routeConfiguration.parameters;
+
+    final hasParameters = parameters != null && parameters.isNotEmpty;
+    final queryParameters = hasParameters ? parameters : null;
+
+    final uri = Uri(path: path, queryParameters: queryParameters);
+
+    return '$uri';
+  }
+}
