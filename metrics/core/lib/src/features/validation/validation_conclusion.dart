6f4965b56b8757d8089e6e6c47e81e26c66e2021
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-06-29 18:19:56 +0300

    Add Validation conclusion and target

---
+++ metrics/core/lib/src/features/validation/validation_conclusion.dart
@@ -0,0 +1,25 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:equatable/equatable.dart';
+import 'package:metrics_core/src/domain/entities/enum.dart';
+
+/// A class that represents a possible conclusion of the validation process.
+class ValidationConclusion<T> extends Enum<T> with EquatableMixin {
+  /// A name of this validation conclusion.
+  final String name;
+
+  /// A visual indicator of this conclusion.
+  final String indicator;
+
+  @override
+  List<Object> get props => [value, name, indicator];
+
+  /// Creates a new instance of the [ValidationConclusion] with the
+  /// given parameters.
+  const ValidationConclusion({
+    this.name,
+    this.indicator,
+    T value,
+  }) : super(value);
+}
---
+++ metrics/core/lib/src/features/validation/validation_target.dart
@@ -0,0 +1,24 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:equatable/equatable.dart';
+import 'package:metrics_core/src/domain/entities/enum.dart';
+
+/// A class that represents a single entity used in the validation process.
+class ValidationTarget<T> extends Enum<T> with EquatableMixin {
+  /// A name of this validation target.
+  final String name;
+
+  /// A description of this validation target.
+  final String description;
+
+  @override
+  List<Object> get props => [value, name, description];
+
+  /// Creates a new instance of the [ValidationTarget] with the given parameters.
+  const ValidationTarget({
+    this.name,
+    this.description,
+    T value,
+  }) : super(value);
+}
