430fde4bd722949cc8de1e65a6040187861f4553
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-07-07 13:25:43 +0300

    Fix tests

--- metrics/ci_integrations/test/source/jenkins/party/jenkins_source_party_test.dart
+++ metrics/ci_integrations/test/source/jenkins/party/jenkins_source_party_test.dart
@@ -1,9 +1,9 @@
 // Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
+import 'package:ci_integration/integration/stub/base/config/validator_factory/validator_factory_stub.dart';
 import 'package:ci_integration/source/jenkins/client_factory/jenkins_source_client_factory.dart';
 import 'package:ci_integration/source/jenkins/config/parser/jenkins_source_config_parser.dart';
-import 'package:ci_integration/source/jenkins/config/validator_factory/jenkins_source_validator_factory.dart';
 import 'package:ci_integration/source/jenkins/party/jenkins_source_party.dart';
 import 'package:test/test.dart';
 
@@ -23,10 +23,10 @@ void main() {
       expect(configParser, isA<JenkinsSourceConfigParser>());
     });
 
-    test("uses JenkinsSourceValidatorFactory as a validator factory", () {
+    test("uses ValidatorFactoryStub as a validator factory", () {
       final validatorFactory = jenkinsSourceParty.configValidatorFactory;
 
-      expect(validatorFactory, isA<JenkinsSourceValidatorFactory>());
+      expect(validatorFactory, isA<ValidatorFactoryStub>());
     });
   });
 }
