5459dca5c46bbafbedad343c3830a569a16dbe9d
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-07-07 12:25:31 +0300

    Update firestore validation to use stubs

--- metrics/ci_integrations/lib/destination/firestore/config/validator_factory/firestore_destination_validator_factory.dart
+++ metrics/ci_integrations/lib/destination/firestore/config/validator_factory/firestore_destination_validator_factory.dart
@@ -7,19 +7,19 @@ import 'package:ci_integration/destination/firestore/config/model/firestore_dest
 import 'package:ci_integration/destination/firestore/config/model/firestore_destination_config_field.dart';
 import 'package:ci_integration/destination/firestore/config/validation_delegate/firestore_destination_validation_delegate.dart';
 import 'package:ci_integration/destination/firestore/config/validator/firestore_destination_validator.dart';
-import 'package:ci_integration/integration/interface/base/config/validator/config_validator.dart';
-import 'package:ci_integration/integration/interface/base/config/validator_factory/config_validator_factory.dart';
+import 'package:ci_integration/integration/stub/base/config/validator/config_validator_stub.dart';
+import 'package:ci_integration/integration/stub/base/config/validator_factory/config_validator_factory_stub.dart';
 import 'package:ci_integration/integration/validation/model/validation_result_builder.dart';
 
 /// A factory class that provides a method for creating
 /// [FirestoreDestinationValidator].
 class FirestoreDestinationValidatorFactory
-    implements ConfigValidatorFactory<FirestoreDestinationConfig> {
+    implements ConfigValidatorFactoryStub<FirestoreDestinationConfig> {
   /// Creates a new instance of the [FirestoreDestinationValidatorFactory].
   const FirestoreDestinationValidatorFactory();
 
   @override
-  ConfigValidator<FirestoreDestinationConfig> create(
+  ConfigValidatorStub<FirestoreDestinationConfig> create(
     FirestoreDestinationConfig config,
   ) {
     ArgumentError.checkNotNull(config, 'config');
