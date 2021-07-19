d9b9127e07ddcad15a826d04d51f58c19f79503c
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-07-19 13:04:23 +0300

    Add validation target with tests

---
+++ metrics/core/lib/src/util/validation/validation_target.dart
@@ -0,0 +1,25 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+/// A class that represents a single entity used in the validation process.
+class ValidationTarget extends Equatable {
+  /// A name of this validation target.
+  final String name;
+
+  /// A description of this validation target.
+  final String description;
+
+  @override
+  List<Object> get props => [name, description];
+
+  /// Creates a new instance of the [ValidationTarget] with the given parameters.
+  ///
+  /// Throws an [AssertionError] if the given [name] is null.
+  const ValidationTarget({
+    @required this.name,
+    this.description,
+  }) : assert(name != null);
+}
