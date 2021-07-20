42830c900e37d3549c00cb5ce111da4fcc16ec3d
Yevheniia Shchypeleva <74961502+solid-yevheniiashchypeleva@users.noreply.github.com>
2021-07-20 20:59:13 +0300

    feat: Implement ValidationResult and ValidationResultBuilder (#1506)
    * Add validation result
    
    * Add validation result builder
    
    * Minor improvements
    
    * Fix typo

---
+++ metrics/core/test/util/validation/validation_result_test.dart
@@ -0,0 +1,62 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'dart:collection';
+
+import 'package:metrics_core/src/util/validation/target_validation_result.dart';
+import 'package:metrics_core/src/util/validation/validation_conclusion.dart';
+import 'package:metrics_core/src/util/validation/validation_result.dart';
+import 'package:metrics_core/src/util/validation/validation_target.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("ValidationResult", () {
+    const name = 'name';
+    const description = 'description';
+    const target = ValidationTarget(name: name);
+    const conclusion = ValidationConclusion(name: name);
+    const targetValidationResult = TargetValidationResult(
+      target: target,
+      conclusion: conclusion,
+      description: description,
+    );
+
+    final results = {
+      target: targetValidationResult,
+    };
+    final result = ValidationResult(results);
+
+    test(
+      "throws an ArgumentError if the given results is null",
+      () {
+        expect(() => ValidationResult(null), throwsArgumentError);
+      },
+    );
+
+    test(
+      "creates an instance with the given results",
+      () {
+        final validationResult = ValidationResult(results);
+
+        expect(validationResult.results, equals(results));
+      },
+    );
+
+    test(
+      ".results is an unmodifiable map view",
+      () {
+        expect(result.results, isA<UnmodifiableMapView>());
+      },
+    );
+
+    test(
+      "equals to another ValidationResult with the same parameters",
+      () {
+        final result = ValidationResult(results);
+        final anotherResult = ValidationResult(results);
+
+        expect(result, equals(anotherResult));
+      },
+    );
+  });
+}
