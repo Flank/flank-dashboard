73fc52de224ce1a62fd22e728122fbe90f4e356d
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-07-26 13:44:33 +0300

    Minor improvements

--- metrics/ci_integrations/lib/integration/validation/model/config_field_validation_conclusion.dart
+++ metrics/ci_integrations/lib/integration/validation/model/config_field_validation_conclusion.dart
@@ -6,12 +6,12 @@ import 'package:metrics_core/metrics_core.dart';
 
 /// Represents a validation conclusion for a specific [Config] field.
 class ConfigFieldValidationConclusion {
-  /// Represents a conclusion that the field is valid.
-  static const valid = ValidationConclusion(name: 'valid');
+  /// Represents a conclusion that the validated field is valid.
+  static const valid = ValidationConclusion(name: 'valid', indicator: '+');
 
-  /// Represents a conclusion that the field is invalid.
-  static const invalid = ValidationConclusion(name: 'invalid');
+  /// Represents a conclusion that the validated field is invalid.
+  static const invalid = ValidationConclusion(name: 'invalid', indicator: '-');
 
-  /// Represents that the conclusion is unknown.
-  static const unknown = ValidationConclusion(name: 'unknown');
+  /// Represents that the conclusion for the validated field is unknown.
+  static const unknown = ValidationConclusion(name: 'unknown', indicator: '?');
 }
