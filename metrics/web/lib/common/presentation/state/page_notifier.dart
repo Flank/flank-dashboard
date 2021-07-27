2ef594ef634027605755066f53edf877b5268380
solid-maxim <maxim.ustych@solid.software>
2021-07-27 16:06:49 +0300

    Add license

--- metrics/web/lib/common/presentation/state/page_notifier.dart
+++ metrics/web/lib/common/presentation/state/page_notifier.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:metrics/common/presentation/navigation/models/page_parameters_model.dart';
 
--- metrics/web/test/common/presentation/navigation/widgets/page_parameters_proxy_test.dart
+++ metrics/web/test/common/presentation/navigation/widgets/page_parameters_proxy_test.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:metrics/common/presentation/navigation/state/navigation_notifier.dart';
