604cd504c554c95dabbdc17aecd75bb66a56da08
Yevheniia Shchypeleva <yevheniia.shchypeleva@solid.software>
2021-07-27 12:48:46 +0300

    Add service name enum

---
+++ metrics/cli/lib/services/common/service/model/service_name.dart
@@ -0,0 +1,23 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+/// Represents a third-party service name.
+enum ServiceName {
+  /// Represents a Firebase service name.
+  firebase,
+
+  /// Represents a Flutter service name.
+  flutter,
+
+  /// Represents a GCloud service name.
+  gcloud,
+
+  /// Represents a Git service name.
+  git,
+
+  /// Represents an npm service name.
+  npm,
+
+  /// Represents a Sentry service name.
+  sentry,
+}
