418a7498bd5c9aeedd31cef99a740ceada2854ed
solid-maxim <maxim.ustych@solid.software>
2021-07-27 09:22:51 +0300

    Improve docs for the PageNotifier

--- metrics/web/lib/common/presentation/state/page_notifier.dart
+++ metrics/web/lib/common/presentation/state/page_notifier.dart
@@ -3,9 +3,10 @@ import 'package:metrics/common/presentation/navigation/models/page_parameters_mo
 
 /// The [ChangeNotifier] that provides an ability to handle page parameters.
 abstract class PageNotifier extends ChangeNotifier {
-  /// Provides the application's [PageParametersModel].
+  /// Provides the page's [PageParametersModel].
   PageParametersModel get parameters;
 
-  /// Handles the application's page parameters using the given [parameters].
+  /// Updates the [PageParametersModel] for the specific page using the given 
+  /// [parameters].
   void handlePageParameters(PageParametersModel parameters);
 }
