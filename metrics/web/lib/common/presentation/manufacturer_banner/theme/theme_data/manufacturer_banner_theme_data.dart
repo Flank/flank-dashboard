a6a7e9b817c69df02578a8db26f0801bc4b936d1
solid-vovabeloded <vova.beloded@solid.software>
2021-08-09 10:49:33 +0300

    Documentation improvements

--- metrics/web/lib/common/presentation/manufacturer_banner/theme/theme_data/manufacturer_banner_theme_data.dart
+++ metrics/web/lib/common/presentation/manufacturer_banner/theme/theme_data/manufacturer_banner_theme_data.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 
 /// A class that represents theme data for the manufacturer banner widget.
 class ManufacturerBannerThemeData {
-  /// A [Color] of the background of the manufacturer banner widget.
+  /// A background [Color] of the manufacturer banner widget.
   final Color backgroundColor;
 
   /// A [TextStyle] of the text in the manufacturer banner widget.
