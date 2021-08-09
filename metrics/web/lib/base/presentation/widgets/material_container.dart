4772ff23c918f22fe6b7ec992223e373bd2efc5d
solid-vovabeloded <vova.beloded@solid.software>
2021-08-09 16:24:49 +0300

    Implement material container

---
+++ metrics/web/lib/base/presentation/widgets/material_container.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+/// A widget that represents the material container.
+class MaterialContainer extends StatelessWidget {
+  /// A [Widget] below this in the widget tree.
+  final Widget child;
+
+  /// An amount of space by which to inset the [child].
+  final EdgeInsets padding;
+
+  /// A [MaterialType] of this container.
+  final MaterialType type;
+
+  /// A z-coordinate at which to place this material widget related to it's
+  /// parents.
+  final double elevation;
+
+  /// A background [Color] of this widget.
+  final Color backgroundColor;
+
+  /// A [Color] of this widget's shadow.
+  final Color shadowColor;
+
+  /// A [BorderRadiusGeometry] of this widget.
+  final BorderRadiusGeometry borderRadius;
+
+  /// Creates a new instance of the [MaterialContainer] with the given
+  /// parameters.
+  ///
+  /// The [padding] defaults to [EdgeInsets.zero].
+  /// The [type] defaults to [MaterialType.canvas].
+  /// The [elevation] defaults to `0.0`.
+  ///
+  /// The [padding] and [type] must not be null.
+  /// The [elevation] must be non-negative double value.
+  const MaterialContainer({
+    Key key,
+    this.padding = EdgeInsets.zero,
+    this.type = MaterialType.canvas,
+    this.elevation = 0.0,
+    this.backgroundColor,
+    this.shadowColor,
+    this.borderRadius,
+    this.child,
+  })  : assert(padding != null),
+        assert(type != null),
+        assert(elevation != null),
+        assert(elevation >= 0.0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: elevation,
+      type: type,
+      color: backgroundColor,
+      shadowColor: shadowColor,
+      borderRadius: borderRadius,
+      child: Padding(
+        padding: padding,
+        child: child,
+      ),
+    );
+  }
+}
