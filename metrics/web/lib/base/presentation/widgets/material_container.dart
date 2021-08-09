d29c63f552c11fa06b7d05987b6e01f925563a4e
solid-vovabeloded <vova.beloded@solid.software>
2021-08-09 16:45:33 +0300

    Add license

--- metrics/web/lib/base/presentation/widgets/material_container.dart
+++ metrics/web/lib/base/presentation/widgets/material_container.dart
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
 /// A widget that represents the material container.
