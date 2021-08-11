3170b34f4b36e8591e24657128a4d3c64472cfe0
Dima Koniaiev <52494380+solid-dimakoniaiev@users.noreply.github.com>
2021-08-11 13:55:54 +0300

    feat: Change the FeatureConfig entity and the FeatureConfigData model (#1568)
    Change the FeatureConfig entity and the FeatureConfigData model
    
    Co-authored-by: solid-maksymtielnyi <maksym.tielnyi@solid.software>

--- metrics/web/test/feature_config/domain/entities/feature_config_test.dart
+++ metrics/web/test/feature_config/domain/entities/feature_config_test.dart
@@ -1,4 +1,4 @@
-// Use of this source code is governed by the Apache License, Version 2.0 
+// Use of this source code is governed by the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
 import 'package:flutter_test/flutter_test.dart';
@@ -6,20 +6,47 @@ import 'package:metrics/feature_config/domain/entities/feature_config.dart';
 
 void main() {
   group("FeatureConfig", () {
+    const isPasswordSignInOptionEnabled = true;
+    const isDebugMenuEnabled = true;
+    const isPublicDashboardEnabled = true;
+
     test(
       "creates an instance with the given parameters",
       () {
-        const isPasswordSignInOptionEnabled = true;
-        const isDebugMenuEnabled = true;
-
         const config = FeatureConfig(
           isPasswordSignInOptionEnabled: isPasswordSignInOptionEnabled,
           isDebugMenuEnabled: isDebugMenuEnabled,
+          isPublicDashboardEnabled: isPublicDashboardEnabled,
         );
 
-        expect(config.isPasswordSignInOptionEnabled,
-            equals(isPasswordSignInOptionEnabled));
+        expect(
+          config.isPasswordSignInOptionEnabled,
+          equals(isPasswordSignInOptionEnabled),
+        );
         expect(config.isDebugMenuEnabled, equals(isDebugMenuEnabled));
+        expect(
+          config.isPublicDashboardEnabled,
+          equals(isPublicDashboardEnabled),
+        );
+      },
+    );
+
+    test(
+      "equals to another FeatureConfig with the same parameters",
+      () {
+        const expected = FeatureConfig(
+          isPasswordSignInOptionEnabled: isPasswordSignInOptionEnabled,
+          isDebugMenuEnabled: isDebugMenuEnabled,
+          isPublicDashboardEnabled: isPublicDashboardEnabled,
+        );
+
+        const featureConfig = FeatureConfig(
+          isPasswordSignInOptionEnabled: isPasswordSignInOptionEnabled,
+          isDebugMenuEnabled: isDebugMenuEnabled,
+          isPublicDashboardEnabled: isPublicDashboardEnabled,
+        );
+
+        expect(featureConfig, equals(expected));
       },
     );
   });
