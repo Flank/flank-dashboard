59d7fdabdc0f0808bbf2e54cd2c0f6784276543c
solid-maksymtielnyi <maksym.tielnyi@solid.software>
2021-08-19 14:22:14 +0300

    Add license to firebase-email-domains-test.js

--- metrics/firebase/test/firestore/rules/allowed-email-domains-test.js
+++ metrics/firebase/test/firestore/rules/allowed-email-domains-test.js
@@ -1,3 +1,6 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 const async = require('async');
 
 const { assertFails } = require("@firebase/rules-unit-testing");
