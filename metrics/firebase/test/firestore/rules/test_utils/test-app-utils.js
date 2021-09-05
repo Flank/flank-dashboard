435cef1e61854b314f6d7cce11d57965fd7bd195
anatolyeva <anatoly.eva@solid.software>
2021-09-05 21:57:44 +0300

    [WIP] Add general function for testing users permissions

--- metrics/firebase/test/firestore/rules/test_utils/test-app-utils.js
+++ metrics/firebase/test/firestore/rules/test_utils/test-app-utils.js
@@ -5,11 +5,14 @@ const {
   initializeTestApp,
   initializeAdminApp,
   loadFirestoreRules,
-  apps,
+  apps, assertSucceeds, assertFails,
 } = require("@firebase/rules-unit-testing");
 
 const { readFileSync } = require("fs");
+const async = require("async");
 const projectId = `rules-spec-${Date.now()}`;
+const featureConfigPath = "feature_config/feature_config";
+const collectionsWithPublicDashboardTests = ["project_groups","projects","build_days","build","user_profiles"];
 
 /** Constructs an application with the given user. */
 exports.getApplicationWith = async function (user) {
@@ -44,4 +47,77 @@ exports.setupTestDatabaseWith = async function (initialData) {
 /** Deletes the application and frees the resources of all associated services. */
 exports.tearDown = async () => {
   return Promise.all(apps().map((app) => app.delete()));
+};
+
+/** Tests security rules with public dashboard feature. */
+exports.userPermissionsTest = async function (usersPermissions, config, collection, addValue, updateValue){
+  async.forEach(usersPermissions, (user, callback) => {
+    describe(user.describe, () => {
+      let publicDashboardState = '';
+      let userPath;
+
+      if(collectionsWithPublicDashboardTests.find(e => e === collection) !== undefined){
+        publicDashboardState = config[featureConfigPath].isPublicDashboardEnabled
+          ? 'on'
+          : 'off';
+        userPath = user.public_dashboard[publicDashboardState];
+      }
+      else {
+        userPath = user;
+      }
+
+
+      let canCreateDescription = userPath.can.create ?
+        "allows creating a build day" : "does not allow creating a build day";
+      let canReadDescription = userPath.can.read ?
+        "allows reading build days" : "does not allow reading build days";
+      let canUpdateDescription = userPath.can.update ?
+        "allows updating a build day" : "does not allow updating a build day";
+      let canDeleteDescription = userPath.can.delete ?
+        "allows deleting a build day" : "does not allow deleting a build day";
+
+      it(canCreateDescription, async () => {
+        const createPromise = user.app.collection(collection).add(addValue);
+
+        if (userPath.can.create) {
+          await assertSucceeds(createPromise)
+        } else {
+          await assertFails(createPromise)
+        }
+      });
+
+      it(canReadDescription, async () => {
+        const readPromise = user.app.collection(collection).get();
+
+        if (userPath.can.read) {
+          await assertSucceeds(readPromise)
+        } else {
+          await assertFails(readPromise)
+        }
+      });
+
+      it(canUpdateDescription, async () => {
+        const updatePromise =
+          user.app.collection(collection).doc("1").update(updateValue);
+
+        if (userPath.can.update) {
+          await assertSucceeds(updatePromise)
+        } else {
+          await assertFails(updatePromise)
+        }
+      });
+
+      it(canDeleteDescription, async () => {
+        const deletePromise =
+          user.app.collection(collection).doc("1").delete();
+
+        if (userPath.can.delete) {
+          await assertSucceeds(deletePromise)
+        } else {
+          await assertFails(deletePromise)
+        }
+      });
+    });
+    callback();
+  });
 };
