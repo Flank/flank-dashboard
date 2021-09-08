453aadb1f316b228ab1a99237ff8b4d6fefeeb1c
solid-anatolyeva <88667108+solid-anatolyeva@users.noreply.github.com>
2021-09-08 14:10:49 +0300

    feat: Change the MetricsUserMenu UI depending on the user authentication state (#1586)
    * Implement UserProfileViewModel class
    
    * [WIP] Integrate UserProfileViewModel into AuthNotifier, change user menu UI
    
    * [WIP] Change user menu UI
    
    * Add MetricsUserMenu tests
    
    * Minor fixes
    
    * Add UserProfileViewModel tests
    
    * Minor changes
    
    * Minor changes
    
    Co-authored-by: solid-maksymtielnyi <maksym.tielnyi@solid.software>
    Co-authored-by: solid-maksymtielnyi <87802654+solid-maksymtielnyi@users.noreply.github.com>

---
+++ metrics/web/lib/auth/presentation/view_models/user_profile_view_model.dart
@@ -0,0 +1,18 @@
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+/// A view model that represents the logged-in user profile.
+class UserProfileViewModel extends Equatable {
+  /// Indicates whether the logged in user is anonymous.
+  final bool isAnonymous;
+
+  /// Creates the [UserProfileViewModel] with the given [isAnonymous] value.
+  const UserProfileViewModel({@required this.isAnonymous})
+      : assert(isAnonymous != null);
+
+  @override
+  List<Object> get props => [isAnonymous];
+}
